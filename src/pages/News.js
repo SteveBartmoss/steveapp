0bfae6679b34b9f1452597c232f7f93112b94c08
Marco Rivas <za19012397@zapopan.tecmm.edu.mx>
2023-03-01 18:43:57 -0600

    upcoming section is ready now and is on produccion

--- src/pages/News.js
+++ src/pages/News.js
@@ -2,6 +2,9 @@ import React from "react";
 import "../App.css";
 import Stiker from "../component/Stiker";
 import need from "../img/heatminiatura.png";
+import real from "../img/realmanMin.jpg";
+import labs from "../img/labsMin.png";
+
 
 function News(){
     return(
@@ -13,6 +16,16 @@ function News(){
                 de vista sobre el juego y decida si le interesa el juego o mejor pasa de el"
             />
             <h1 className="title1">Proximanente</h1>
+
+            <Stiker imge={real} title="The real man dont click"
+            paragrap="El camino para ser un linuxero no es facil pero es el camino que hay qye seguir
+            lograre convertirme en un linuxero completo y fracasare en mi mision y volvere al lado 
+            oscuro de windows" />
+
+            <Stiker imge={labs} title="Stevelabs"
+            paragrap="Tutoriales de un chico que no es precisamente un hacker/desarrollador Senior o simplemente
+            un verdadero erudito de la programacion pero comparte lo que sabe y que quisa pueda servirte" />
+
         </div>
     );
 }
