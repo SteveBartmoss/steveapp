2621038cb0f6a6efc68210c4a8bdf75b0bbb0e4e
Marco Rivas <za19012397@zapopan.tecmm.edu.mx>
2023-03-18 14:54:15 -0600

    new seccion is in progres now

---
+++ src/Articles/RC1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Articulo(){
+    return(
+        <>
+            <div>
+                <h1>Articulo pendiente</h1>
+            </div>
+        </>
+    );
+}
