22183eeadb5113f9fd8b268ee3a138078a43d580
Marco Rivas <za19012397@zapopan.tecmm.edu.mx>
2023-03-22 22:02:09 -0600

    add some text from the new seccion

--- src/Articles/RC1.jsx
+++ src/Articles/RC1.jsx
@@ -4,7 +4,15 @@ function Articulo(){
     return(
         <>
             <div>
-                <h1>Articulo pendiente</h1>
+                <h1>El cyber punk esta delante de nosotros</h1>
+                <p>La mayoria de personas se siente atraida por la idea de vivir en una epoca ambientada en
+                    la tematica futurista y distopica de una sociedad cyberpunk ya que para ser honestos a quien
+                    no le gusta la idea de que tengamos autos voladores, implamentes tecnologicos y a una red de
+                    tecnologia que nos permite estar conectados con la mayoria. Pero esto no esta lejos de nuestra
+                    actualidad y de hecho la gran mayoria de personas esta viviendo en una epoca de tecnologia al 
+                    nivel de una historia cyberpunk aunque no lo paresca hay una gran muestra de esto en muchos lados
+                    como podemos ver a continuacion,
+                </p>
             </div>
         </>
     );
