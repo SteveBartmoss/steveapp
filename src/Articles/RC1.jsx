0ea6fb9994a35bda702df3a1d6667556ac965571
Marco Rivas <za19012397@zapopan.tecmm.edu.mx>
2023-03-23 22:20:52 -0600

    add content for the new seccion

--- src/Articles/RC1.jsx
+++ src/Articles/RC1.jsx
@@ -13,6 +13,52 @@ function Articulo(){
                     nivel de una historia cyberpunk aunque no lo paresca hay una gran muestra de esto en muchos lados
                     como podemos ver a continuacion,
                 </p>
+                <p>
+                    La mayoría de personas que no vivieron la revolución tecnologica no entenderán porque el cyberpunk 
+                    y es una realidad ya que ellos nacieron cuando ya todo estaba normalizado o simplemente ya había mucha 
+                    más tecnología que antes, para alguien que nació entre el 2015 y el 2018 no resulta nuevo ver sitios de 
+                    internet modernos como lo son YouTube, facebook, Instagram o incluso juegos como fornite, pero los que 
+                    nacieron antes de los 2000 e incluso los que nacieron en la década de los ochenta habrán quedado 
+                    sorprendidos por el gran cambio que tuvo el internet en un tiempo tan corto como lo fueron 10 años y es 
+                    por esto que la mayoría no siente que el cyberpunk está presente pero realmente estamos viviendo en un 
+                    futuro como el que se planteaba blade runner o alguna otra distopia futurista pero el problema es que o 
+                    bien no tenemos acceso a la última tecnología o bien los escenarios anteriores si exageraron un poco. Para 
+                    ponernos en situación tendríamos que viajar un poco al pasado a la época en que fueron escritas historias 
+                    como volver al futuro o blade runner, dejando un poco de lado la gran ficción que tienen ambos ejemplos 
+                    podemos centrarnos en algo importante y es que en esa época la tecnología con la se contaba era bastante 
+                    reducida y como muestra de eso tenemos los teléfonos inteligentes que representan en su mayoría el nivel 
+                    de avance que se logró en todo este tiempo, una persona que vivía en los 80 tenía que cargar una gran cantidad 
+                    de complementos con el para poder tener acceso a unas cuantas funciones que nos brindan los teléfonos actuales. 
+                    por ejemplo para poder escuchar música había falta llegar un walkman ya fuera la versión de casete o la versión 
+                    más moderna de cd, los audífonos solían ser más grande que el estándar actual y no se contaba con la ventaja de 
+                    audífonos inalambricos, para tomar una fotografía era necesario llevar su propia cámara fotográfica y rollo fotográfico 
+                    y en ocasiones la foto no se podia ver hasta que era revelada, com estos dos ejemplos ya estariamos entendiendo el por qué 
+                    el estamos viviendo en el futuro pero no lo percibimos como tal, retomando el ejemplo del teléfono existe una tecnología aún 
+                    más importante y que hace unos años era imposible de tener un acceso tan fácil como lo tenemos actualmente y es poder navegar 
+                    por internet. En algunas épocas directamente no existia el internet o existia pero su acceso estaba limitado a universidades y 
+                    laboratorios y si una persona quería que tener acceso a el debía pagar una cantidad bastante alta de dinero y además tenía una conexión 
+                    tan buene como las actuales. Hoy en día incluso pagando el servicio más bajo de internet tendríamos acceso a por lo menos 10 megas 
+                    en el peor de los casos pero esto sigue estando muy por arriba de los estándares de ciertas décadas, actualmente no solo la conexión 
+                    es mejor si no también por demos acceder a internet desde nuestro teléfono pagando un plan a la compañía de teléfonia y navegar es tan 
+                    sencillo como activar los datos móviles y abrir la pestaña de nuestro navegador favorito. La facilidad con la que interactuamos con el 
+                    internet no nos sorprende pero ciertamente es muy avanzada cuando nos ponemos a observar como eran las cosas antes, además del caso del 
+                    internet existen muchas otras tecnologías que han superado la barrera de la ficción y que han llegado a la realidad pensemos por ejemplo 
+                    en el caso de las impresoras 3d cuya tecnología empieza a ser más accesible para las personas y no solo se ve limitada a grandes empresas 
+                    o millonarios. Anterior mente se tenía que solicitar un repuesto a un fabricante porque probablemente el producto estaba echo mediante 
+                    moldes de inyección y no tan sencillo replicar las piezas con las que se ensambla un producto pero en la actualidad la empresa 
+                    puede tener la opción de bucsar un modelo en 3d de la pieza y crearla ahí mismo lo cual no representa la opción más factible a nivel 
+                    costo pero que si es de gran utilidad en entornos de poca accesibilidad o que el envío de la pieza es bastante difícil. En un inicio 
+                    está tecnología no estaba disponible para todo el mundo pero actualmente se puede adquirir una impresora 3d en diferentes tiendas en línea 
+                    y aún precio relativamente accesible ya que al igual que paso con las computadoras la tecnología empezó a bajar su costo con lo cual está 
+                    disponible para un público mayor, ejemplos como los anteriores siguen en una lista bastante larga pero es importante mencionar otro componente y 
+                    es que el cyberpunk no solo refleja los avances tecnologicos si no también la decadencia que sufre la humanidad con el aumento de la tecnología 
+                    algo que de igual manera está presente en la actualidad ya que el consumismo y las grandes empresas que dominan el mercado avanzan cada día más y 
+                    la calidad de vida no es la mejor con lo cual tenemos la dualidad de las distopías futuristas un nivel muy altos e tecnología y una calidad de vida baja. 
+                    La corriente de las obras de cyberpunk seguiron como una expresión del temor que su tubo con los grandes avances en la época de los ochentas en la cual 
+                    se inicia el camino de las tecnologías que están disponibles en la actualidad, las obras de ficción se plantean cómo una advertencia de lo que nos separa 
+                    el futuro pero esto no parece tener el impacto que se esperaba. En cualquier caso el avance de la tecnología no se detiene y lo único que nos queda 
+                    es obtener lo bueno que nos ofrece y solventar las desventajas que esto conyeba.  
+                </p>
             </div>
         </>
     );
