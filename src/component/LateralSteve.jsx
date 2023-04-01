ea31a022f8516a16d94dc6be2de2f2ab317ebade
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-04-01 16:38:20 -0600

    add some clasname and remove old elements

--- src/component/LateralSteve.jsx
+++ src/component/LateralSteve.jsx
@@ -14,6 +14,8 @@ function Lateral(){
     return(
         <>
         <div className="div-lateral">
+
+            <div className="back-color">
             <h1 className="title1">Lenguajes de programacion que he usado</h1>
             <div className="div-prog">
                 <img className="img-prog" src={java} />
@@ -22,32 +24,43 @@ function Lateral(){
                 <img className="img-prog" src={py} />
                 <img className="img-prog" src={js} />
             </div>
+            </div>
+
+            <div className="back-color">
             <h1 className="title1">Conocimientos en bases de datos</h1>
             <div className="div-prog">
                 <img className="img-prog" src={mysql} />
                 <img className="img-prog" src={sql} />
                 <img className="img-prog" src={mongo} />
             </div>
+            </div>
+
+            <div className="back-color">
             <h1 className="title1">Lenguaje de programacion Favorito</h1>
             <div>
                 <img className="img-prog" src={java} />
             </div>
-            <p>Java el mejor lenguaje que existe</p>
+            </div>
 
+            <div className="back-color">
             <h1 className="title1">Editor de codigo Favorito</h1>
             <div>
                 <img className="img-prog" src={vscode} />
             </div>
-            <p>Vscode ya que vim aun no lo domino</p>
+            </div>
 
+            <div className="back-color">
             <h1 className="title1">Distribucion que usa actual mente</h1>
             <div>
                 <img className="img-prog" src={ubuntu} />
             </div>
-            <p>Ubuntu ya que es para novatos</p>
+            </div>
 
+            <div className="back-color">            
             <h1 className="title1">Manejador de versiones usado</h1>
             <p>Github un clasico</p>
+            </div>
+
         </div>
         </>
     );
