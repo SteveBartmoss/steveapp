07f2df5c58ba53caade0074792053302d456dcd8
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-04-13 20:34:58 -0600

    new seccion is in proges now

---
+++ src/dumix/dumixHome.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DumixHome(){
+    return(
+        <>
+            <h1 className="title1">Dumix Home</h1>
+        </>
+    );
+}
+
+export default DumixHome;
