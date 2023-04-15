fdf725935d9addb368a8203ec1c4bdd79ea7b03b
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-04-15 17:18:24 -0600

    data provides now a route for img, that come from assets folder

--- src/component/Data/Favoritos.js
+++ src/component/Data/Favoritos.js
@@ -1,33 +1,29 @@
-import book from "../../img/book.png";
-import arcade from "../../img/arcade.png";
-import music from "../../img/music.png";
-
 export const Favoritos=[
     {
         'id': 1,
         'titulo': 'Libros favoritos',
-        'imagen': book,
+        'imagen': "/Assets/book.png",
         'parrafo': 'Algunos de mis libros favoritos que inspiran la tematica del canal',
         'titulos': ['Saga de harry Potter','Ready Player One', 'Altered Carbon'],
     },
     {
         'id': 2,
         'titulo': 'Juegos favoritos',
-        'imagen': arcade,
+        'imagen': "/Assets/arcade.png",
         'parrafo': 'Los juegos que suelo jugar cuando tengo tiempo libre',
         'titulos': ['Halo reach', 'Age of empires', 'Ned for speed underground','Prince of persia','Fable','Cyber punk']
     },
     {
         'id': 3,
         'titulo': 'Series favoritas',
-        'imagen': music,
+        'imagen': "/Assets/music.png",
         'parrafo': 'Las series que suelo ver cuando tengo tiempo libre',
         'titulos': ['Rick y Morty', 'Love dead and robots', 'Peaky blinders','Mr robot']
     },
     {
         'id': 4,
         'titulo': 'Peliculas Favoritas',
-        'imagen': book,
+        'imagen': "/Assets/book.png",
         'parrafo': 'Algunoas de mis peliculas favoritas que inspiran la tematica del canal',
         'titulos': ['Volver al futuro','Ready Player One', 'Blade runer', 'Drive', 'Iron man']
     },
@@ -35,7 +31,7 @@ export const Favoritos=[
         
         'id': 5,
         'titulo': 'Caricaturas Favoritas',
-        'imagen': book,
+        'imagen': "/Assets/book.png",
         'parrafo': 'Algunas de mis caricaturas favoritas',
         'titulos': ['Un show mas','Hora de aventura', 'clarence', 'Tio grandpa', 'Hey arnoold']
     },
@@ -43,7 +39,7 @@ export const Favoritos=[
         
         'id': 6,
         'titulo': 'Animes',
-        'imagen': arcade,
+        'imagen': "/Assets/arcade.png",
         'parrafo': 'Algunas de mis caricaturas favoritas',
         'titulos': ['Cowboy','Bersek', 'clarence', 'Tio grandpa', 'Hey arnoold']
     },
