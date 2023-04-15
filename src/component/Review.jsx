2dffe8625679c350bfb239f803e1da52c51f7145
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-04-15 16:15:23 -0600

    new folder implements and property src in img mark changed and delete inecesary imports fron the code

--- src/component/Review.jsx
+++ src/component/Review.jsx
@@ -1,10 +1,5 @@
 import React from "react";
 import Card from "./Card";
-import need from "../img/heatminiatura.png";
-import learn from "../img/whyle.png";
-import punk from "../img/frostpunminiatura.png";
-import sunset from "../img/sunsetMiniatura.jpg";
-import Baner from "./Baner";
 
 function Review(){
     return (
@@ -22,24 +17,24 @@ function Review(){
                         uno se da cuenta de que realmente esl juego se trata de un cascaron vacio
                     </p>
                     <center>
-                        <a className="linkVideo">Seguir leyendo</a>
+                        <a className="linkVideo" href="article">Seguir leyendo</a>
                     </center>
                 </div>
             </section>
             <div className="Videos">
-                <Card imgUrl={need} title="Net for speed Heat" 
+                <Card imgUrl={"/Assets/heatminiatura.png"} title="Net for speed Heat" 
                 paragraph="Un nuevo need for speed pero Esta a la altura de la edad de oro?"
                 urlVideo="https://www.youtube.com/watch?v=jmLM6DvAn84"/>
 
-                <Card imgUrl={learn} title="while true learn" 
+                <Card imgUrl={"/Assets/whyle.png"} title="while true learn" 
                 paragraph="Un juego unico en su clase y que ademas busca dar conocimiento a los usuarios"
                 urlVideo="https://www.youtube.com/watch?v=GVYxvpG3RkM"/>
 
-                <Card imgUrl={punk} title="Frost punk" 
+                <Card imgUrl={"/Assets/frostpunminiatura.png"} title="Frost punk" 
                 paragraph="Un juego que propone un desafio helado para todos los que se atrevan a jugarlo"
                 urlVideo="https://www.youtube.com/watch?v=vfIXW96xbZA"/>
 
-                <Card imgUrl={sunset} title="Sunset Overdrive" 
+                <Card imgUrl={"/Assets/sunsetMiniatura.jpg"} title="Sunset Overdrive" 
                 paragraph="Un juego que tal vez no fue tan valorado pero que tiene mucho para ofrecer si lo pruebas"
                 urlVideo="https://www.youtube.com/watch?v=Ip7WDHnbB5s"/>
 
