f345d6644025e0f137f83e14b741e3ae3b7c1a57
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-04-15 16:16:09 -0600

    new folder implements and property src in img mark changed and delete inecesary imports fron the code

--- src/component/Videos.js
+++ src/component/Videos.js
@@ -1,12 +1,7 @@
 import React from "react";
 import Card from "./Card";
-import sys32 from "../img/system32.jpg";
-import pac from "../img/pacman.jpg";
-import cpu from "../img/cpu.jpg";
-import lib from "../img/librerias.jpg";
 
 import '../App.css';
-import Baner from "./Baner";
 
 function Videos(){
     return(
@@ -14,19 +9,19 @@ function Videos(){
         <section className="back-videos">
         <div className="grid-videos">
             <div className="Videos">
-                <Card imgUrl={sys32} title="Borrar Sytem32" 
+                <Card imgUrl={"/Assets/system32.jpg"} title="Borrar Sytem32" 
                 paragraph="La popular broma de borrar System32 para que tu pc sea mas rapida puede generar mas problemas de lo que crees"
                 urlVideo="https://www.youtube.com/watch?v=QsPAVmQFnUQ"/>
 
-                <Card imgUrl={pac} title="Los fantasmas de pacman"
+                <Card imgUrl={"/Assets/pacman.jpg"} title="Los fantasmas de pacman"
                 paragraph="El popular juego guarda algunos secretos sobre el comportamiento de sus enemigos, una forma de programacion interesante"
                 urlVideo="https://www.youtube.com/watch?v=XLCuuMvgbec"/>
 
-                <Card imgUrl={cpu} title="Procesador multi nucleo"  
+                <Card imgUrl={"/Assets/cpu.jpg"} title="Procesador multi nucleo"  
                 paragraph="Los procesadores tienen diferentes propiedades y una de ellas es el numero de nucleos que pose, aprende mas en el video"
                 urlVideo="https://www.youtube.com/watch?v=AuPJ8kL-iLs"/>
 
-                <Card imgUrl={lib} title="Librerias de programacion" 
+                <Card imgUrl={"/Assets/librerias.jpg"} title="Librerias de programacion" 
                 paragraph="Las librerias de programacion son un tema recurrente dentro de la programacion, visita el video para saber mas sobre ellas"
                 urlVideo="https://www.youtube.com/watch?v=V4ZxnSYly-Y"/>
             </div>
