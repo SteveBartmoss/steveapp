c20882e94faa109c489cf40590df4aad025c34c0
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-04-15 16:11:30 -0600

    property alt is added, eliminate warnings now

--- src/component/Card.js
+++ src/component/Card.js
@@ -5,7 +5,7 @@ function Card({imgUrl,title,paragraph,urlVideo}){
     return(
         <div className="back-card">
         <div className="div-card">
-            <img className="imgCard" src={imgUrl}/>
+            <img className="imgCard" src={imgUrl} alt="imagenVideo"/>
             <h3 className="titleCard">{title}</h3>
             <p>{paragraph}</p>
             <a className="linkVideo" href={urlVideo}>Ir al video</a>
