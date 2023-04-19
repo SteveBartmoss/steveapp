3d4a0a530f1cf89c329adc9f7edad43e30318bcf
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-04-18 22:13:08 -0600

    add some informacion, missing styles and the page is in progres

---
+++ src/dumix/pages/dumixAbout.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function DumixAbout(){
+    return(
+        <>
+        <h1 className="title1">Que es dumix</h1>
+        <p className="Content">Ya que mis habilidades para apoyar al desarrollo del codigo abierto
+        no son las mejores y mi tiempo es limitado, cree este apartado en mi pagina en el compartari
+        tutoriales para que la gente pueda introducirse a los comandos de linux de manera facil y perder
+        el miedo de usar la terminal.</p>
+        <p className="Content">Por el momento solo se contaran con comandos basicos que se deben aprender 
+        en linux pero con suerte la pagina contara con mas tutoriales y espero en algun momento obtener el 
+        apoyo de personas mejor calificadas en el tema para poder ofrecer mejores conocmientos pero por ahora
+        este es un pequeño aporte para la comunidad</p>
+
+        <h1 className="title1">¿Porque deberia aprender linux?</h1>
+        <p className="Content">Si no eres un usuario que esta enfocado a la informartica, las ingenierias de
+        computacion o ciencias puede que no sea una gran herramienta, no lo mal interpretes no es malo que aprendas
+        linux pero siendo un usario comun de computadora no obtendras una herramienta tan util</p>
+        </>
+    );
+}
+
+export default DumixAbout;
