c86ce1a1b92a5c2e44a1d99fc25f768df68db92b
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-04-19 21:19:01 -0600

    add fisrt card curse in the main page

--- src/dumix/dumixHome.jsx
+++ src/dumix/dumixHome.jsx
@@ -8,6 +8,16 @@ function DumixHome(){
             <Baner/>
             <div>
                 <h1 className="title1">Contenido principal</h1>
+                <div>
+                    <h1 className="title1">Primeros pasos</h1>
+                    <p className="Content">En esta seccion se abordaran temas para personas
+                    que no han interactuado con linux, se describira lo que es una terminal
+                    de comandos, cuales son las partes que componen la terminal, comandos basicos
+                    para crear un archovo, como ver los permisos, como listar los elementos de un 
+                    directorio, como navegae entre direcctorios. Esta parte busca dar una introduccion
+                    a los usuarios que nunca han utilizado comandos</p>
+                    <button>Ir al curso</button>
+                </div>
             </div>
             <DumixAbout />
         </>
