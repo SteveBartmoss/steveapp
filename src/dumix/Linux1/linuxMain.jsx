e359d376aff3a75ff2677c206ad4c032ef22f075
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-04-21 16:16:25 -0600

    add divs for the content, is in working now

--- src/dumix/Linux1/linuxMain.jsx
+++ src/dumix/Linux1/linuxMain.jsx
@@ -4,6 +4,10 @@ function PrimerosPasos(){
     return(
         <>
             <h1>curso de primeros pasos</h1>
+            <div className="grid-dumix">
+                <div className="left-dumix">
+                </div>
+            </div>
         </>
     );
 }
