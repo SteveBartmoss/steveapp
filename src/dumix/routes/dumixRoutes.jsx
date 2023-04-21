d2a0a76988807131fc1e1b62677379871d947e39
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-04-21 16:17:23 -0600

    add new router, is in progres now

---
+++ src/dumix/routes/dumixRoutes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter, Routes } from "react-router-dom";
+
+function RoutesDumix (){
+    <BrowserRouter>
+        <Routes>
+            
+        </Routes>
+    </BrowserRouter>
+}
