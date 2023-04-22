9908ab3fdf7fd8f6b586ab08c4ac516c430c1f91
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-04-21 22:19:56 -0600

    add new classname and the new elements for the page

--- src/dumix/Linux1/linuxMain.jsx
+++ src/dumix/Linux1/linuxMain.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Linux1NavBar from "./routes/Linux1Navbar";
 
 function PrimerosPasos(){
     return(
         <>
-            <h1>curso de primeros pasos</h1>
+            <h1 className="title1">curso de primeros pasos</h1>
             <div className="grid-dumix">
                 <div className="left-dumix">
                 </div>
@@ -11,3 +12,5 @@ function PrimerosPasos(){
         </>
     );
 }
+
+export default PrimerosPasos;
