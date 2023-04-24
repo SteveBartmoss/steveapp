865873433e6f35ec70caa7a6f69c47ad91e2e9fc
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-04-23 19:53:17 -0600

    add new routes for navegation

--- src/dumix/routes/dumixRoutes.jsx
+++ src/dumix/routes/dumixRoutes.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import PrimerosPasos from "../Linux1/linuxMain";
 import Presentacion from "../Linux1/pages/Prensentacion";
 import Linux1NavBar from "../Linux1/routes/Linux1Navbar";
+import Cli from "../Linux1/pages/Cli";
 
 function RoutesDumix (){
     return(
@@ -10,6 +10,14 @@ function RoutesDumix (){
             <Linux1NavBar />
             <Routes>
                 <Route path="/presentacion" element={<Presentacion/>}/>
+            </Routes>
+
+            <Routes>
+                <Route path="/comandline" element={<Cli/>}/>
+            </Routes>
+
+            <Routes>
+                <Route path="/" element={<Presentacion/>}/>
             </Routes>
         </div>
     );
