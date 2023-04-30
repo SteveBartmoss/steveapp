54538a91045e8ecf5893358068dc9c8d792a4f20
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-04-29 22:47:41 -0600

    add new div and outlet component for function

--- src/dumix/Linux1/routes/Linux1Navbar.jsx
+++ src/dumix/Linux1/routes/Linux1Navbar.jsx
@@ -1,58 +1,61 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 function Linux1NavBar(){
     return(
-        <div className="linux-menu">
+        <div className="grid-dumix">
 
-            <li>
-                <NavLink className={({isActive})=> `tema ${isActive ? 'Active':''}`} to="presentacion">Presentacion</NavLink>
-            </li>
+            <div className="linux-menu">
+                
+                <li>
+                    <NavLink className={({isActive})=> `tema ${isActive ? 'active':''}`} to="presentacion">Presentacion</NavLink>
+                </li>
 
-            <li>
-                <NavLink className={({isActive})=> `tema ${isActive ? 'Active':''}`} to="comandline">Comand Line</NavLink>
-            </li>
+                <li>
+                    <NavLink className={({isActive})=> `tema ${isActive ? 'active':''}`} to="comandline">Comand Line</NavLink>
+                </li>
 
-            <li>
-                <NavLink className={({isActive})=> `tema ${isActive ? 'Active':''}`} to="sintaxis">Sintaxis</NavLink>
-            </li>
+                <li>
+                    <NavLink className={({isActive})=> `tema ${isActive ? 'active':''}`} to="sintaxis">Sintaxis</NavLink>
+                </li>
 
-            <li>
-                <NavLink className={({isActive})=> `tema ${isActive ? 'Active':''}`} to="directorio">Directorio</NavLink>
-            </li>
+                <li>
+                    <NavLink className={({isActive})=> `tema ${isActive ? 'active':''}`} to="directorio">Directorio</NavLink>
+                </li>
 
-            <li>
-                <NavLink className={({isActive})=> `tema ${isActive ? 'Active':''}`} to="archivos">Archivos</NavLink>
-            </li>
+                <li>
+                    <NavLink className={({isActive})=> `tema ${isActive ? 'active':''}`} to="archivos">Archivos</NavLink>
+                </li>
 
-            <li>
-                <NavLink className={({isActive})=> `tema ${isActive ? 'Active':''}`} to="editores">Editores</NavLink>
-            </li>
+                <li>
+                    <NavLink className={({isActive})=> `tema ${isActive ? 'active':''}`} to="editores">Editores</NavLink>
+                </li>
 
-            <li>
-                <NavLink className={({isActive})=> `tema ${isActive ? 'Active':''}`} to="paquetes">Paquetes</NavLink>
-            </li>
+                <li>
+                    <NavLink className={({isActive})=> `tema ${isActive ? 'active':''}`} to="paquetes">Paquetes</NavLink>
+                </li>
 
-            <li>
-                <NavLink className={({isActive})=> `tema ${isActive ? 'Active':''}`} to="redireccion">Redireccion</NavLink>
-            </li>
+                <li>
+                    <NavLink className={({isActive})=> `tema ${isActive ? 'active':''}`} to="redireccion">Redireccion</NavLink>
+                </li>
 
-            <li>
-                <NavLink className={({isActive})=> `tema ${isActive ? 'Active':''}`} to="sudo">Sudo</NavLink>
-            </li>
+                <li>
+                    <NavLink className={({isActive})=> `tema ${isActive ? 'active':''}`} to="sudo">Sudo</NavLink>
+                </li>
 
-            <li>
-                <NavLink className={({isActive})=> `tema ${isActive ? 'Active':''}`} to="system">System</NavLink>
-            </li>
+                <li>
+                    <NavLink className={({isActive})=> `tema ${isActive ? 'active':''}`} to="system">System</NavLink>
+                </li>
 
-            <li>
-                <NavLink className={({isActive})=> `tema ${isActive ? 'Active':''}`} to="usuarios">Usuarios</NavLink>
-            </li>
+                <li>
+                    <NavLink className={({isActive})=> `tema ${isActive ? 'active':''}`} to="usuarios">Usuarios</NavLink>
+                </li>
 
-            <li>
-                <NavLink className={({isActive})=> `tema ${isActive ? 'Active':''}`} to="permisos">Permisos</NavLink>
-            </li>
-
+                <li>
+                    <NavLink className={({isActive})=> `tema ${isActive ? 'active':''}`} to="permisos">Permisos</NavLink>
+                </li>
+            </div>
+            <Outlet />
         </div>
     );
 }
