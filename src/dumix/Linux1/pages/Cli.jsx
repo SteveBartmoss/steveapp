4400c04e3dfa9e26971eb5176e9d1dc3229bafb0
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-05-01 20:52:01 -0600

    add new contend for the subject cli, is in progres now

--- src/dumix/Linux1/pages/Cli.jsx
+++ src/dumix/Linux1/pages/Cli.jsx
@@ -11,14 +11,37 @@ function Cli(){
             aparece una linea de texto que comun mente se llamaba promp</p>
 
             <div className="bash">
-                <p className="Content">steve@steve-HP-Notebook:~$</p>
+                <p className="promp">steve@steve-HP-Notebook:~$</p>
             </div>
 
             <p className="Content">Usar una terminal puede parecer dificil pero no es para tanto solo se trata de
-            escribir intrucciones para la computadora lo cual era bastante comun con las primeras computadoras
+            escribir intrucciones para la computadora lo cual era bastante comun con las primeras computadoras en
+            las cuales no existia una interfaz de usuario y todo era dirigido por la linea de comandos, esto hacia
+            que manejar una computadora fuera una tarea mas compleja para los usuarios que no estaban muy acostumbrados
+            a programar pero cuando se volvio popular tener un computadora se busco la manera de facilitar su uso y por eso
+            se crearon las interfaces de usuarios para hacer que mas personas pudieran usar una computadora.
             </p>
 
-            <h1 className="title1">¿Porque usar un cli?</h1>        
+            <h1 className="title1">¿Porque usar un cli?</h1>
+
+            <p className="Content">Si bien el uso de una terminal dejo de ser popular y actualmente solo se usa para
+            intalar programas avanzados o para revisar la ip de un equipo aprender a usarla puede ser de gran ayuda
+            sobre todo en el ambito de redes y de servidores en el cual usar una terminal es muy comun, esto se debe 
+            a que un sistema menajdo por comandos es muy ligero en cuestion de recursos logrando un mejor desempeño 
+            a costa de tener un manejo mas complejo y menos llamativos para las personas pero aunque paresca lo contrario
+            la termial tiene el mismo o mejor nivel de manejo del sistema operativo ya que muchas veces la interfaz de
+            usuario ejecuta comandos de terminal en segundo plano sin que el usuario se de cuenta ya que muchas veces la
+            unica manera de realizaer ciertas tares es mediante la llamda de cierta rutina y eso se logra mediante 
+            un comando.
+            </p>
+
+            <p className="Content">Para empezar a usar una terminal es tan cencillo como usar el comando cntrl+alt+t 
+            si estas en un sistema de linux y esto abria una nueva terminal de comandos ya sea usando bash o con zsh 
+            que es la terminal que se suale utilizar un sistema unix o mac, en caso de que el comando no funcion en la
+            distribucion que tiene instalada basta con buscar en entre las aplicaciones la que diga terminal en el caso
+            del sistema operativo windows podemos buscar usando el menu la aplicacion de cmd o tambien la aplicacion de
+            power shell difernecia antre ambas queda como tarea de los usarios de windows.
+            </p> 
         </div>
         
         
