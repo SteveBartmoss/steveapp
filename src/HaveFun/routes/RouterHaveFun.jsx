5d847e4c2ed65648d73a3ebbebbb2928779990cf
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-05-06 23:48:16 -0600

    add routes for rickosoapp and marvel app

--- src/HaveFun/routes/RouterHaveFun.jsx
+++ src/HaveFun/routes/RouterHaveFun.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import GifApp from "../gifapp/gifMain";
+import RickosoApp from "../rickoso/rickosoMain";
+import MarvelApp from "../marvel/marvelMain";
 
 function HaveFunRouter(){
     return(
         <div>
+
             <Routes>
                 <Route path="gifapp" element={<GifApp />}/>
+                <Route path="rickosoapp" element={<RickosoApp />}/>
+                <Route path="marvelapp" element={<MarvelApp />}/>
             </Routes>
         </div>
     );
