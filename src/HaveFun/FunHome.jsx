fde0acd7cb3601321dbf78e896b332399bda2aec
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-05-06 23:01:51 -0600

    add a title and tag for the gifapp

--- src/HaveFun/FunHome.jsx
+++ src/HaveFun/FunHome.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function FunHome(){
-    return(<h1 className="title1">have fun page</h1>);
+    return(
+    <>
+        <h1 className="title1">have fun page</h1>
+
+        <div>
+            <h1 className="Content">gifApp</h1>
+            <a href="havefun/gifapp">visitar</a>
+        </div>
+    </>
+    );
 }
 
 export default FunHome;
