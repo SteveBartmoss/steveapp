a9a27ae30badf909826675fa76f8f368c80610a2
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-05-06 23:44:39 -0600

    add some information for the gifapp and rickosoapp, add a descrippcion for the bot subjets

--- src/HaveFun/FunHome.jsx
+++ src/HaveFun/FunHome.jsx
@@ -5,9 +5,21 @@ function FunHome(){
     <>
         <h1 className="title1">have fun page</h1>
 
+        <p className="Content">Un apartado donde puedes divertirte un poco con las aplicaciones
+        que ofrecen diferentes interacciones e informacion de que te puede agradar</p>
+
         <div>
-            <h1 className="Content">gifApp</h1>
+            <h1 className="title1">gifApp</h1>
+            <p className="Content">Una aplicacion para buscar gifs que te gusten
+            solo ingresa las palabras busca tus gifs favoritos</p>
             <a href="havefun/gifapp">visitar</a>
+        </div>
+
+        <div>
+            <h1 className="title1">rickosoApp</h1>
+            <p className="Content">Relajate un rato y ponte rickoso mientras buscas informacion
+            sobre tus personajes favoritos de rick y morty</p>
+            <a href="havefun/rickosoapp">visitar</a>
         </div>
     </>
     );
