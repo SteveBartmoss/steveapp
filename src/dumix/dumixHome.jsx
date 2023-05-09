e9c39f89c17431d02aa1c9fc8dfdb2f9bdfb25c3
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-05-08 22:22:29 -0600

    add new topic in the home page

--- src/dumix/dumixHome.jsx
+++ src/dumix/dumixHome.jsx
@@ -39,6 +39,14 @@ function DumixHome(){
                     consola de linux</p>
                     <a href="dumix/terminalapps">Ir al post</a>
                 </div>
+
+                <div>
+                    <h1 className="title1">Vimgg una guia para mortales</h1>
+                    <p className="Content">Una recopilacion de las mejores aplicaciones que se pueden usar 
+                    en la terminal y que resultan bastante utiles cuando se esta interactuando con la 
+                    consola de linux</p>
+                    <a href="dumix/vimgg">Ir al post</a>
+                </div>
                 
             </div>
             <DumixAbout />
