31a13f0544629b17185e91b4fb3b2e578bbe6287
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-05-11 20:24:18 -0600

    add new elements for the style of the page and new classname

--- src/dumix/dumixHome.jsx
+++ src/dumix/dumixHome.jsx
@@ -5,23 +5,33 @@ function DumixHome(){
     return(
         <>
             <div className="back-dumix"></div>
-            <div>
-                <di className="center">
-                    <h1 className="title1">Contenido principal</h1>
-                </di>
+            
+            <div className="center">
+                <h1 className="title1">Contenido principal</h1>
+            </div>
+
+            <div className="display-topics">
                 
-                <div>
-                    <h1 className="title1">Primeros pasos</h1>
+                <div className="div-topic">
+
+                    <div className="center">
+                        <h1 className="title1">Primeros pasos</h1>
+                    </div>
+                    
                     <p className="Content">En esta seccion se abordaran temas para personas
                     que no han interactuado con linux, se describira lo que es una terminal
                     de comandos, cuales son las partes que componen la terminal, comandos basicos
                     para crear un archovo, como ver los permisos, como listar los elementos de un 
                     directorio, como navegae entre direcctorios. Esta parte busca dar una introduccion
                     a los usuarios que nunca han utilizado comandos</p>
-                    <a className="dumix-btn" href="dumix/linux1">Ir al curso</a>
+
+                    <div className="center">
+                        <a className="dumix-btn" href="dumix/linux1">Ir al curso</a>
+                    </div>
+                    
                 </div>
 
-                <div>
+                <div className="div-topic">
                     <h1 className="title1">Pimp my terminal</h1>
                     <p className="Content">Un poco de estilo a nadie le queda mal y si vas pasar gran
                     parte de tu tiempo interactuando con la terminal de comando es buena idea que puedas
@@ -31,7 +41,7 @@ function DumixHome(){
                     <a className="dumix-btn" href="dumix/pimp">Ir al post</a>
                 </div>
 
-                <div>
+                <div className="div-topic">
                     <h1 className="title1">Mejores aplicaciones para terminal</h1>
                     <p className="Content">Una recopilacion de las mejores aplicaciones que se pueden usar 
                     en la terminal y que resultan bastante utiles cuando se esta interactuando con la 
@@ -39,7 +49,7 @@ function DumixHome(){
                     <a className="dumix-btn" href="dumix/terminalapps">Ir al post</a>
                 </div>
 
-                <div>
+                <div className="div-topic">
                     <h1 className="title1">Vimgg una guia para mortales</h1>
                     <p className="Content">Empezar a utilizar vim no es facil pero en esta seccion se encuentran
                     tutoriales o consejos que pueden hacer mas facil el cambio a este tipo de aplicaciones
