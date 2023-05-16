551b6b53cd515eaff26b67f629e310367572ab22
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-05-15 21:43:34 -0600

    add some div and some information for the subject, is in progres now

--- src/dumix/Linux1/pages/Directorios.jsx
+++ src/dumix/Linux1/pages/Directorios.jsx
@@ -2,7 +2,17 @@ import React from "react";
 
 function Directorio(){
     return(
-        <h1 className="title1">Directorios</h1>
+        <di className="div-curso">
+            <h1 className="title1">Directorios</h1>
+
+            <p className="Content">Los directiorios en linux son como las carpetas
+            en widows solo que se cambia la forma en la que se llaman pero a niveles
+            practicos cumplen con la misma funcion y no tienen diferencia en su uso 
+            convencional</p>
+
+            <h1 className="title1">Estructura de los direcctorio de linux</h1>
+        </di>
+        
     );
 }
 
