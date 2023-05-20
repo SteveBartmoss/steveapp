fa208e5cf626f82c98cdebe3a2d75701614912d0
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-05-19 22:04:44 -0600

    the page is in progres now, add new informations ande new clasname for the style

--- src/dumix/pages/vimgg.jsx
+++ src/dumix/pages/vimgg.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 function Vimgg(){
-    return(<h1 className="title1">Vimgg page</h1>);
+    return(
+        <div className="div-vimgg">
+            <h1 className="title1">Vimgg</h1>
+            <h1 className="title1">Una guia para los mortales como steve</h1>
+
+            <p  className="Content">Empezar a utilizar vim no es un camino facil
+            pero aprender nuevas cosas resulta interesante y ademas te puede dar
+            una nueva herramienta para futuros proyectos o simplemente para poder
+            variar el entorno de trabajo y poder elegir de otras opciones</p>
+
+            <h1 className="title1">¿Porque aprender vim?</h1>
+
+            <h1 className="title1">¿Que es vim?</h1>
+        </div>
+    );
 }
 
 export default Vimgg;
