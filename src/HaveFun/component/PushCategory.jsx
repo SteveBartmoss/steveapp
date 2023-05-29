372eb344ede9b41acf993fa506e34e4c4773b44a
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-05-28 21:07:04 -0600

    add new code for the component pushcategory

--- src/HaveFun/component/PushCategory.jsx
+++ src/HaveFun/component/PushCategory.jsx
@@ -1,8 +1,31 @@
 import React, { useState } from "react";
 
-function PushCategory(){
+function PushCategory({onNewCategory}){
 
     const [inputValue, setInputValue]=useState();
 
-    
+    const onInputChange=(event)=>{
+        setInputValue(event.target.value);
+    }
+
+    const onSumit=(event)=>{
+        event.preventDefault();
+        if(inputValue.trim().lenght<=1) return;
+        onNewCategory(inputValue.trim());
+        setInputValue('');
+    }
+
+    return(
+        <form onSubmit={(event)=>onSumit(event)}>
+            <input 
+            type="text" 
+            placeholder="buscar un gif" 
+            value={inputValue}
+            onChange={(event)=>onInputChange(event)}
+            />
+        </form>
+    );
+
 }
+
+export default PushCategory;
