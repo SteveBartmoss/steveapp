a09824dff5f1612ff90cd11c4f77a26cd5d87ac5
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-05-28 21:04:01 -0600

    add new prop for the component for a test

--- src/HaveFun/component/AddCategory.jsx
+++ src/HaveFun/component/AddCategory.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function AddCategory({newCategorie}){
+function AddCategory({newCategorie,elements}){
 
     const [inputValue, setInputValue] = useState(''); 
 
