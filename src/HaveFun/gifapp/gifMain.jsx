5a610725665f523637a37dbcaea42ad39d07401e
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-05-29 20:40:11 -0600

    coment import of the addcategory because is never used, implements validation for the elements in the categories

--- src/HaveFun/gifapp/gifMain.jsx
+++ src/HaveFun/gifapp/gifMain.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
-import AddCategory from "../component/AddCategory";
+//import AddCategory from "../component/AddCategory";
 import PushCategory from "../component/PushCategory";
 
 function GifApp(){
 
-    const [categories, setCategories] = useState([['hack']]);
+    const [categories, setCategories] = useState(['hack']);
 
     const onAddCategory = (newCategory) =>{
+
+        //se valida que el elemento a insertar no exista en el arreglo
+        if (categories.includes(newCategory) ) return;
         //se crea un nuevo arreglo y al final se le agrega la nueva categoria
         setCategories([newCategory,...categories]);
         // segunda opcion en la segunda opcion se utilizan callbacks
