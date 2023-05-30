9e90f336970a311b78071d638471638c81b50b7b
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-05-29 20:55:07 -0600

    create new component for contains and orde the gifs in the future implementation

---
+++ src/HaveFun/component/GifGrid.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function GifGrid({value}){
+    
+    return(
+        <>
+            <h1 className="title1">{value}</h1>
+            <p  className="Content">hi</p>
+        </>
+    );
+}
+
+export default GifGrid;
