ffd3bf52797b7ab88256f9222567cd6fa420551c
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-06-01 19:54:54 -0600

    add new funtion for call the api giphy, now thats brignme the id, title and img of the value in the component gifgrid

--- src/HaveFun/component/GifGrid.jsx
+++ src/HaveFun/component/GifGrid.jsx
@@ -2,6 +2,26 @@ import React from "react";
 
 function GifGrid({value}){
     
+    const getGifs=async()=>{
+        const url=`https://api.giphy.com/v1/gifs/search?api_key=EgCmS5NAfDFkbjdQePCgH4s4UUlYJ1l8&q=${value}&limit=10`;
+
+        const resp=await fetch(url);
+
+        const {data}=await resp.json();
+
+        const gifs=data.map(img=>{
+            return{
+                id: img.id, 
+                title: img.title,
+                url: img.images.downsized_medium.url
+            }
+        })
+        
+        console.log(gifs);
+    }
+
+    getGifs();
+
     return(
         <>
             <h1 className="title1">{value}</h1>
