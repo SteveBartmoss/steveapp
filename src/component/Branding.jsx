7eec97da4c774c9b15114560ee03488e12a340aa
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-06-02 22:00:46 -0600

    add new comonent for the brands discord and canal, is reusable now

---
+++ src/component/Branding.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../App.css';
+
+function Branding({name, title, url}){
+    return(
+        <section className={name}>
+            <div className="BrandContenido">
+                <h2>{title}</h2>
+                <a href={url} target="_blank" className="Btn">Ir al canal</a>
+            </div>
+        </section>
+    );
+}
+
+export default Branding;
