e087cdbd18ccfab5b884704bd9f1b11aa62bd51a
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-06-02 21:59:59 -0600

    add new componet to reduce components discord and canal, the new component implements discord and canal

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,7 @@ import Review from "../component/Review";
 import Canal from "../component/Canal";
 import Footer from "../component/Pie";
 import Pie from "../component/Pie";
+import Branding from "../component/Branding";
 
 
 
@@ -18,13 +19,19 @@ function Home(){
             <h3 className="title1">Videos con mas visitas</h3>   
         </div>
         <Videos/>
-        <Discord/>
+        <Branding name={"Discord"} 
+        title={"Un servidor de discord con temas de videojuegos e informatica"}
+        url={"https://discord.gg/QTdVBq5Q9r"}
+        />
         
         <div className="Contenedor">
             <h3 className="title1">Reviews mas recientes</h3>
         </div>
         <Review/>
-        <Canal/>
+        <Branding name={"Canal"}
+        title={"Un canal dedicado a los temas de informatica, videojuegos y techrunners"}
+        url={"https://www.youtube.com/channel/UCsaZsVCeb3BkqBrROgc1uhw"}
+        />
         <Pie/>
         </>
     );
