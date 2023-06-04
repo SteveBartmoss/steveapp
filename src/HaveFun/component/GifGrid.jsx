a93ca8bc292cc155b14322308598900b324af7bb
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-06-04 16:36:04 -0600

    cleaned the code, remove a function get gitfs for more clean code

--- src/HaveFun/component/GifGrid.jsx
+++ src/HaveFun/component/GifGrid.jsx
@@ -1,24 +1,7 @@
 import React from "react";
-
-const getGifs=async(prop)=>{
-    const url=`https://api.giphy.com/v1/gifs/search?api_key=EgCmS5NAfDFkbjdQePCgH4s4UUlYJ1l8&q=${prop}&limit=10`;
-    const resp=await fetch(url);
-    const {data}=await resp.json();
-
-    const gifs=data.map(img=>{
-        return{
-            id: img.id, 
-            title: img.title,
-            url: img.images.downsized_medium.url
-        }
-    })
-    
-    console.log(gifs);
-}
+import { getGifs } from "../helpers/getGifs";
 
 function GifGrid({value}){
-    
-    
 
     getGifs(value);
 
