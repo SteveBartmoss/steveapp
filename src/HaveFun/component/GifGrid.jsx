267b905b8b99306a193428c27ad3916308f0cf71
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-06-04 17:49:53 -0600

    add new function for set the data of the api call

--- src/HaveFun/component/GifGrid.jsx
+++ src/HaveFun/component/GifGrid.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 function GifGrid({value}){
 
+    const [images, setImages]=useState([]);
+
+    const getImages=async()=>{
+        const newImages=await getGifs(value);
+        setImages(newImages);
+    }
+
     useEffect(()=>{
-        getGifs(value); 
+        getImages(); 
     },[]);
 
     return(
         <>
             <h1 className="title1">{value}</h1>
-            <p  className="Content">hi</p>
         </>
     );
 }
