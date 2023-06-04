9484c87c81f7fb05c8954b60487e774310b330dd
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-06-04 16:37:13 -0600

    add new folfer for the helpers, contains the functuion getgifs

---
+++ src/HaveFun/helpers/getGifs.js
@@ -0,0 +1,17 @@
+export const getGifs=async(prop)=>{
+    
+    const url=`https://api.giphy.com/v1/gifs/search?api_key=EgCmS5NAfDFkbjdQePCgH4s4UUlYJ1l8&q=${prop}&limit=10`;
+    const resp=await fetch(url);
+    const {data}=await resp.json();
+
+    const gifs=data.map(img=>{
+        return{
+            id: img.id, 
+            title: img.title,
+            url: img.images.downsized_medium.url
+        }
+    })
+    
+    console.log(gifs);
+    return gifs;
+}
