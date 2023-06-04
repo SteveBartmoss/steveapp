bf37e659b327a5892b07759e83a42caf09f27955
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-06-04 15:10:14 -0600

    change component for the implementation of the function getgifs

--- src/HaveFun/component/GifGrid.jsx
+++ src/HaveFun/component/GifGrid.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 
+const getGifs=async(prop)=>{
+    const url=`https://api.giphy.com/v1/gifs/search?api_key=EgCmS5NAfDFkbjdQePCgH4s4UUlYJ1l8&q=${prop}&limit=10`;
+    const resp=await fetch(url);
+    const {data}=await resp.json();
+
+    const gifs=data.map(img=>{
+        return{
+            id: img.id, 
+            title: img.title,
+            url: img.images.downsized_medium.url
+        }
+    })
+    
+    console.log(gifs);
+}
+
 function GifGrid({value}){
     
-    const getGifs=async()=>{
-        const url=`https://api.giphy.com/v1/gifs/search?api_key=EgCmS5NAfDFkbjdQePCgH4s4UUlYJ1l8&q=${value}&limit=10`;
+    
 
-        const resp=await fetch(url);
-
-        const {data}=await resp.json();
-
-        const gifs=data.map(img=>{
-            return{
-                id: img.id, 
-                title: img.title,
-                url: img.images.downsized_medium.url
-            }
-        })
-        
-        console.log(gifs);
-    }
-
-    getGifs();
+    getGifs(value);
 
     return(
         <>
