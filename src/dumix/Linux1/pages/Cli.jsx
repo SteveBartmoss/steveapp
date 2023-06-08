da63ab2424003152c1a252661dde6b912037a323
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-06-07 22:24:17 -0600

    change the classname in the tag p for new style in the paragraphs

--- src/dumix/Linux1/pages/Cli.jsx
+++ src/dumix/Linux1/pages/Cli.jsx
@@ -18,7 +18,7 @@ function Cli(){
                 <p className="promp">steve@steve-HP-Notebook <span className="Content">:</span><span className="directorio">~</span><span className="Content">$</span></p>
             </div>
 
-            <p className="Content">Usar una terminal puede parecer dificil pero no es para tanto solo se trata de
+            <p className="content-curso">Usar una terminal puede parecer dificil pero no es para tanto solo se trata de
             escribir intrucciones para la computadora lo cual era bastante comun con las primeras computadoras en
             las cuales no existia una interfaz de usuario y todo era dirigido por la linea de comandos, esto hacia
             que manejar una computadora fuera una tarea mas compleja para los usuarios que no estaban muy acostumbrados
@@ -31,7 +31,7 @@ function Cli(){
             </div>
             
 
-            <p className="Content">Si bien el uso de una terminal dejo de ser popular y actualmente solo se usa para
+            <p className="content-curso">Si bien el uso de una terminal dejo de ser popular y actualmente solo se usa para
             intalar programas avanzados o para revisar la ip de un equipo aprender a usarla puede ser de gran ayuda
             sobre todo en el ambito de redes y de servidores en el cual usar una terminal es muy comun, esto se debe 
             a que un sistema menajdo por comandos es muy ligero en cuestion de recursos logrando un mejor desempeño 
@@ -42,7 +42,7 @@ function Cli(){
             un comando.
             </p>
 
-            <p className="Content">Para empezar a usar una terminal es tan cencillo como usar el comando cntrl+alt+t 
+            <p className="content-curso">Para empezar a usar una terminal es tan cencillo como usar el comando cntrl+alt+t 
             si estas en un sistema de linux y esto abria una nueva terminal de comandos ya sea usando bash o con zsh 
             que es la terminal que se suale utilizar un sistema unix o mac, en caso de que el comando no funcion en la
             distribucion que tiene instalada basta con buscar en entre las aplicaciones la que diga terminal en el caso
@@ -61,18 +61,18 @@ function Cli(){
                 </div>
             </div>
 
-            <p className="Content"> si logra ver una pantalla similar a la representacion que se muestra en la parte superior
+            <p className="content-curso"> si logra ver una pantalla similar a la representacion que se muestra en la parte superior
             entonces logro abrir con exito nuna nueva terminal
             </p>
 
             <h1 className="title1">El promp de la terminal</h1>
 
-            <p className="Content">Al abrir la terminal notaras que hay unas palabras escritas bueno eso
+            <p className="content-curso">Al abrir la terminal notaras que hay unas palabras escritas bueno eso
             es lo que normalmente llamamos promp que es los que marca la posicion del cursor en la pantalla 
             y aporta informacion como el mobre del usuario y el hots, el directorion el que no es encontramos
             y alguna otra informacion que puede variar.</p>
 
-            <p className="Content">Por lo general la estructura de un propmp sige estos parametros </p>
+            <p className="content-curso">Por lo general la estructura de un propmp sige estos parametros </p>
 
             <div className="center">
                 <div className="div-code">
@@ -80,7 +80,7 @@ function Cli(){
                 </div>
             </div>
 
-           <p className="Content">El usuario hace referencia a la sesion que se inicio, el host es el
+           <p className="content-curso">El usuario hace referencia a la sesion que se inicio, el host es el
            nombre que el sistema le asigno al equipo, el direcotrio es la informacion que puede
            resultar mas util y en el ejemplo el elemento que indica el directorio actual es ~ que hace
            referencia a la direccion /home/steve que es la direccion de la carpeta personas del usuario. 
