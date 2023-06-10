02e9a5f5217e96608ca7634f571bdff41955f14d
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-06-09 21:30:23 -0600

    add new information for the subeject archivos, is in progres now

--- src/dumix/Linux1/pages/Archivos.jsx
+++ src/dumix/Linux1/pages/Archivos.jsx
@@ -4,6 +4,14 @@ function Archivos(){
     return(
         <div className="div-curso">
             <h1 className="title1">Archivos</h1>
+
+            <hr className="line"/>
+
+            <p className="content-curso">Los archivos son un elemento importante 
+            en la mayoria de sistemas operativos ya que estos pueden tener programas, 
+            informacion, o cumplen una funcion en el sistema operativo (en realidad 
+            todo el sistema operativo esta completamente compuesto por archivos) 
+            por esta razon es importante conocer el manejo de los mismos.</p>
         </div>
     );
 }
