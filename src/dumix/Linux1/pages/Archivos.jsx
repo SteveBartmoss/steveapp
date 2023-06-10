ac1e646ac079d252d392f8337721592ebba67eb9
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-06-10 13:57:50 -0600

    add new example in the subject archivos

--- src/dumix/Linux1/pages/Archivos.jsx
+++ src/dumix/Linux1/pages/Archivos.jsx
@@ -19,6 +19,13 @@ function Archivos(){
             listar los archivos o el contenido de un directorio, el comando es 
             bastante sencillo y ya se ha visto en temas anteriores y es el ls 
             a continuacion se muestra su sintaxis.</p>
+
+            <div className="center">
+                <div className="div-code">
+                    <p className="code-simple">ls -[opciones] [archivo]</p>
+                </div>
+            </div>
+
         </div>
     );
 }
