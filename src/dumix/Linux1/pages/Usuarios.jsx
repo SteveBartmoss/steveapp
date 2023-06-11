fb4e2d2295b47f111e99355423211c6048327a15
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-06-11 16:27:24 -0600

    add new div for the style of the curso because generete build of producction

--- src/dumix/Linux1/pages/Usuarios.jsx
+++ src/dumix/Linux1/pages/Usuarios.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Usuarios(){
-    return(<h1 className="title1">Usuarios</h1>);
+    return(
+        <div className="div-curso">
+            <h1 className="title1">Usuarios</h1>
+        </div>
+    );
 }
 
 export default Usuarios;
