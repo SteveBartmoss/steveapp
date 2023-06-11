363802aa4fac5841ad43d0b13dc3bb9cf78719b6
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-06-11 16:43:38 -0600

    add new div for the style of the havefun home

--- src/HaveFun/FunHome.jsx
+++ src/HaveFun/FunHome.jsx
@@ -1,42 +1,48 @@
 import React from "react";
 import DivBaner from "../component/DivBaner";
 
-function FunHome(){
-    return(
-    <>
-        <DivBaner name="baner-have-fun" />
+function FunHome() {
+    return (
+        <>
+            <DivBaner name="baner-have-fun" />
 
-        <p className="Content">Un apartado donde puedes divertirte un poco con las aplicaciones
-        que ofrecen diferentes interacciones e informacion de que te puede agradar</p>
+            <div className="div-havefun">
 
-        <div>
-            <h1 className="title1">gifApp</h1>
-            <p className="Content">Una aplicacion para buscar gifs que te gusten
-            solo ingresa las palabras busca tus gifs favoritos</p>
-            <a href="havefun/gifapp">visitar</a>
-        </div>
+                <p className="p-havefun">Un apartado donde puedes divertirte un poco con las aplicaciones
+                    que ofrecen diferentes interacciones e informacion de que te puede agradar</p>
 
-        <div>
-            <h1 className="title1">rickosoApp</h1>
-            <p className="Content">Relajate un rato y ponte rickoso mientras buscas informacion
-            sobre tus personajes favoritos de rick y morty</p>
-            <a href="havefun/rickosoapp">visitar</a>
-        </div>
+                <div>
+                    <h1 className="title1">gifApp</h1>
+                    <p className="Content">Una aplicacion para buscar gifs que te gusten
+                        solo ingresa las palabras busca tus gifs favoritos</p>
+                    <a href="havefun/gifapp">visitar</a>
+                </div>
 
-        <div>
-            <h1 className="title1">AssembleApp</h1>
-            <p className="Content">Encuentra informacion sobre tus super heroes favoritos y diviertete
-            por unos momentos con esta aplicacions de super heroes</p>
-            <a href="havefun/marvelapp">visitar</a>
-        </div>
+                <div>
+                    <h1 className="title1">rickosoApp</h1>
+                    <p className="Content">Relajate un rato y ponte rickoso mientras buscas informacion
+                        sobre tus personajes favoritos de rick y morty</p>
+                    <a href="havefun/rickosoapp">visitar</a>
+                </div>
 
-        <div>
-            <h1 className="title1">AtrapalosApp</h1>
-            <p className="Content">Busca la informacion de tu pokemon favorito en esta aplicacion que 
-            te brinda la informacion de la mayoria de los pokemon gracias a la pokeapi</p>
-            <a href="havefun/pokeapp">visitar</a>
-        </div>
-    </>
+                <div>
+                    <h1 className="title1">AssembleApp</h1>
+                    <p className="Content">Encuentra informacion sobre tus super heroes favoritos y diviertete
+                        por unos momentos con esta aplicacions de super heroes</p>
+                    <a href="havefun/marvelapp">visitar</a>
+                </div>
+
+                <div>
+                    <h1 className="title1">AtrapalosApp</h1>
+                    <p className="Content">Busca la informacion de tu pokemon favorito en esta aplicacion que
+                        te brinda la informacion de la mayoria de los pokemon gracias a la pokeapi</p>
+                    <a href="havefun/pokeapp">visitar</a>
+                </div>
+
+            </div>
+
+
+        </>
     );
 }
 
