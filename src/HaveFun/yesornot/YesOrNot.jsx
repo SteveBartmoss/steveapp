cb23a944d4b95782f39a07cac2b326fee3515b83
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-06-11 21:46:13 -0600

    add new page for the new aplication yes or not app

---
+++ src/HaveFun/yesornot/YesOrNot.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function YesOrNot(){
+    return(
+        <div>
+            <h1 className="title1">Yes or Not</h1>
+        </div>
+    );
+}
+
+export default YesOrNot;
