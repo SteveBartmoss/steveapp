ce2cb6a29125926547da5d36fefbec181ea38ae6
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-06-11 20:54:41 -0600

    add classname for the styeles, add new div for styles, add a footer for the page

--- src/HaveFun/FunHome.jsx
+++ src/HaveFun/FunHome.jsx
@@ -11,28 +11,30 @@ function FunHome() {
                 <p className="p-havefun">Un apartado donde puedes divertirte un poco con las aplicaciones
                     que ofrecen diferentes interacciones e informacion de que te puede agradar</p>
 
-                <div>
-                    <h1 className="title1">gifApp</h1>
-                    <p className="Content">Una aplicacion para buscar gifs que te gusten
-                        solo ingresa las palabras busca tus gifs favoritos</p>
-                    <a href="havefun/gifapp">visitar</a>
+                <div className="card-havefun">
+                    <h1 className="h1-havefun">gifApp</h1>
+                    <p className="p-card">Una aplicacion para buscar gifs que te gusten
+                    solo ingresa las palabras busca tus gifs favoritos</p>
+                    <div className="button-havefun">
+                        <a className="a-card" href="havefun/gifapp">visitar</a>
+                    </div>
                 </div>
 
-                <div>
+                <div className="card-havefun">
                     <h1 className="title1">rickosoApp</h1>
                     <p className="Content">Relajate un rato y ponte rickoso mientras buscas informacion
                         sobre tus personajes favoritos de rick y morty</p>
                     <a href="havefun/rickosoapp">visitar</a>
                 </div>
 
-                <div>
+                <div className="card-havefun">
                     <h1 className="title1">AssembleApp</h1>
                     <p className="Content">Encuentra informacion sobre tus super heroes favoritos y diviertete
                         por unos momentos con esta aplicacions de super heroes</p>
                     <a href="havefun/marvelapp">visitar</a>
                 </div>
 
-                <div>
+                <div className="card-havefun">
                     <h1 className="title1">AtrapalosApp</h1>
                     <p className="Content">Busca la informacion de tu pokemon favorito en esta aplicacion que
                         te brinda la informacion de la mayoria de los pokemon gracias a la pokeapi</p>
@@ -41,6 +43,9 @@ function FunHome() {
 
             </div>
 
+            <div>
+               <h1 className="h1-havefun">Footer have fun</h1> 
+            </div>
 
         </>
     );
