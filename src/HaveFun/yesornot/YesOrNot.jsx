470b6d01cbfa5da964e754e5191c8bc147683516
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-06-19 20:03:32 -0600

    add new element sendquestion for implements a question for the oracle

--- src/HaveFun/yesornot/YesOrNot.jsx
+++ src/HaveFun/yesornot/YesOrNot.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import SendQuestion from "../component/SendQuestion";
 
 function YesOrNot(){
+
+    const [question, setQuestion]=useState('');
+
+    const onAddQuestion=(question)=>{
+        setQuestion(question)
+        console.log(question);
+    }
     return(
         <div>
-            <h1 className="title1">Yes or Not</h1>
+            <div>
+                <h1 className="title1">Yes or Not</h1>
+                <SendQuestion onNewQuestion={event=>onAddQuestion(event)} />
+
+            </div>
         </div>
     );
 }
