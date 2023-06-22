b2a830f9a2ae67c6530128c57eb8292e7b328233
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-06-21 21:43:31 -0600

    add new code for the consume of the api yesornot

--- src/HaveFun/yesornot/YesOrNot.jsx
+++ src/HaveFun/yesornot/YesOrNot.jsx
@@ -1,15 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SendQuestion from "../component/SendQuestion";
 import FrameAnswer from "../component/FrameAnswer";
+import { getOracle } from "../helpers/getOracle";
 
 function YesOrNot(){
 
     const [question, setQuestion]=useState('');
+    const [answer, setAnswer]=useState({
+        answer: '',
+        forced: '',
+        url: ''
+    });
 
     const onAddQuestion=(question)=>{
         setQuestion(question)
-        console.log(question);
+        //console.log(question);
     }
+
+    const getAnswer=async()=>{
+        const newAnswer=await getOracle();
+        //console.log(newAnswer);
+        setAnswer({...newAnswer, answer: newAnswer.answer});
+        console.log(answer);
+    }
+
+    useEffect(()=>{
+        getAnswer();
+    },[])
     return(
         <div>
             <div>
