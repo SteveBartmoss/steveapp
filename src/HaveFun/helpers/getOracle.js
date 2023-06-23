92413ab678be794ce41531d50cce3b8b8f876e32
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-06-22 21:38:39 -0600

    remove consolelog for the test of aplication, cleaning the code

--- src/HaveFun/helpers/getOracle.js
+++ src/HaveFun/helpers/getOracle.js
@@ -2,7 +2,6 @@ export const getOracle=async()=>{
     const url="https://yesno.wtf/api";
     const resp=await fetch(url);
     const data=await resp.json();
-    //console.log(data);
 
     return data;
 }
