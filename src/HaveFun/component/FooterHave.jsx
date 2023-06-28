6d56e7fdbe5f8f038e16157de02c77184ca000dc
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-06-27 21:43:39 -0600

    add new component footer, remove a old code for the footer

---
+++ src/HaveFun/component/FooterHave.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "../../App.css";
+
+function FooterHave() {
+
+    return (
+        <div className="gif-footer">
+            <div className="footer-item">
+                <h1 className="title1">Importante</h1>
+                <p className="Content">Esta seccion fue creada gracias al curso de
+                    react de Fernando Herrera, si quieres apoyarlo puedes comprar su
+                    curso o recomendarlo.
+                </p>
+
+                <p className="Content">Esta pagina forma parte de mi portafolio, si
+                    te interesa puedes contactarme en algunas de las siguientes redes.</p>
+
+                <p className="Content">Tambien puedes pasar a mi canal de Youtube
+                    quiza encuentras contenido que te agrade y me agregas a tus canala
+                    preferidos</p>
+            </div>
+            <div className="item-grid">
+                <h1 className="title1">Contacto</h1>
+                <p className="Content">stevechambitas@gmai.com</p>
+
+                <h1 className="title1">Githud</h1>
+                <h1 className="title1">Youtube</h1>
+                <h1 className="title1">Power by Giphy</h1>
+            </div>
+        </div>
+    );
+}
+
+export default FooterHave;
