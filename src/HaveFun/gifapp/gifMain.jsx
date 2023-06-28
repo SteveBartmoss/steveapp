b31d1413ae053fe3bd67b45d6cd0869e9c07970f
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-06-27 21:43:09 -0600

    add new component footer, remove a old code for the footer

--- src/HaveFun/gifapp/gifMain.jsx
+++ src/HaveFun/gifapp/gifMain.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import PushCategory from "../component/PushCategory";
 import GifGrid from "../component/GifGrid";
 import "../../App.css";
+import FooterHave from "../component/FooterHave";
 
 function GifApp(){
 
@@ -35,30 +36,8 @@ function GifApp(){
                 })}
             </div>
 
-            <div className="gif-footer">
-                <div className="footer-item">
-                    <h1 className="title1">Importante</h1>
-                    <p className="Content">Esta seccion fue creada gracias al curso de 
-                    react de Fernando Herrera, si quieres apoyarlo puedes comprar su 
-                    curso o recomendarlo.
-                    </p>
-
-                    <p className="Content">Esta pagina forma parte de mi portafolio, si 
-                    te interesa puedes contactarme en algunas de las siguientes redes.</p>
-
-                    <p className="Content">Tambien puedes pasar a mi canal de Youtube 
-                    quiza encuentras contenido que te agrade y me agregas a tus canala 
-                    preferidos</p>
-                </div>
-                <div className="item-grid">
-                    <h1 className="title1">Contacto</h1>
-                    <p className="Content">stevechambitas@gmai.com</p>
-
-                    <h1 className="title1">Githud</h1>
-                    <h1 className="title1">Youtube</h1>
-                    <h1 className="title1">Power by Giphy</h1>
-                </div>
-            </div> 
+            <FooterHave />
+            
         </div>
     );
 }
