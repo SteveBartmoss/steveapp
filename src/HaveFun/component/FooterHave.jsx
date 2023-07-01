031f5b7a0e30d62741f48b8b48def71864ed8339
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-06-30 22:02:15 -0600

    add new href for the anchortag giphy, and remove some informacion trahs

--- src/HaveFun/component/FooterHave.jsx
+++ src/HaveFun/component/FooterHave.jsx
@@ -21,14 +21,13 @@ function FooterHave() {
             </div>
             <div className="item-grid">
                 <h1 className="title1">Contacto</h1>
-                <p className="Content">stevechambitas@gmai.com</p>
                 <a>mail</a>
                 <h1 className="title1">Githud</h1>
                 <a href="https://github.com/MarcoAntonioGuadalupeRivasGuzman">Githud</a>
                 <h1 className="title1">Youtube</h1>
                 <a href="https://www.youtube.com/@stevechambitas2782">Youtube</a>
                 <h1 className="title1">Power by Giphy</h1>
-                <a>Giphy</a>
+                <a href="https://giphy.com/">Giphy</a>
             </div>
         </div>
     );
