933385d2779cccd44a7381201741b27479b60ae2
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-07-01 20:48:13 -0600

    correct some errors in the styles, and change some elements

--- src/HaveFun/component/FooterHave.jsx
+++ src/HaveFun/component/FooterHave.jsx
@@ -21,11 +21,10 @@ function FooterHave() {
             </div>
             <div className="item-grid">
                 <h1 className="title1">Contacto</h1>
-                <p className="Content">Enviame un <a className="contacto" href="mailto:stevechanvitas@gmail.com">Correo</a></p>
+                <p className="contacto">Enviame un <a className="contacto" href="mailto:stevechanvitas@gmail.com">Correo</a></p>
                 <a className="contacto" href="https://github.com/MarcoAntonioGuadalupeRivasGuzman">Githud</a>
                 <a className="contacto" href="https://www.youtube.com/@stevechambitas2782">Youtube</a>
-                <h1 className="title1">Power by Giphy</h1>
-                <a className="contacto" href="https://giphy.com/">Giphy</a>
+                <h1 className="title1">Power by <a className="contacto" href="https://giphy.com/">Giphy</a></h1>
             </div>
         </div>
     );
