7b00879f8ead13c788004a029257e41284681897
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-07-03 21:34:54 -0600

    add new state for render or not a answer from the oracle

--- src/HaveFun/yesornot/YesOrNot.jsx
+++ src/HaveFun/yesornot/YesOrNot.jsx
@@ -6,6 +6,9 @@ import FooterHave from "../component/FooterHave";
 function YesOrNot(){
 
     const [question, setQuestion]=useState('');
+
+    const [isQuestion, setIsQuestion]=useState(true);
+
     const [answer, setAnswer]=useState({
         res: '',
         forced: '',
@@ -22,6 +25,7 @@ function YesOrNot(){
         const newAnswer=await getOracle();
         //console.log(newAnswer);
         setAnswer(prevState=>({...prevState,res: newAnswer.answer, forced: newAnswer.forced, url: newAnswer.image }));
+        setIsQuestion(false);
         //console.log(answer);
     }
 
@@ -30,10 +34,12 @@ function YesOrNot(){
             <div className="gif-div">
                 <h1 className="gif-title1">Yes or Not</h1>
                 <SendQuestion onNewQuestion={event=>onAddQuestion(event)} />
+                { isQuestion ? (<h1 className="gif-title1">Ask me someting :D</h1>): null
+                }
                 <h1 className="gif-title1">{question}</h1>
                 <h1 className="gif-title1">{answer.res}</h1>
                 <div className="div-answer">
-                    <img className="oracle-answer" src={answer.url}/>
+                    <img className="oracle-answer" src={answer.url} alt="Ask me Question"/>
                 </div>
             </div>
 
