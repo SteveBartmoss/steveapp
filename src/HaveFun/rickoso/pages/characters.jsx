259f434fa67f42eb9ead5012c95085431d2a3d8f
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-07-09 19:10:45 -0600

    add a new forder pages for the pages of the app, add a page for characters, add a page for episodes, add a page for locations

---
+++ src/HaveFun/rickoso/pages/characters.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Characters(){
+    return(
+        <h1>Characters Page</h1>
+    );
+}
+
+export default Characters;
---
+++ src/HaveFun/rickoso/pages/episodes.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Episodes(){
+    return(
+        <h1>Episodes page</h1>
+    );
+}
