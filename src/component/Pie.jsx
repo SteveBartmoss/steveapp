ef2d3814dc297c1d38b468f1c08f8795403f1f2b
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-07-10 21:42:55 -0600

    add a new information for the footer

--- src/component/Pie.jsx
+++ src/component/Pie.jsx
@@ -3,6 +3,10 @@ import React from "react";
 function Pie(){
     return(
         <div className="footer">
+            <div>
+                <p className="Content">Pagina creada por steve</p>
+                <p className="Content">Quieres aprender a programar? visita el enlace</p>
+            </div>
             <a className="footer-text" href="https://www.youtube.com/@stevechambitas2782" target="_blank">steve chambitas</a>
         </div>
     );
