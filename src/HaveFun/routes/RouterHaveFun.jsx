3f6100ebf0ebbebbdc2dde381f0df75f6990afe3
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-07-10 18:21:38 -0600

    add a new nested routes for the rickosoapp

--- src/HaveFun/routes/RouterHaveFun.jsx
+++ src/HaveFun/routes/RouterHaveFun.jsx
@@ -4,15 +4,31 @@ import GifApp from "../gifapp/gifMain";
 import RickosoApp from "../rickoso/rickosoMain";
 import MarvelApp from "../marvel/marvelMain";
 import YesOrNot from "../yesornot/YesOrNot";
+import RickosoNavbar from "../rickoso/routes/rickosoNavBar";
+import Characters from "../rickoso/pages/characters";
 
 function HaveFunRouter(){
     return(
         <>
             <Routes>
+
+                <Route path="rickosoapp" element={<RickosoNavbar/>}>
+                    <Route index element={<Characters/>} />
+                </Route>
+                
+                
+            </Routes>
+
+            <Routes>
                 <Route path="gifapp" element={<GifApp />}/>
-                <Route path="rickosoapp" element={<RickosoApp />}/>
-                <Route path="marvelapp" element={<MarvelApp />}/>
+            </Routes>
+
+            <Routes>
                 <Route path="yesornotapp" element={<YesOrNot />} />
+            </Routes>
+
+            <Routes>
+                <Route path="marvelapp" element={<MarvelApp />}/>
             </Routes>
         </>
     );
