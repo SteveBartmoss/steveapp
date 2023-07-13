2868708065da79a6e86abe812217ccae652674fc
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-07-12 22:05:33 -0600

    add a more information for the main page

--- src/stevelabs/homeLabs.jsx
+++ src/stevelabs/homeLabs.jsx
@@ -2,7 +2,19 @@ import React from "react";
 
 function HomeLabs(){
     return(
-        <h1 className="title1">Steve Labs</h1>
+        <>
+            <h1 className="title1">Steve Labs</h1>
+            <h1 className="title1">Comienza a aprender</h1>
+            <div>
+                <div>
+                    <h1 className="title1">Primeros pasos en programacion</h1>
+                    <p className="Content">En este curso se abordan temas basicos de programacion
+                     ya que esta orientado a personas que comienzan a programar por lo cual no 
+                     se esperan muchos conocimientos por parte de las personas que toman el curso</p>
+                </div>
+            </div>
+        </>
+        
     );
 }
 
