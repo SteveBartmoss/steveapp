3335d1238be2f1546d84515e6ca5906b8a080783
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-07-15 13:02:14 -0600

    add a classname display topics and div topics for the styles in the main page

--- src/stevelabs/homeLabs.jsx
+++ src/stevelabs/homeLabs.jsx
@@ -9,13 +9,15 @@ function HomeLabs(){
                 <h1 className="title1">Comienza a aprender</h1>
             </div>
 
-            <div>
-                <div>
+            <div className="display-topics">
+
+                <div className="div-topic">
                     <h1 className="title1">Primeros pasos en programacion</h1>
                     <p className="Content">En este curso se abordan temas basicos de programacion
                      ya que esta orientado a personas que comienzan a programar por lo cual no 
                      se esperan muchos conocimientos por parte de las personas que toman el curso</p>
                 </div>
+                
             </div>
         </>
         
