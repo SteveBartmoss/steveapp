2e196cdb1f1ef0acc9337aeb8f53cc5db7af02a4
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-07-18 18:33:56 -0600

    fix a error whit the footer im the stevelabs app

--- src/stevelabs/components/labsAbout.jsx
+++ src/stevelabs/components/labsAbout.jsx
@@ -35,23 +35,23 @@ function LabsAbout() {
                 plataformas porque es un sistemas muy eficiente y bastante estable
             </p>
 
-            <div className="gif-footer">
-                <div className="footer-item">
-                    <h1 className="title1">Importante</h1>
-                    <p className="Content">Esta seccion es creada con el fin de compartir informacion
-                        con otros usuarios o personas que quieren empezar en el mundo de linux pero
-                        no proviene de un maestro o de alguien que cuenta con un doctorado en ciencias 
-                        de la computacion o carreras a fin,</p>
-                </div>
-                <div className="item-grid">
-                    <h1 className="title1">Contacto</h1>
-                    <p className="contacto">Enviame un <a className="contacto" href="mailto:stevechanvitas@gmail.com">Correo</a></p>
-                    <a className="contacto" href="https://github.com/MarcoAntonioGuadalupeRivasGuzman">Githud</a>
-                    <a className="contacto" href="https://www.youtube.com/@stevechambitas2782">Youtube</a>
-                    <h1 className="title1">Power by <a className="contacto" href="https://giphy.com/">Giphy</a></h1>
-                </div>
-            </div>
+        </div>
 
+        <div className="gif-footer">
+            <div className="footer-item">
+                <h1 className="title1">Importante</h1>
+                <p className="Content">Esta seccion es creada con el fin de compartir informacion
+                    con otros usuarios o personas que quieren empezar en el mundo de linux pero
+                    no proviene de un maestro o de alguien que cuenta con un doctorado en ciencias
+                    de la computacion o carreras a fin,</p>
+            </div>
+            <div className="item-grid">
+                <h1 className="title1">Contacto</h1>
+                <p className="contacto">Enviame un <a className="contacto" href="mailto:stevechanvitas@gmail.com">Correo</a></p>
+                <a className="contacto" href="https://github.com/MarcoAntonioGuadalupeRivasGuzman">Githud</a>
+                <a className="contacto" href="https://www.youtube.com/@stevechambitas2782">Youtube</a>
+                <h1 className="title1">Power by <a className="contacto" href="https://giphy.com/">Giphy</a></h1>
+            </div>
         </div>
 
     </>
