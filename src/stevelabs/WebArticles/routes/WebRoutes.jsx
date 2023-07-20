03416b12a7bf25362fccecfb37c7d780ec9c5179
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-07-19 19:29:34 -0600

    chage the import of the component opinion1 because move the component to another folder

--- src/stevelabs/WebArticles/routes/WebRoutes.jsx
+++ src/stevelabs/WebArticles/routes/WebRoutes.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
-import Opinion1 from '../Web1/pages/Opinion1';
+import Opinion1 from '../Articles/Opinion1';
+
 
 
 const WebRoutes = () => {
   return (
     <div>
          <Routes>
-                <Route path="opinion1" element={<Opinion1 />} />
+                <Route path="opinion1" element={<Opinion1/>} />
          </Routes>
       
     </div>
