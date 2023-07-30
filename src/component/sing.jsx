ceb43de02f0d0e7392108bd0169858360f2d4fcc
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-07-30 16:31:08 -0600

    add a new component for the sing in articles

---
+++ src/component/sing.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const CreatorSing=()=>{
+    return(
+        <>
+            <div className="div-sing">
+                <img className="img-sing" src="/Assets/Enrique.jpg" />
+                <div className="content-sing">
+                    <p>Por <span className="autor">Enrique Dev</span> Ing en Sistemas</p>
+                    <p>08/06/23</p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default CreatorSing;
