0ed655e3198238904706d3f951bdb45e17b79f5b
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-07-31 20:30:58 -0600

    add a new class name for the styles of the footer in the main page

--- src/component/Pie.jsx
+++ src/component/Pie.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 
-function Pie(){
-    return(
-        <div className="footer">
-            <div>
-                <p className="Content">Pagina creada por steve</p>
-                <p className="Content">Quieres aprender a programar? visita el enlace</p>
-                <p className="Content">Preguntas frecuentes</p>
+function Pie() {
+    return (
+        <div className="gif-footer">
+            <div className="footer-item">
+                <h1 className="title1">Hola a todos</h1>
+                <p className="Content">Esta pagina fue creada como primer
+                    portafolio pero no por eso es menos importante ya que 
+                    recibira actualizaciones para hacerla mas interesante.
+                </p>
+
+                <p className="Content">La version actual seria 2.8.0 pero 
+                    se planea que siga evolucionando hasta volverse una 
+                    plataforma mas completa.
+                </p>
+
+                <p className="Content">Tambien puedes pasar a mi canal de Youtube
+                    quiza encuentras contenido que te agrade y me agregas a tus canales
+                    preferidos
+                </p>
                 <p className="Content">Sigue aprendiendo</p>
+                <p className="contacto">Quieres aprender a programar? visita el enlace</p>
             </div>
-            <a className="footer-text" href="https://www.youtube.com/@stevechambitas2782" target="_blank">steve chambitas</a>
+            <div className="item-grid">
+                <h1 className="title1">Contacto</h1>
+                <p className="contacto">Enviame un <a className="contacto" href="mailto:stevechanvitas@gmail.com">Correo</a></p>
+                <a className="contacto" href="https://github.com/MarcoAntonioGuadalupeRivasGuzman">Githud</a>
+                <a className="contacto" href="https://www.youtube.com/@stevechambitas2782">Youtube</a>
+                <p className="contacto">Preguntas frecuentes</p>
+            </div>
         </div>
     );
 }
