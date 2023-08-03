4995a7ef6caa3bc635afc43ef413446fb018e889
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-08-02 19:36:58 -0600

    add a new home page for the new secction

---
+++ src/Articles/articlesHome.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ArticlesHome(){
+    return(
+        <>
+            <h1 className="title1">Los mejores articulos</h1>
+        </>
+    );
+}
+
+export default ArticlesHome;
