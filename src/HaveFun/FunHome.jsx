900aed76ff0b90ef7c001ddb0d15e0cc369e078c
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-08-02 20:02:09 -0600

    add new footer fron the main page

--- src/HaveFun/FunHome.jsx
+++ src/HaveFun/FunHome.jsx
@@ -9,7 +9,7 @@ function FunHome() {
             <div className="div-havefun">
 
                 <p className="p-havefun">Un apartado donde puedes divertirte un poco con las aplicaciones
-                    que ofrecen diferentes interacciones e informacion de diferentes personajes 
+                    que ofrecen diferentes interacciones e informacion de diferentes personajes
                     que te puede agradar</p>
 
                 <div className="card-havefun">
@@ -23,8 +23,8 @@ function FunHome() {
 
                 <div className="card-havefun">
                     <h1 className="h1-havefun">OraculoApp</h1>
-                    <p className="p-card">Una aplicacion a la cual puedes hacerle preguntas 
-                    para sabar mas sobre tu destino</p>
+                    <p className="p-card">Una aplicacion a la cual puedes hacerle preguntas
+                        para sabar mas sobre tu destino</p>
                     <div className="button-havefun">
                         <a className="a-card" href="havefun/yesornotapp">visitar</a>
                     </div>
@@ -59,8 +59,28 @@ function FunHome() {
 
             </div>
 
-            <div>
-                <h1 className="h1-havefun">Footer have fun</h1>
+            <div className="gif-footer">
+                <div className="footer-item">
+                    <h1 className="title1">Importante</h1>
+                    <p className="Content">Estas seccion fueron creadas gracias al curso de
+                        react de Fernando Herrera, si quieres apoyarlo puedes comprar su
+                        curso o recomendarlo.
+                    </p>
+
+                    <p className="Content">Esta pagina forma parte de mi portafolio, si
+                        te interesa puedes contactarme en algunas de las siguientes redes.</p>
+
+                    <p className="Content">Tambien puedes pasar a mi canal de Youtube
+                        quiza encuentras contenido que te agrade y me agregas a tus canala
+                        preferidos</p>
+                </div>
+
+                <div className="item-grid">
+                    <h1 className="title1">Contacto</h1>
+                    <p className="contacto">Enviame un <a className="contacto" href="mailto:stevechanvitas@gmail.com">Correo</a></p>
+                    <a className="contacto" href="https://github.com/MarcoAntonioGuadalupeRivasGuzman">Githud</a>
+                    <a className="contacto" href="https://www.youtube.com/@stevechambitas2782">Youtube</a>
+                </div>
             </div>
 
         </>
