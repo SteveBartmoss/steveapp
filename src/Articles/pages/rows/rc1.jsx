1dfcf713e9ea1bc8c2de86c0a246f8512f2859ae
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-08-04 20:35:19 -0600

    git add a new folder an new page for the first article in the topic rows and colums

---
+++ src/Articles/pages/rows/rc1.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Rc1(){
+    return(
+        <>
+            <h1 className="title1">La gama alta no es la meta</h1>
+        </>
+    );
+}
+
+export default Rc1;
