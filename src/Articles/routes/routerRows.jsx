1520cf86db35aa721f92615439a3c24be113728c
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-08-04 20:37:01 -0600

    add a new routes for the rows topics

---
+++ src/Articles/routes/routerRows.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Rc1 from "../pages/rows/rc1";
+
+function RouterRows(){
+    return(
+        <>
+            <Routes>
+                <Route path="rc1" element={<Rc1 />} />
+            </Routes>
+        </>
+    );
+}
