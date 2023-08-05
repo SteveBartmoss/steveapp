9f034e7082ffe50c2da6ded95e9018feea6a40e2
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-08-04 20:09:07 -0600

    modify a route for the topic rows and colums

--- src/Articles/routes/routerArticles.jsx
+++ src/Articles/routes/routerArticles.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Rows from "../pages/rows";
 
 function ArticlesRouter(){
     return(
         <>
             <Routes>
-                <Route path="rowsandcolums" element={<></>} />
+                <Route path="rows" element={<Rows />} />
             </Routes>
         </>
     );
