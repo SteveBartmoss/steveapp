4a0253a821d5b8a774cff7e672edd45c85bc2b98
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-08-04 21:00:53 -0600

    add a new nestwd route for the rows and coplums articles

--- src/Articles/routes/routerArticles.jsx
+++ src/Articles/routes/routerArticles.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Rows from "../pages/rows";
+import RouterRows from "./routerRows";
 
 function ArticlesRouter(){
     return(
         <>
             <Routes>
                 <Route path="rows" element={<Rows />} />
+
+                <Route path="rows/*" element={<RouterRows />} />
+                
             </Routes>
         </>
     );
