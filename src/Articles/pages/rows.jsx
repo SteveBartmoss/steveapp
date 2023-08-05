4d27ab853566732db5508175f7ebf8f0f389407c
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-08-05 17:27:54 -0600

    add new topic for the firts article in the rows page

--- src/Articles/pages/rows.jsx
+++ src/Articles/pages/rows.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 
-function Rows(){
+function Rows() {
     return (
         <>
-            <h1 className="title1">Rows and Colums</h1>
+            <div className="div-about">
+                <div className="center">
+                    <h1 className="title1">Rows and Colums</h1>
+                </div>
+
+                <div className="div-topic">
+                    <h1 className="title1">La gama Alta no es todo</h1>
+                    <p className="Content"> Actualmente la mayoria de usuarios de pc tiene 
+                    la idea de que se debe llegar a la gama alta del pc gaming y tener 
+                    toda la configuracion de una computadora tope de gama llegano a formar 
+                    de cierta manera una elite conformada
+                    </p>
+                    <div className="center">
+                        <a className="dumix-btn" href="rows/rc1">Ver articulo</a>
+                    </div>
+                </div>
+
+            </div>
         </>
     );
 }
