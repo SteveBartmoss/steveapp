8a181755cb69beec5959284f7b7e05bf5ffebffe
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-08-04 21:31:03 -0600

    add a new page for the progres reports

---
+++ src/pages/progreso.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Progreso(){
+    return(
+        <>
+            <h1 className="title1">Estamos trabajando para que la pagina cresca</h1>
+        </>
+    );
+}
+
+export default Progreso;
