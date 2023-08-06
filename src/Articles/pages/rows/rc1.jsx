3fce2b38213ea11e0db36baa566c69029ced1379
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-08-06 11:48:57 -0600

    add a new div whit the classname back-articles for the new background in the articles

--- src/Articles/pages/rows/rc1.jsx
+++ src/Articles/pages/rows/rc1.jsx
@@ -7,87 +7,95 @@ function Rc1() {
                 <div className="center">
                     <h1 className="main-title">La gama alta no es la meta</h1>
                 </div>
-                
-                <hr className="line"/>
 
-                <p className="content-curso">
-                    Actualmente la mayoria de usuarios de pc tiene la idea de que se debe llegar a 
-                    la gama alta del pc gaming y tener toda la configuracion de una computadora tope 
-                    de gama llegano a formar de ciert manera una elite conformada solo por aquellos 
-                    que tienen el dinero suficiente para comprar los componentes de mayor rendimiento
-                    despues de todo se busca tener la mejor calidad el jugar y poder llegar a los 
-                    rangos mal altos de las clasificatorias.
-                </p>
-                
-                <p className="content-curso">
-                    Pero esto realmente se trata de una mejora o simplemente podria ser una 
-                    estrategia de marketing bueno algunas cosas parecen indicar que solo se trata de 
-                    marketing para tener la mejores las configuraciones de hardware, lo primero que 
-                    es importante mencionar es que no se trata de una crítica y si eres un usuario de 
-                    la gama alta de pc o te gusta tener el tope de gama no tienes por qué ofenderte y 
-                    tampoco debes tomar como una regla o ley los ejemplos que se mencionan en este post 
-                    solo disfruta de tu computadora. 
-                </p>
+                <hr className="line" />
 
-                <p className="content-curso">
-                    Desde hace mucho tiempo que las marcas de componentes de computadoras aprovecharon 
-                    el mercado de juegos en pc para aumentar el costo de sus componentes e incluso 
-                    llegar a crear ciertos modelos que están destinados para los jugadores como lo son 
-                    los modelos de Aprous o Rog que se enfocan en llamar la atencion de los jugadores y 
-                    creando la imagen de gaming competitivo pero esto no quiere precisamente que sea la 
-                    mejor configuración para jugar, el problema de ir en busca de una configuración de 
-                    tope de gama para juegos es que la mayoría de recursos no estarían siendo utilizados 
-                    en su totalidad y en la gran mayoría de casos se pueden ver más como un símbolo de 
-                    estatus que como una herramienta a la cual se le pueda sacar el mayor provecho, muchas 
-                    personas piensan que tener la gráfica tope de gama te dará el poder de ver más de 120  
-                    fps pero esto no es tan simple como se piensa, además de se debe equilibrar la calidad 
-                    de los otros componentes como el procesador, la fuente de poder y la placa madre 
-                    también es necesario tener un monitor que pueda dar una tasa de refrescamiento 
-                    alta ya que de lo contrario no permitirá ver los fps como deberías asi que también 
-                    se debe invertir en un buen monitor para poder sacarle provecho al máximo y es posible 
-                    que no todas las resoluciones puedan ir a fps altos y de manera estable. Otro factor 
-                    importante es que el juego debe estar bastante optimizado para poder tener un rendimiento 
-                    bueno ya que existen casos en los que no siquiera el tope de gama puede con el juego y 
-                    aunque esto pueda parecer una exageración ya existen casos como lo fue crisis 3 que no 
-                    estaba bien optimizado y no se aprovechaba por que la optimización del juego no es 
-                    la mejor y aunque actualmente existen mejores adaptaciones de juegos no siempre se podrá 
+                <div className="back-articles">
+                    <p className="content-curso">
+                        Actualmente la mayoria de usuarios de pc tiene la idea de que se debe llegar a
+                        la gama alta del pc gaming y tener toda la configuracion de una computadora tope
+                        de gama llegano a formar de ciert manera una elite conformada solo por aquellos
+                        que tienen el dinero suficiente para comprar los componentes de mayor rendimiento
+                        despues de todo se busca tener la mejor calidad el jugar y poder llegar a los
+                        rangos mal altos de las clasificatorias.
+                    </p>
+
+                    <p className="content-curso">
+                    Pero esto realmente se trata de una mejora o simplemente podria ser una
+                    estrategia de marketing bueno algunas cosas parecen indicar que solo se trata de
+                    marketing para tener la mejores las configuraciones de hardware, lo primero que
+                    es importante mencionar es que no se trata de una crítica y si eres un usuario de
+                    la gama alta de pc o te gusta tener el tope de gama no tienes por qué ofenderte y
+                    tampoco debes tomar como una regla o ley los ejemplos que se mencionan en este post
+                    solo disfruta de tu computadora.
+                    </p>
+
+                    <p className="content-curso">
+                    Desde hace mucho tiempo que las marcas de componentes de computadoras aprovecharon
+                    el mercado de juegos en pc para aumentar el costo de sus componentes e incluso
+                    llegar a crear ciertos modelos que están destinados para los jugadores como lo son
+                    los modelos de Aprous o Rog que se enfocan en llamar la atencion de los jugadores y
+                    creando la imagen de gaming competitivo pero esto no quiere precisamente que sea la
+                    mejor configuración para jugar, el problema de ir en busca de una configuración de
+                    tope de gama para juegos es que la mayoría de recursos no estarían siendo utilizados
+                    en su totalidad y en la gran mayoría de casos se pueden ver más como un símbolo de
+                    estatus que como una herramienta a la cual se le pueda sacar el mayor provecho, muchas
+                    personas piensan que tener la gráfica tope de gama te dará el poder de ver más de 120
+                    fps pero esto no es tan simple como se piensa, además de se debe equilibrar la calidad
+                    de los otros componentes como el procesador, la fuente de poder y la placa madre
+                    también es necesario tener un monitor que pueda dar una tasa de refrescamiento
+                    alta ya que de lo contrario no permitirá ver los fps como deberías asi que también
+                    se debe invertir en un buen monitor para poder sacarle provecho al máximo y es posible
+                    que no todas las resoluciones puedan ir a fps altos y de manera estable. Otro factor
+                    importante es que el juego debe estar bastante optimizado para poder tener un rendimiento
+                    bueno ya que existen casos en los que no siquiera el tope de gama puede con el juego y
+                    aunque esto pueda parecer una exageración ya existen casos como lo fue crisis 3 que no
+                    estaba bien optimizado y no se aprovechaba por que la optimización del juego no es
+                    la mejor y aunque actualmente existen mejores adaptaciones de juegos no siempre se podrá
                     sacar el máximo a nuestros componentes en la mayoría de juegos.
-                </p>
+                    </p>
 
-                <p className="content-about">
-                    Lo anterior cobra mas sentido cuando nos ponemos a pensar que juegos tan antiguos pero 
-                    que fueron tan bien optimizados se puedan correr en diferentes tipos de hardware 
-                    (asi es doom te estoy hablando a ti) asi que el tope de gama no siempre te dara la mayor 
-                    tase de fps pero si exprimirá tu dinero, en muchas ocaciones el gasto extra ni siquiera 
+                    <p className="content-curso">
+                    Lo anterior cobra mas sentido cuando nos ponemos a pensar que juegos tan antiguos pero
+                    que fueron tan bien optimizados se puedan correr en diferentes tipos de hardware
+                    (asi es doom te estoy hablando a ti) asi que el tope de gama no siempre te dara la mayor
+                    tase de fps pero si exprimirá tu dinero, en muchas ocaciones el gasto extra ni siquiera
                     tiene sentido ya que muchas veces ni siquiera mejora tanto ek rendimiento ya que por ejemplo
-                    muchos modelos de aorus o rog no incorporan mas que leds llamativos y un pequeno overclock 
-                    que en la mayoria de los casos no se notara tanto al momento de juagr pero tu bolsillo notara 
-                    el cambio, otro ejemplo de esto son las velocidades de la memoria ram ya que desde hace un tiempo 
-                    la mayoría busca tener memorias de 3400 de frecuencia porque de esta manera tendrían 
-                    el mejor rendimiento de su equipo pero esto no daba una mejora tan significante 
-                    pero el precio de la memoria también subía bastante y la mejora solo podía ser 
-                    significante cuando tenias una gpu integrada pero la verdad es que teniendo una 
-                    grafica integrada poco se puede hacer para que tengas el rendimiento de una gpu 
-                    dedica, en todo caso era mejor ahorrar un poco en las memorias y comprar una grafica 
-                    dedicada la cual si nos da un aumento considerable en la mayoría de juegos. La 
-                    lista de ejemplos continua con cosas como las refrigeraciones liquidas para 
-                    procesadores que no estarán bajo cargar intensas de trabajo las 24 horas ni a un 
-                    overclockt tan potente hasta invertir dinero un almacenamiento solido de un 
-                    terabyte para poder almacenar los juegos en el a pesar de que la mejora del 
-                    desempeño de los juegos es bastante baja, considero que la verdadera razón 
-                    para que las marcas usen la estrategia del pc gaming para conseguir mas dinero es 
-                    simplemente que las personas están dispuestas a pagar los precias mas elevados para 
-                    poder tener el topo de gama personalmente creo que no hay nada de malo en no tener 
-                    el tope de gama ya que se puede jugar bastante bien con algo de hardware de entrada 
-                    y cuidando nuestros bolsillos pero tampoco se puede odiar a las personas que invierten 
-                    para tener el tope de gama ya que ellas pueden sentirse cómodas con eso, mi recomendación 
-                    es que busquer tener un equilibrio entre el precio y el desempeño para no tener 
-                    gastar todo tu dinero en componentes y mejor puedas invertir en mejores periféricos 
+                    muchos modelos de aorus o rog no incorporan mas que leds llamativos y un pequeno overclock
+                    que en la mayoria de los casos no se notara tanto al momento de juagr pero tu bolsillo notara
+                    el cambio, otro ejemplo de esto son las velocidades de la memoria ram ya que desde hace un tiempo
+                    la mayoría busca tener memorias de 3400 de frecuencia porque de esta manera tendrían
+                    el mejor rendimiento de su equipo pero esto no daba una mejora tan significante
+                    pero el precio de la memoria también subía bastante y la mejora solo podía ser
+                    significante cuando tenias una gpu integrada pero la verdad es que teniendo una
+                    grafica integrada poco se puede hacer para que tengas el rendimiento de una gpu
+                    dedica, en todo caso era mejor ahorrar un poco en las memorias y comprar una grafica
+                    dedicada la cual si nos da un aumento considerable en la mayoría de juegos. La
+                    lista de ejemplos continua con cosas como las refrigeraciones liquidas para
+                    procesadores que no estarán bajo cargar intensas de trabajo las 24 horas ni a un
+                    overclockt tan potente hasta invertir dinero un almacenamiento solido de un
+                    terabyte para poder almacenar los juegos en el a pesar de que la mejora del
+                    desempeño de los juegos es bastante baja, considero que la verdadera razón
+                    para que las marcas usen la estrategia del pc gaming para conseguir mas dinero es
+                    simplemente que las personas están dispuestas a pagar los precias mas elevados para
+                    poder tener el topo de gama personalmente creo que no hay nada de malo en no tener
+                    el tope de gama ya que se puede jugar bastante bien con algo de hardware de entrada
+                    y cuidando nuestros bolsillos pero tampoco se puede odiar a las personas que invierten
+                    para tener el tope de gama ya que ellas pueden sentirse cómodas con eso, mi recomendación
+                    es que busquer tener un equilibrio entre el precio y el desempeño para no tener
+                    gastar todo tu dinero en componentes y mejor puedas invertir en mejores periféricos
                     o incluso en decoraciones para tu cuarto. Bueno esto fue filas y columnas hasta la próxima.
-                </p>
+                    </p>
+                </div>
+
+               
+
+                
+
+                
 
                 <p>
-                    
+
                 </p>
             </div>
         </>
