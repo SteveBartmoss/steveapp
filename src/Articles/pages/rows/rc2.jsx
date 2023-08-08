5ceb5fd2cc34007319aa5f56ddc484ef9c9ea07d
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-08-07 21:45:14 -0600

    add a new page for the second article in the secction rows and colums, is in progres now

---
+++ src/Articles/pages/rows/rc2.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Rc2(){
+    return(
+        <>
+        <div className="div-about">
+            <div className="center">
+                <h1 className="main-title">Juegos que reunen polvo</h1>
+            </div>
+        </div>
+        </>
+    );
+}
