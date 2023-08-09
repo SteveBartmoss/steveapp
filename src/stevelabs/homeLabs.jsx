e852cbf7bb29fbf85b5da714e16f4874efba278f
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-08-08 22:17:28 -0600

    add a div-flex-articles and div-min-article for the estyles in the home labs page

--- src/stevelabs/homeLabs.jsx
+++ src/stevelabs/homeLabs.jsx
@@ -12,21 +12,20 @@ function HomeLabs() {
                 <h1 className="title1">Comienza a aprender</h1>
             </div>
 
-            <div className="display-topics">
+            <div className="div-flex-articles">
 
-                <div className="div-topic">
+                <div className="div-min-article">
                     <h1 className="title1">Primeros pasos en programacion</h1>
                     <p className="Content">En este curso se abordan temas basicos de programacion
                         ya que esta orientado a personas que comienzan a programar por lo cual no
-                        se esperan muchos conocimientos por parte de las personas que toman el curso
-                        el lenguaje que se utiliza sera c</p>
+                        se esperan muchos conocimientos</p>
                     <div className="center">
                         <a className="dumix-btn" href="#">Ir al curso</a>
                     </div>
 
                 </div>
 
-                <div className="div-topic">
+                <div className="div-min-article">
                     <h1 className="title1">Web developer</h1>
                     <p className="Content">En este apartado puede consultar los articulos que fueron
                         creados por un colaborador, los temas tratados seran sobre desarrollo de paginas
@@ -37,7 +36,7 @@ function HomeLabs() {
                     </div>
                 </div>
 
-                <div className="div-topic">
+                <div className="div-min-article">
                     <h1 className="title1">Coder Life</h1>
                     <p className="Content">Aqui podras leer articulos sobre temas de la vida 
                     de desarolladores, juegos o simplemente reflexiones sobre temas de informatica
