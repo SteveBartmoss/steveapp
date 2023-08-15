309dfaf62af3c714a07b3eac85f50f98cf3628ad
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-08-14 20:37:51 -0600

    add a new component for the new implementation in the homes page, this gonna be a main section in the home page

---
+++ src/component/MenuItem.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function MenuItems() {
+    return (
+        <>
+            <div>
+                <h1 className="title1">Secciones mas poppulares</h1>
+                <div className="div-flex-articles">
+                    <div className="div-min-article">
+                        <div className="center">
+                            <h1 className="title1">Videos con mas vistas</h1>
+                        </div>
+                        <p className="Content">En esta seccion puedes ver una lista de mis
+                            videos con mas visitas y un paquena descripcion del mismo que tal
+                            vez te llame la atencion y decidas verlo</p>
+                        <div className="center">
+                            <a>Saber mas</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+export default MenuItems; 
