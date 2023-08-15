8d291900a1cf6460b037e2772b346e444fb989bb
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-08-14 19:13:51 -0600

    remove a div background for new style in comming

--- src/component/Card.js
+++ src/component/Card.js
@@ -3,13 +3,11 @@ import '../App.css';
 
 function Card({imgUrl,title,paragraph,urlVideo}){
     return(
-        <div className="back-card">
         <div className="div-card">
             <img className="imgCard" src={imgUrl} alt="imagenVideo"/>
             <h3 className="titleCard">{title}</h3>
             <p>{paragraph}</p>
             <a className="linkVideo" href={urlVideo} target="_blank">Ir al video</a>
-        </div>
         </div>
     );
 }
