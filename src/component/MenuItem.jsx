23359cea9fcfc3d52f40def32d7326425bc64420
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-08-14 22:03:55 -0600

    add a new elements in the menu items for more content

--- src/component/MenuItem.jsx
+++ src/component/MenuItem.jsx
@@ -6,6 +6,7 @@ function MenuItems() {
             <div>
                 <h1 className="title1">Secciones mas poppulares</h1>
                 <div className="div-flex-articles">
+
                     <div className="div-min-article">
                         <div className="center">
                             <h1 className="title1">Videos con mas vistas</h1>
@@ -14,7 +15,31 @@ function MenuItems() {
                             videos con mas visitas y un paquena descripcion del mismo que tal
                             vez te llame la atencion y decidas verlo</p>
                         <div className="center">
-                            <a>Saber mas</a>
+                            <a className="dumix-btn">Saber mas</a>
+                        </div>
+                    </div>
+
+                    <div className="div-min-article">
+                        <div className="center">
+                            <h1 className="title1">Videos con pocas vistas</h1>
+                        </div>
+                        <p className="Content">En esta seccion puedes ver una lista con videos que
+                        no tienen tantas vistas pero pienso que si deberian tener mas vistas, puedes
+                        darles un oportunidad quiza te agradan</p>
+                        <div className="center">
+                            <a className="dumix-btn">Saber mas</a>
+                        </div>
+                    </div>
+
+                    <div className="div-min-article">
+                        <div className="center">
+                            <h1 className="title1">Rows and Colums</h1>
+                        </div>
+                        <p className="Content">En esta seccion encontraras mis videos sobre reflexiones
+                        o es contenido mas tranquilo para poder tener un descanso de videos informativos
+                        pero que seguramente te podra interesar</p>
+                        <div className="center">
+                            <a className="dumix-btn">Saber mas</a>
                         </div>
                     </div>
                 </div>
