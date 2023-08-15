69fe763ad16505382799fc485d35a9595dfa2554
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-08-14 20:36:08 -0600

    format the document

--- src/Articles/articlesHome.jsx
+++ src/Articles/articlesHome.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Pie from "../component/Pie";
 
-function ArticlesHome(){
-    return(
+function ArticlesHome() {
+    return (
         <>
             <div className="center">
                 <h1 className="title1">Los mejores articulos</h1>
@@ -10,47 +10,47 @@ function ArticlesHome(){
 
             <div className="div-flex-articles">
 
-            <div className="div-min-article">
+                <div className="div-min-article">
                     <div className="center">
                         <h1 className="title1">Rows and Colums</h1>
                     </div>
-                    
-                    <p className="Content"> Una seccion mas tranquila con opiones sobre temas, 
-                    reflexiones o simplemente alguna idea sobre un tema, es contenido para
-                    poder entretener a los usuarios
+
+                    <p className="Content"> Una seccion mas tranquila con opiones sobre temas,
+                        reflexiones o simplemente alguna idea sobre un tema, es contenido para
+                        poder entretener a los usuarios
                     </p>
                     <div className="center">
                         <a className="dumix-btn" href="articulos/rows">Ver articulos</a>
                     </div>
-            </div>
+                </div>
 
-            <div className="div-min-article">
+                <div className="div-min-article">
                     <div className="center">
-                    <h1 className="title1">Reviews</h1>
+                        <h1 className="title1">Reviews</h1>
                     </div>
-                    
-                    <p className="Content"> Los guiones que uso para los videos 
-                    de review de videojuegos pero en formato de articulo y con 
-                    detalles extras para los que les agrada leer.
+
+                    <p className="Content"> Los guiones que uso para los videos
+                        de review de videojuegos pero en formato de articulo y con
+                        detalles extras para los que les agrada leer.
                     </p>
                     <div className="center">
                         <a className="dumix-btn" href="#">Ver articulos</a>
                     </div>
-            </div>
+                </div>
 
-            <div className="div-min-article">
+                <div className="div-min-article">
                     <div className="center">
-                    <h1 className="title1">Articulos informativos</h1>
+                        <h1 className="title1">Articulos informativos</h1>
                     </div>
-                    
-                    <p className="Content"> Una extension de los videos informativos 
-                    en los cuales se puede encontrar un poco mas de informacion y 
-                    posiblemente mejores ejemplos pues toman menos tiempo.
+
+                    <p className="Content"> Una extension de los videos informativos
+                        en los cuales se puede encontrar un poco mas de informacion y
+                        posiblemente mejores ejemplos pues toman menos tiempo.
                     </p>
                     <div className="center">
                         <a className="dumix-btn" href="#">Ver articulos</a>
                     </div>
-            </div>
+                </div>
 
             </div>
 
