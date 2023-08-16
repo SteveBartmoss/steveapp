92a08657acfc3c4cba11d973b886edaeca34b61d
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-08-15 19:14:23 -0600

    add a new classname for the main div for new model box, add a new topics cards

--- src/component/MenuItem.jsx
+++ src/component/MenuItem.jsx
@@ -3,9 +3,8 @@ import React from "react";
 function MenuItems() {
     return (
         <>
-            <div>
-                <h1 className="title1">Secciones mas poppulares</h1>
-                <div className="div-flex-articles">
+            <div className="div-menu-item">
+                <div className="div-flex-item">
 
                     <div className="div-min-article">
                         <div className="center">
@@ -24,8 +23,8 @@ function MenuItems() {
                             <h1 className="title1">Videos con pocas vistas</h1>
                         </div>
                         <p className="Content">En esta seccion puedes ver una lista con videos que
-                        no tienen tantas vistas pero pienso que si deberian tener mas vistas, puedes
-                        darles un oportunidad quiza te agradan</p>
+                            no tienen tantas vistas pero pienso que si deberian tener mas vistas, puedes
+                            darles un oportunidad quiza te agradan</p>
                         <div className="center">
                             <a className="dumix-btn">Saber mas</a>
                         </div>
@@ -36,12 +35,39 @@ function MenuItems() {
                             <h1 className="title1">Rows and Colums</h1>
                         </div>
                         <p className="Content">En esta seccion encontraras mis videos sobre reflexiones
-                        o es contenido mas tranquilo para poder tener un descanso de videos informativos
-                        pero que seguramente te podra interesar</p>
+                            o es contenido mas tranquilo para poder tener un descanso de videos informativos
+                            pero que seguramente te podra interesar</p>
                         <div className="center">
                             <a className="dumix-btn">Saber mas</a>
                         </div>
                     </div>
+
+                    <div className="div-min-article">
+                        <div className="center">
+                            <h1 className="title1">Articulos</h1>
+                        </div>
+                        <p className="Content">En este apartado se encuentran los articulos de
+                            la pagina que son referentes a los videos de la misma pero con un poco
+                            mas de informacion que te puede agradar</p>
+                        <div className="center">
+                            <a className="dumix-btn">Saber mas</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="div-article">
+                    <h1 className="title1">El cyber punk esta delante de nosotros</h1>
+                    <p className="content-columna">La mayoria de personas se siente atraida por la idea de vivir en una epoca ambientada en
+                        la tematica futurista y distopica de una sociedad cyberpunk ya que para ser honestos a quien
+                        no le gusta la idea de que tengamos autos voladores, implamentes tecnologicos y a una red de
+                        tecnologia que nos permite estar conectados con la mayoria. Pero esto no esta lejos de nuestra
+                        actualidad y de hecho la gran mayoria de personas esta viviendo en una epoca de tecnologia al
+                        nivel de una historia cyberpunk aunque no lo paresca hay una gran muestra de esto en muchos lados
+                        como podemos ver a continuacion.
+                    </p>
+                    <div className="center">
+                        <a className="dumix-btn">Seguir leyendo</a>
+                    </div>
+                    
                 </div>
             </div>
         </>
