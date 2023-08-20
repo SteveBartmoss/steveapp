d92a296a35650e7efe622ce89524d59d701590c7
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-08-20 11:48:15 -0600

    remove a duplicate data

--- src/pages/Steve.js
+++ src/pages/Steve.js
@@ -130,19 +130,6 @@ function Steve() {
                 <div className="main-contetn">
                     <Actividad juego={"border lands"} trabajo={"pagina canal"} estudio={"react"} review={"papers please"} />
 
-                    <div className="biblio">
-                        <p>Actualmente estudiante de ingenieria en sistemas con cero porciento de probabilidades
-                            de triunfar pero con el cien porciento de fe, uno de mis gustos es la cultura geek por lo cual me
-                            me gustan las peliculas de super heroes, la ciencia ficcion, los videojuegos y los temas de programacion
-                            aunque en esto ultimo no soy tan bueno. Inicie mi canal de youtube para ayudarme a repasar temas de la
-                            carrera y de paso podia compartir temas que me gustaban con otras personas teniendo la esperanza de que
-                            otras personas se interesen por los temas de programacion, una de mis metas es poder fundar una academia en
-                            linea para que las personas puedan acceder al conocimiento de manera facil y rapida esto aun queda muy lejos
-                            y tenemos todo en contra pero hare lo que se pueda. Por esta razon inicia mi camino en youtube para empezar a
-                            compartir informacion con otras personas pero poco a poco espero poder mudarme a mi propia plataforma.
-                        </p>
-                    </div>
-
                 </div>
             </div>
             <div className="Contenedor">
