cbec9471875393efbde6e84b3f64cf0c68ffa670
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-08-20 18:46:39 -0600

    add a new information for the second article in the page

--- src/Articles/pages/rows/rc2.jsx
+++ src/Articles/pages/rows/rc2.jsx
@@ -1,13 +1,86 @@
 import React from "react";
+import CreatorSing from "../../../component/sing";
 
-function Rc2(){
-    return(
+function Rc2() {
+    return (
         <>
-        <div className="div-about">
-            <div className="center">
-                <h1 className="main-title">Juegos que reunen polvo</h1>
+            <div className="div-about">
+                <div className="center">
+                    <h1 className="main-title">Juegos que reunen polvo</h1>
+                </div>
+
+                <hr className="line" />
+
+                <CreatorSing autor={"Steve"} date={"08/04/23"} img={"/Assets/steveA.png"} grade={"Free coder"} />
+
+                <div className="back-articles">
+                    <p className="content-curso">
+                        Desde que era pequeno pensaba en que algun dia tendria una gran colleccion de juegos y en
+                        cierta manera logre tener muchos juegos debido a que el Xbox que tenia contaba con la típica
+                        modificación con lo cual podía conseguir muchos juegos sin tener que invertir mucho dinero
+                        lo cual me permitió poder probar bastantes juegos e incluso de diferentes plataformas ya que
+                        los emuladores estaban presentes en la primera generación de Xbox, sin duda alguna considero
+                        que esa fue mi epoca dorada del gaming y se puede deber a que fue la primara consola que tuve
+                        pero los titulos que pude proba fueron relamente buenos.</p>
+
+                    <p className="content-curso"> Actualmente cuento con ingresos y puedo jugar bastantes títulos gracias a la computadora que
+                        arme con el tiempo por lo que mi temporada favorita son las rebajas de steam ya que siempre guardo
+                        dinero para poder comprar unos cuentos juegos que podre disfrutar durante los fines de semana,
+                        despues de todo esa es la ventaja de poder tener tu dinero verdad? Nuca habia estao tan equicado
+                        en mi vida ya que actualmente se esta viviendo un nuevo fenómeno del cual lamentablemente
+                        tambien soy participante y que afecta a los juegadores de pc o cualquier otra plataforma pues
+                        el principal objetivos son los adultos que buscamos seguir disfrutando del pasatiempo de los videojugos.
+                    </p>
+                </div>
+
+                <h1 className="title1">El peor miedo de los jugadores</h1>
+                <hr className="line" />
+
+                <div>
+                    <p>
+                        Cuando
+                        escuche sobre la gente que compraba juegos en steam pero que nunca los jugaba o que solo los habían
+                        probado por unos cuantos minutos no podía creerlo y pensaba que solo se trataba de gente quería
+                        ver arder el mundo o que comprobaba si su equipo era suficiente para poder correr el juego
+                        pero actualmente he caído en una situación similar y he descubierto que la mayoría de personas
+                        adultas que es parte de la comunidad del gaming tiene un tiempo tan reducido que un catalogo de
+                        juegos tan amplio les resulta una tarea imposible ya que no disponen ni la mitad del tiempo necesario
+                        para poder completar todos los juegos de la librería de steam, no hace falta hacer un estudio para
+                        comprobar esto ya que podemos descubrir que este fenemo ocurre y que se mantiene con la ayuda de los
+                        logros de steam ya que gracias a las estadísticas de los mismo podemos ver que un porcentaje de los
+                        jugadores nunca obtiene los logros mas simples o que indican que completaron el prologo o el primer
+                        nivel del juego y es por experiencia propia que puedo decir que en ocasiones compro un titulo que
+                        estaba en muy buena oferta pero que no soy capas de probarlo en mucho tiempo porque tengo otros juegos
+                        o porque mis amigos están conectados en algún battle royale o similar. Aunque esto pueda parecer algo
+                        que no tiene importancia y además de que resulta una evidencia de la cultura del consumismo que cada
+                        vez está más presente en la sociedad actual es un punto importante que se tomara en cuenta en las
+                        industrias de los videojuegos y que también es un factor importante para entender porque algunos juegos
+                        multijugador son tan populares ya que en este tipo de juegos no hace falta invertir grande cantidades
+                        de horas y basta con encontrar una partida y rápidamente enfrentarse a otros jugadores y si se termina
+                        es cuestión de minutos para poder iniciar otro partida, títulos como elden ring, grand def rancho o
+                        cualquier otro triple a respetable resultan una experiencia buena pero algo desesperante para los
+                        usuarios que buscan pasar un rato agradable en algún titulo que no requiera mucho tiempo de farmeo o
+                        que tenga misiones tan largas que requieran de por lo menos una hora de duración. En este sentido la
+                        industria tiene un problema claro a la hora de desinar los títulos ya que por un lado esta el sector
+                        de jugadores que disfrutan los juegos que requieren muchas horas de inversión y que tienen el tiempo
+                        necesario para poder invertirlo en estos tipos de juegos ya que puede que no tengan  un presupuesto
+                        tan alto para poder comprar diferentes títulos  e invertir su dinero en algo que terminaran en apenas
+                        unas pocas horas no suena como una buena idea, por otro lado están las personas que no disponen de
+                        tanto tiempo como para poder completar títulos de largas duraciones y en ese caso prefieren juegos
+                        que sean rápido o que permitan tener partidas rápida y debido a esto no es raro que juegos multijugadores
+                        como fornite o warzone se vuelvan populares rápidamente gracias a la rapidez con la que terminas las
+                        partidas y puedes iniciar una nueva lo cual permite invertir menos tiempo y quizas solo un poco de
+                        dinero para los pases de batalla o en algún juego indie que se completa rápidamente y que permite jugar
+                        rápidamente. Debido a esto la industria tiene que empezar a considerar que no todo el mundo esta dispuesto
+                        a completar títulos de mas de 80 horas de duración y empezar a concentrarse en experiencias mas agiles o
+                        que permitan disfrutar a las personas que no disponen de tanto tiempo, esto representa un cambio en el
+                        futuro de la industria de los video juegos bueno esto fue filass y columnas hasta próxima
+                    </p>
+                </div>
+
             </div>
-        </div>
         </>
     );
 }
+
+export default Rc2;
