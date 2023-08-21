95d72ba3da3484441a1441c49fd5b49016260e07
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-08-20 18:48:05 -0600

    add a new route for the second article in the page

--- src/Articles/routes/routerRows.jsx
+++ src/Articles/routes/routerRows.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Rc1 from "../pages/rows/rc1";
+import Rc2 from "../pages/rows/rc2";
 
 function RouterRows(){
     return(
         <>
             <Routes>
                 <Route path="rc1" element={<Rc1 />} />
+
+                <Route path="rc2" element={<Rc2 />} />
             </Routes>
         </>
     );
