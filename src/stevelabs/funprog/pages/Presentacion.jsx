904c8d8d04e5621f534d68952b1a2bce55977b8e
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-21 22:08:20 -0600

    add a new content for the first page in the curse

---
+++ src/stevelabs/funprog/pages/Presentacion.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Presentacion(){
+    return(
+        <>
+        <div>
+            <h1>Acerca del curso</h1>
+            <p>El curso esta pensado para que personas que no han programado
+                nunca y que quieren empezar a programar, por esta razon se abordan
+                temas simples y tambien se explicaran cosas sencillas como la configuracion 
+                del ambiente para poder inciar con la programacion.
+            </p>
+        </div>
+        </>
+    );
+}
+
+export default Presentacion;
