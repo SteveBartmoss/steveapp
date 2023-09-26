a75042654a48bf3298ab1db34922f8814c41363b
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-25 21:17:08 -0600

    add a new content for the page presentacion

--- src/stevelabs/funprog/pages/Presentacion.jsx
+++ src/stevelabs/funprog/pages/Presentacion.jsx
@@ -5,10 +5,21 @@ function Presentacion(){
         <>
         <div>
             <h1>Acerca del curso</h1>
-            <p>El curso esta pensado para que personas que no han programado
+            <p> 
+                El curso esta pensado para que personas que no han programado
                 nunca y que quieren empezar a programar, por esta razon se abordan
                 temas simples y tambien se explicaran cosas sencillas como la configuracion 
                 del ambiente para poder inciar con la programacion.
+            </p>
+            <p>
+                El curso se creo por un estudiante de temas relacionados con programcion pero
+                se espera que la informacion pueda servir para las personas que 
+                quieren aprender y quisa la explicacion sea mas sencilla y clara.
+            </p>
+            <p>
+                Durante el curso se invitara al usuario a usar tanto el sistema operativo
+                windows como un sistema linux aunque las variaciones estaran principalmente 
+                en la configuracion del ambiente de desarrollo.
             </p>
             <h1>Ambiente de desarrollo</h1>
         </div>
