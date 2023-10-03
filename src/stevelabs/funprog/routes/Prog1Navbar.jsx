f2347a18ac60539ee9ae1b7944750b9cd0d402d7
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-10-02 21:57:39 -0600

    add a subjects for the course

--- src/stevelabs/funprog/routes/Prog1Navbar.jsx
+++ src/stevelabs/funprog/routes/Prog1Navbar.jsx
@@ -1,15 +1,36 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
 
-function Prog1Navbar(){
+function Prog1Navbar() {
     return (
         <>
             <div className="div-prime">
                 <div className="grid-dumix">
                     <div className="linux-menu">
                         <li>
-                            <NavLink className={({isActive})=> `tema ${isActive ? 'select' : ''}`} to="presentacion">Presentacion</NavLink>
+                            <NavLink className={({ isActive }) => `tema ${isActive ? 'select' : ''}`} to="presentacion">Presentacion</NavLink>
                         </li>
+
+                        <li>
+                            <NavLink className={({ isActive }) => `tema ${isActive ? 'select' : ''}`} to="presentacion">Salida Estandar</NavLink>
+                        </li>
+
+                        <li>
+                            <NavLink className={({ isActive }) => `tema ${isActive ? 'select' : ''}`} to="presentacion">Entrada Estandar</NavLink>
+                        </li>
+
+                        <li>
+                            <NavLink className={({ isActive }) => `tema ${isActive ? 'select' : ''}`} to="presentacion">Operadores</NavLink>
+                        </li>
+
+                        <li>
+                            <NavLink className={({ isActive }) => `tema ${isActive ? 'select' : ''}`} to="presentacion">Condicionales</NavLink>
+                        </li>
+
+                        <li>
+                            <NavLink className={({ isActive }) => `tema ${isActive ? 'select' : ''}`} to="presentacion">Bucles</NavLink>
+                        </li>
+
                     </div>
                     <Outlet />
                 </div>
