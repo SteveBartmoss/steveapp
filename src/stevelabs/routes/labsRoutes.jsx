3863db90d820af3171be4ec7003bf7ae34f5d11b
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-10-29 14:21:45 -0600

    add a new page for the route input

--- src/stevelabs/routes/labsRoutes.jsx
+++ src/stevelabs/routes/labsRoutes.jsx
@@ -5,6 +5,7 @@ import Prog1Presentacion from "../funprog/pages/Presentacion";
 import Prog1Algoritmo from "../funprog/pages/Algoritmo";
 import Prog1Compile from "../funprog/pages/Compile";
 import Prog1Estructura from "../funprog/pages/Estructura";
+import Prog1Input from "../funprog/pages/Input";
 
 function LabsRoutes(){
     return(
@@ -21,7 +22,7 @@ function LabsRoutes(){
 
                     <Route path="estructura" element={<Prog1Estructura />} />
 
-                    <Route path="salida" element={<h1>Salida</h1>} />
+                    <Route path="salida" element={<Prog1Input />} />
 
                     <Route path="entrada" element={<h1>Entrada</h1>} />
 
