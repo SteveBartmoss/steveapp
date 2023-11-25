1c29fd89fe2397b926d29e04a5c11b676c0ea7eb
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-11-25 10:43:24 -0600

    add a new section for the reviews, add a main page prototipe

---
+++ src/Articles/pages/rewiews.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Reviews(){
+    return(
+        <>
+        <div>
+            <div>
+                <h1>Reviews</h1>
+            </div>
+
+            <div>
+                
+            </div>
+
+        </div>
+        </>
+    );
+}
+
+export default Reviews;
