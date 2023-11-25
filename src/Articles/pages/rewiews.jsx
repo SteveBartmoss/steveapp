081ac215d3474e8c9d06ccd1724a9e05cf8bd756
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-11-25 11:15:58 -0600

    add a styles for the card in the main page of reviews

--- src/Articles/pages/rewiews.jsx
+++ src/Articles/pages/rewiews.jsx
@@ -3,21 +3,21 @@ import React from "react";
 function Reviews(){
     return(
         <>
-        <div>
-            <div>
-                <h1>Reviews</h1>
+        <div className="div-about">
+            <div className="center">
+                <h1 className="title1">Reviews</h1>
             </div>
 
-            <div>
-                <h1>Death Stranding</h1>
-                <p>Aunque las primeras impresiones del juego fueron
+            <div className="div-topic">
+                <h1  className="title1">Death Stranding</h1>
+                <p className="Content">Aunque las primeras impresiones del juego fueron
                     un poco raras este juego resulto ser mucho mas 
                     que uber simulator, me pase un buen rato reconstruyendo 
                     el mundo junto a sam briges y mirando el paisaje que 
                     kojima prepararo para el jugador
                 </p>
-                <div>
-                    <a>Ver review</a>
+                <div className="center">
+                    <a className="dumix-btn" href="#">Ver review</a>
                 </div>
             </div>
 
