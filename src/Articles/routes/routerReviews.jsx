5338384d62017e67ea735b448658b35c4a097f9b
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-11-25 11:04:20 -0600

    add a new routes for the router reviews, and the main page of reviews

--- src/Articles/routes/routerReviews.jsx
+++ src/Articles/routes/routerReviews.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Rw1 from "../pages/reviews/rw1";
 
 function RouterReviews(){
     return(
         <>
+            <Routes>
+                <Route path="rw1" element={<Rw1 />} />
+            </Routes>
         </>
     );
 }
