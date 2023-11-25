aad85ba550783598b56b209ca191d783348badcb
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-11-25 11:03:22 -0600

    add a new route for the first article

--- src/Articles/routes/routerArticles.jsx
+++ src/Articles/routes/routerArticles.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Rows from "../pages/rows";
 import RouterRows from "./routerRows";
+import Reviews from "../pages/rewiews";
+import RouterReviews from "./routerReviews";
 
 function ArticlesRouter(){
     return(
@@ -10,6 +12,10 @@ function ArticlesRouter(){
                 <Route path="rows" element={<Rows />} />
 
                 <Route path="rows/*" element={<RouterRows />} />
+
+                <Route path="reviews" element={<Reviews />} />
+
+                <Route path="reviews/*" element={<RouterReviews />} />
                 
             </Routes>
         </>
