898021cdb27a0ae42b72e7d37458226febc56d34
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-11-25 10:48:53 -0600

    add a new card for the first review

--- src/Articles/pages/rewiews.jsx
+++ src/Articles/pages/rewiews.jsx
@@ -9,7 +9,16 @@ function Reviews(){
             </div>
 
             <div>
-                
+                <h1>Death Stranding</h1>
+                <p>Aunque las primeras impresiones del juego fueron
+                    un poco raras este juego resulto ser mucho mas 
+                    que uber simulator, me pase un buen rato reconstruyendo 
+                    el mundo junto a sam briges y mirando el paisaje que 
+                    kojima prepararo para el jugador
+                </p>
+                <div>
+                    <a>Ver review</a>
+                </div>
             </div>
 
         </div>
