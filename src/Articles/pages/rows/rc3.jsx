37259f3fda0603c767595e7937a6ae4d7d3cf8ff
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-11-26 18:14:59 -0600

    add a paragraph

--- src/Articles/pages/rows/rc3.jsx
+++ src/Articles/pages/rows/rc3.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import CreatorSing from "../../../component/sing";
 
-function Rc3(){
-    return(
+function Rc3() {
+    return (
         <>
             <div>
                 <div>
@@ -10,48 +10,62 @@ function Rc3(){
                 </div>
 
                 <hr />
-                
+
                 <CreatorSing autor={"Steve"} date={"08/04/23"} img={"/Assets/steveA.png"} grade={"Free coder"} />
 
+                <div>
+                    <p>
+                        La forma en que los jugadores interactuan con el mercado de los juegos ha cambiado
+                        bastante ya que ahora no solo hay modelos de juegos como servicio y que son gratis
+                        al publico si no que tambien es posible apollar juegos que aun estan en desarrollo,
+                        mediante la nueva forma de lanzar juegos de acceso anticipado en la cual el desarrollador
+                        recibe un poco de apollo para continuar su juegos y los jugadores pueden probar un juego
+                        al que se iran agregando nuevas cosas. Nunca tuve la oportunidad de participar en un 
+                        acceso anticipado porque antes de tener mi computadora solo podía jugar a títulos que 
+                        estaban completos y tambien porque en esa epoca no era comun los accesos anticipados 
+                        en las consolas, recientemente obtuve otro título de acceso anticipado ya que me llamo 
+                        la atencion el genero y pensaba que seria muy buen juego aunque debo decir que la 
+                        experiencia no fue tan buena como pensaba.
+                    </p>
+                </div>
+
                 <p>
-                    Nunca tuve la oportunidad de participar en un acceso anticipado porque antes de tener 
-                    mi computadora solo podía jugar a títulos que estaban completos y recientemente obtuve 
-                    otro título de acceso anticipado y la experiencia no fue tan buena como pensaba, ya 
-                    antes había comprado juegos que aún estaban en desarrollo siendo valheim uno de ellos 
-                    pero el título que probé en este caso me dejó un poco desanimado pues se sentía 
-                    bastante incompleto lo que me molestó un poco al inicio pero recordé que aún se estaba 
-                    trabajando en el juego lo cual me tranquilizo pensando en que posteriormente se 
-                    tendría un proceso y que además estaba apoyando a un desarrollador indie que dispone 
-                    de pocos recursos y quien sabe tal vez el juego se llegue a convertir en uno de mis 
-                    favoritos solo que aún falta tiempo para completarlo. Luego de un rato me surgió la 
-                    duda de que pasaría si el juego nunca continúa su desarrollo ya que si bien el título 
-                    pone en la tienda de steam que es un juego sin terminar no te da una cláusula para 
-                    poder regresar este tipo de juegos ya que al comprar un acceso anticipado lo haces 
-                    bajo tu propio riesgo, esto parece un poco injusto ya que por una parte el 
-                    desarrollador obtiene apoyo para completar su juego lo cual puede resultar una 
-                    buena experiencia para ambos pero por otro lado los usuarios solo se mantienen con 
-                    la promesa de que el juego se terminara en algún momento basado en la confianza que 
-                    se tiene pero no existe nada que obligué a terminar el juego que no sea la 
-                    responsabilidad moral del apoyo que se recibió por los jugadores. Desde este punto 
-                    de vista los accesos anticipados representan un riesgo para los jugadores pero 
-                    también una posible pérdida pues uno de los juegos que apoyes puede convertirse 
-                    en una de las joyas indie pero hace falta tener fe en que no recibirás una basura 
-                    por tu confianza, eisten juegos como proyect zomboid lleva en acceso anticipado 
-                    alrededor de 10 años y si bien el titulo recibe actualizaciones de contenido y se 
-                    puede jugar en el estado en que se encuentra no es el caso de otros juegos que nunca 
-                    abandonaron el acceso anticipado y ni si quiera ofrecieron un contenido jugable en 
-                    caso de no completarse, leyendo la normas de steam sobre los juegos de acceso 
-                    anticipado descubrí que steam es muy claro a la hora de mostrar el proceso de que 
-                    es un juego anticipado y que no lo es, así como dejar en claro que se deben seguir 
-                    ciertos compromisos y en un apartado se menciona que se debe pensar muy bien a la hora 
-                    de cancelar un juegos anticipado pues el juego no se podrá volver a publicar en steam 
-                    y se les puede ofrecer a las personas un reembolso, esto calma un poco las inquietudes 
-                    de las personas que depositan la confianza en un título que puede resultar bueno pero 
-                    aún con eso nada te asegura que el juego no estará en desarrollo por los próximos 15 
-                    años, habrá que confiar tener fe en los desarrolladores para que terminen sus juegos 
-                    ya que seria una pena que un titulo como papers please, stardew valley o valheim 
-                    nunca vean la luz por falta de apoyo pero también debemos cuidar nuestro dinero y 
-                    no regalar recursos a quien no se lo merece bueno esto fue filas y columnas hasta 
+                    , ya
+                    antes había comprado juegos que aún estaban en desarrollo siendo valheim uno de ellos
+                    pero el título que probé en este caso me dejó un poco desanimado pues se sentía
+                    bastante incompleto lo que me molestó un poco al inicio pero recordé que aún se estaba
+                    trabajando en el juego lo cual me tranquilizo pensando en que posteriormente se
+                    tendría un proceso y que además estaba apoyando a un desarrollador indie que dispone
+                    de pocos recursos y quien sabe tal vez el juego se llegue a convertir en uno de mis
+                    favoritos solo que aún falta tiempo para completarlo. Luego de un rato me surgió la
+                    duda de que pasaría si el juego nunca continúa su desarrollo ya que si bien el título
+                    pone en la tienda de steam que es un juego sin terminar no te da una cláusula para
+                    poder regresar este tipo de juegos ya que al comprar un acceso anticipado lo haces
+                    bajo tu propio riesgo, esto parece un poco injusto ya que por una parte el
+                    desarrollador obtiene apoyo para completar su juego lo cual puede resultar una
+                    buena experiencia para ambos pero por otro lado los usuarios solo se mantienen con
+                    la promesa de que el juego se terminara en algún momento basado en la confianza que
+                    se tiene pero no existe nada que obligué a terminar el juego que no sea la
+                    responsabilidad moral del apoyo que se recibió por los jugadores. Desde este punto
+                    de vista los accesos anticipados representan un riesgo para los jugadores pero
+                    también una posible pérdida pues uno de los juegos que apoyes puede convertirse
+                    en una de las joyas indie pero hace falta tener fe en que no recibirás una basura
+                    por tu confianza, eisten juegos como proyect zomboid lleva en acceso anticipado
+                    alrededor de 10 años y si bien el titulo recibe actualizaciones de contenido y se
+                    puede jugar en el estado en que se encuentra no es el caso de otros juegos que nunca
+                    abandonaron el acceso anticipado y ni si quiera ofrecieron un contenido jugable en
+                    caso de no completarse, leyendo la normas de steam sobre los juegos de acceso
+                    anticipado descubrí que steam es muy claro a la hora de mostrar el proceso de que
+                    es un juego anticipado y que no lo es, así como dejar en claro que se deben seguir
+                    ciertos compromisos y en un apartado se menciona que se debe pensar muy bien a la hora
+                    de cancelar un juegos anticipado pues el juego no se podrá volver a publicar en steam
+                    y se les puede ofrecer a las personas un reembolso, esto calma un poco las inquietudes
+                    de las personas que depositan la confianza en un título que puede resultar bueno pero
+                    aún con eso nada te asegura que el juego no estará en desarrollo por los próximos 15
+                    años, habrá que confiar tener fe en los desarrolladores para que terminen sus juegos
+                    ya que seria una pena que un titulo como papers please, stardew valley o valheim
+                    nunca vean la luz por falta de apoyo pero también debemos cuidar nuestro dinero y
+                    no regalar recursos a quien no se lo merece bueno esto fue filas y columnas hasta
                     la próxima
                 </p>
             </div>
