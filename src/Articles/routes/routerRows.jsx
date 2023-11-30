46c5f2d25f1b863ec0fcfb60bdd2c40397c600bb
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-11-29 21:28:19 -0600

    add a new route for new article

--- src/Articles/routes/routerRows.jsx
+++ src/Articles/routes/routerRows.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Rc1 from "../pages/rows/rc1";
 import Rc2 from "../pages/rows/rc2";
+import Rc3 from "../pages/rows/rc3";
 
 function RouterRows(){
     return(
@@ -10,6 +11,8 @@ function RouterRows(){
                 <Route path="rc1" element={<Rc1 />} />
 
                 <Route path="rc2" element={<Rc2 />} />
+
+                <Route path="rc3" element={<Rc3 />} />
             </Routes>
         </>
     );
