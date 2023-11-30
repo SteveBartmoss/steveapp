d722ce318362b29c8c82902b3dcf99662af0e06a
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-11-29 21:16:24 -0600

    add a new section for the article

--- src/Articles/pages/rows/rc3.jsx
+++ src/Articles/pages/rows/rc3.jsx
@@ -53,7 +53,7 @@ function Rc3() {
                 <h1>Todo muy bien pero Completaran el juego verdad?</h1>
 
                 <hr />
-                
+
                 <div>
                     <p>
                         Luego de un rato me surgió la
@@ -69,21 +69,29 @@ function Rc3() {
                     </p>
                 </div>
 
+                <h1>Mi cartera corre peligro ?</h1>
+                <hr />
+
+                <div>
+                    <p>
+                        Desde este punto
+                        de vista los accesos anticipados representan un riesgo para los jugadores pero
+                        también una posible pérdida pues uno de los juegos que apoyes puede convertirse
+                        en una de las joyas indie pero hace falta tener fe en que no recibirás una basura
+                        por tu confianza, existen juegos como proyect zomboid lleva en acceso anticipado
+                        alrededor de 10 años y si bien el titulo recibe actualizaciones de contenido y se
+                        puede jugar en el estado en que se encuentra no es el caso de otros juegos que nunca
+                        abandonaron el acceso anticipado y ni si quiera ofrecieron un contenido jugable en
+                        caso de no completarse, leyendo la normas de steam sobre los juegos de acceso
+                        anticipado descubrí que steam es muy claro a la hora de mostrar el proceso de que
+                        es un juego anticipado y que no lo es, así como dejar en claro que se deben seguir
+                        ciertos compromisos y en un apartado se menciona que se debe pensar muy bien a la hora
+                        de cancelar un juegos anticipado pues el juego no se podrá volver a publicar en steam
+                        y se les puede ofrecer a las personas un reembolso,
+                    </p>
+                </div>
                 <p>
-                    Desde este punto
-                    de vista los accesos anticipados representan un riesgo para los jugadores pero
-                    también una posible pérdida pues uno de los juegos que apoyes puede convertirse
-                    en una de las joyas indie pero hace falta tener fe en que no recibirás una basura
-                    por tu confianza, eisten juegos como proyect zomboid lleva en acceso anticipado
-                    alrededor de 10 años y si bien el titulo recibe actualizaciones de contenido y se
-                    puede jugar en el estado en que se encuentra no es el caso de otros juegos que nunca
-                    abandonaron el acceso anticipado y ni si quiera ofrecieron un contenido jugable en
-                    caso de no completarse, leyendo la normas de steam sobre los juegos de acceso
-                    anticipado descubrí que steam es muy claro a la hora de mostrar el proceso de que
-                    es un juego anticipado y que no lo es, así como dejar en claro que se deben seguir
-                    ciertos compromisos y en un apartado se menciona que se debe pensar muy bien a la hora
-                    de cancelar un juegos anticipado pues el juego no se podrá volver a publicar en steam
-                    y se les puede ofrecer a las personas un reembolso, esto calma un poco las inquietudes
+                    esto calma un poco las inquietudes
                     de las personas que depositan la confianza en un título que puede resultar bueno pero
                     aún con eso nada te asegura que el juego no estará en desarrollo por los próximos 15
                     años, habrá que confiar tener fe en los desarrolladores para que terminen sus juegos
