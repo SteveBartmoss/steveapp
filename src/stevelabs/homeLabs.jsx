4b428a75d26548e78ee93c2ddf3a0569ea256ab7
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-04 19:09:45 -0600

    change the title of the document

--- src/stevelabs/homeLabs.jsx
+++ src/stevelabs/homeLabs.jsx
@@ -4,7 +4,7 @@ import LabsAbout from "./components/labsAbout";
 function HomeLabs() {
     return (
         <>
-        {document.title="stevelabs"}
+        {document.title="Stevelabs"}
             <div className="center">
                 <h1 className="labs-title">Steve Labs</h1>
             </div>
