bd49e820355623209494df19898a283a830af8db
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-05 20:26:55 -0600

    add more content for the article

--- src/Articles/pages/rows/rc4.jsx
+++ src/Articles/pages/rows/rc4.jsx
@@ -9,52 +9,66 @@ function Rc4() {
                     <h1 className="main-title">Los indie una nueva esperanza</h1>
                 </div>
 
-                <hr className="line"/>
+                <hr className="line" />
 
                 <CreatorSing autor={"Steve"} date={"08/04/23"} img={"/Assets/steveA.png"} grade={"Free coder"} />
 
                 <div className="back-articles">
                     <p className="content-curso">
                         Cuando era menor siempre pensaba que mi trabajo favorito sería probar Videojuegos
-                        o poder crear videojuegos y ambos casos se podian considerar como trabajos que todos 
-                        desearina pero habia un detalle y es que en ese entonces los únicos que desarrollaban 
-                        juegos eran empresas grandes como bungie, activision, blizzard o muchos otros estudios 
-                        que estaban bien establecidos y los estudios independientes no eran tan populares o 
-                        no tenían tanto éxito como actualmente se tiene. 
+                        o poder crear videojuegos y ambos casos se podian considerar como trabajos que todos
+                        desearina pero habia un detalle y es que en ese entonces los únicos que desarrollaban
+                        juegos eran empresas grandes como bungie, activision, blizzard o muchos otros estudios
+                        que estaban bien establecidos y los estudios independientes no eran tan populares o
+                        no tenían tanto éxito como actualmente se tiene.
                     </p>
 
                     <p className="content-curso">
-                        Hoy en dia es comun ver grandes titulos 
-                        que fueron desarrollados por estudios independientes como stray, papers please, the forest 
-                        entre muchos otros que hace demuestran una buena calidad y no solo se hacen populadores 
-                        por ser indies, hace poco probé pineapple on a pizza un juego de un desarrollador llamado 
-                        Alva majo o al menos así se presenta en Youtube y al probar el juego tuve una buena experiencia 
-                        aunque fue algo simple ya que en el juego solo se basa en recorrer la isla y no requiere 
-                        interactuar de forma compleja si no que basta con acercar se a los personajes que bailan 
-                        o chocar nuestro personaje con elementos del escenario.
+                        Hoy en dia es comun ver grandes titulos que fueron desarrollados por estudios independientes
+                        como stray, papers please, the forest entre muchos otros que demuestran la buena calidad que
+                        un creador independiente puede ofrecer y no solo se hacen populadores por ser indies, hace poco
+                        probé pineapple on a pizza un juego de un desarrollador llamado Alva majo o al menos así se
+                        presenta en Youtube y al probar el juego tuve una buena experiencia aunque fue algo simple ya
+                        que en el juego solo se basa en recorrer la isla y no requiere interactuar de forma compleja
+                        si no que basta con acercar se a los personajes que bailan o chocar nuestro personaje con
+                        elementos del escenario.
                     </p>
 
                 </div>
 
                 <h1 className="title1">Un cocepto simple pero bien implementado</h1>
-                <hr className="line"/>
+                <hr className="line" />
 
                 <div className="back-articles">
                     <p className="content-curso">
-                        Una mecánica muy simple considerando que recorrer la isla no tomaba mas 
-                        de unos cuantos minutos pero apesar de esto me permitió pasar un momento 
+                        Una mecánica muy simple considerando que recorrer la isla no tomaba mas
+                        de unos cuantos minutos pero apesar de esto me permitió pasar un momento
                         de diversión y entonces recordé otro título que también me había llamado la atención por
-                        su manera de implementar el gameplay y curiosamente era del mismo desarrollador, 
-                        el juego trataba de sacar una espada de una piedra así como contaba la leyenda 
-                        del rey Arturo con esto la mecánica estaba planteada pero hacía falta algo que la volviera interesante algo
-                        que animará a los jugadores a probar lo. Bueno detrás de la simple idea de solo sacar
-                        una espada había un modo de multijugador que tendría a los usuarios frustrados por
+                        su manera de implementar el gameplay y curiosamente era del mismo desarrollador,
+                        el juego trataba de sacar una espada de una piedra así como contaba la leyenda
+                        del rey Arturo con esto la mecánica estaba planteada pero hacía falta algo que la volviera
+                        interesante algo que animará a los jugadores a probar lo.
+                    </p>
+
+                    <p className="content-curso">
+                        Bueno detrás de la simple idea
+                        de solo sacar una espada había un modo de multijugador que tendría a los usuarios frustrados por
                         mucho tiempo y es que el juego mostraba una simple espada pero la longitud y el peso
-                        de la misma variaba cada que un jugador lograba sacarla lo cual hacia que los posteriores
-                        intentos fueran mucho más difíciles, lo anterior se complicaba más con la forma
+                        de la misma variaba cada que un jugador lograba sacarla, lo cual hacia que los posteriores
+                        intentos fueran mucho más difíciles lo anterior se complicaba más con la forma
                         en que se jugaba ya que la espada solo subía si desplazabas el ratón hacia adelante
-                        y mantienes presionado el botón derecho del ratón. Si por algo dejas de apretar el
-                        botón la espada cae haciendo que pierdas todo el progreso que tenías viejo no te
+                        y mantienes presionado el botón derecho del ratón si por algo dejas de apretar el
+                        botón la espada cae haciendo que pierdas todo el progreso que tenías.
+                    </p>
+                </div>
+
+                <h1 className="title1">La paz nunca fue una opcion</h1>
+
+                <hr className="line" />
+
+                <div className="back-articles">
+                    <p className="content-curso">
+                        Viejo no te
                         voy a mentir dure bastante tiempo tratando de sacar la espada en el modo sin conexión
                         y tener que explicarle a mi familia porque cuando entraban al cuarto estaba mirando
                         siempre la misma pantalla y deslizando el mouse no fue fácil, luego descubrí un truco
@@ -63,12 +77,23 @@ function Rc4() {
                         la espada al máximo y lamentablemente ya no tenía tanto tiempo pero me pareció una
                         idea buena para hacer que las personas probaran el jugador.
                     </p>
+
+                    <p className="content-curso">
+                        Al revisar un video del crador descubri que el habia programado el juego para que 
+                        la longitud y el peso de la espada bajaba con el tiempo siempre y cuando no se hicieran 
+                        mas intentos pero esto no sucedio pues la gente no dejaba de intentar sacar la espada 
+                        y debido a esto el creador tuvo que modificar el comporatamiento de la espada para que 
+                        se reiniciaran los atributos cuando un usuario sacara la espada en los valores maximos,
+                        esta claro que el creador no esperaba que tuviera tanto exito el juego pero me alegro saber
+                        que una idea tan simple tuviera tanto exito ya que esto demostro que no siempre en necesario
+                        pensar en un triple A para pasar un buen rato.
+                    </p>
                 </div>
 
                 <h1 className="title1">Menos mundos enormes y mejores mecanicas</h1>
 
-                <hr className="line"/>
-                
+                <hr className="line" />
+
                 <div className="back-articles">
                     <p className="content-curso">
 
