4446dce3e9b7cb947d89a643fd5762d5cf907ee8
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-05 19:00:25 -0600

    add style for the page

--- src/pages/changeLog.jsx
+++ src/pages/changeLog.jsx
@@ -8,10 +8,10 @@ function ChangeLog() {
 
                 <h1>Ultima actualizacion</h1>
 
-                <h1>25/11/2023</h1>
+                <h1 className="title1">25/11/2023</h1>
                 <hr />
 
-                <p>
+                <p className="content-curso">
                     Se agrego el primer articulo de las reviews el cual es sobre death stranding,
                     se agrego otro articulo de rows and colums que esta en proceso. Se seguira
                     implemetando contendio para el curso de programacion y el de linux para obtener
@@ -19,10 +19,10 @@ function ChangeLog() {
                     amanecer pero de momento nos vemos optimistas.
                 </p>
 
-                <h1>28/10/2023</h1>
+                <h1 className="title1">28/10/2023</h1>
                 <hr />
 
-                <p>
+                <p className="content-curso">
                     Se implemento una mejora en los menus de los cursos ya que ahora se trata de un
                     menu desplegable lo cual hace que el curso sea mas facil de visualizar, se sigue
                     agregando contenido al curso de programacion pero aun esta en 1% de avance, los
@@ -30,20 +30,20 @@ function ChangeLog() {
                     es mas largo que nunca pero en algun momento debe terminar?
                 </p>
 
-                <h1>31/08/2023</h1>
+                <h1 className="title1">31/08/2023</h1>
                 <hr />
 
-                <p>
+                <p className="content-curso">
                     Se continua implementando un nuevo estilo en la seccion de steve (se agrego
                     una seccion con el porcentaje de  uso de algunos lenguaes de programacion) por el
                     momento se esta viviendo un invierno en la pagina pero se espera seguir desarollando
                     la nueva version pronto, el invierno ha llegado y no se puede ver el final
                 </p>
 
-                <h1>14/08/2023</h1>
+                <h1 className="title1">14/08/2023</h1>
                 <hr />
 
-                <p>
+                <p className="content-curso">
                     Se implemento un nuevo estilo a las tajertas para las secciones de los articulos, 
                     steve labs y proximamente se implementara en las secciones de have fun, dumix y la 
                     pagina princiapal. Los colores de la pagina estan cambiando para tomar la tonalidad 
@@ -52,10 +52,10 @@ function ChangeLog() {
                     movil pero se trabajara en ellas una vez que se cierre la verison 3 de la pagina.
                 </p>
 
-                <h1>07/08/2023</h1>
+                <h1 className="title1">07/08/2023</h1>
                 <hr />
 
-                <p>
+                <p className="content-curso">
                     Se tiene implementada la sesccion de articulos rows and colums por lo que se puede acceder 
                     al primer articul o de esta seccion, se estan preparando los cambios para la nueva version 
                     de la pagina que contara con un nuevo diseno que se espera que este lista a finales de diciembre. 
@@ -64,20 +64,20 @@ function ChangeLog() {
                     implementar una tercera para esta version o en la proxima version de la pagina.
                 </p>
 
-                <h1>28/06/2023</h1>
+                <h1 className="title1">28/06/2023</h1>
                 <hr />
 
-                <p>
+                <p className="content-curso">
                     Se completo una nueva funcion en la cual se puede hacer preguntas como si de una bola magica 
                     se tratara, se mejoraron algunos estilos del curso de linux para que se vea mejor en pantalla 
                     de resolucion mayor a las 14 pulgadas, en las proximas semana se generara los builds de produccion 
                     para que se pueda desplegar la nueva aplicacion.
                 </p>
 
-                <h1>10/06/2023</h1>
+                <h1 className="title1">10/06/2023</h1>
                 <hr />
 
-                <p>
+                <p className="content-curso">
                     Se completo la funcionalidad de la aplicacion gifapp a un 90% lo cual es un porcentaje suficiente 
                     para poder cerrar la etapa de desarrollo y empezar dar estilos a la aplicacion para que tenga una 
                     mejor presentacion, se estara trabajando en este ultimo apartado. Se completo el tema de directorios 
@@ -86,92 +86,93 @@ function ChangeLog() {
                     estara trabajando en el proximo tema que es archivos.
                 </p>
 
-                <h1>31/05/2023</h1>
+                <h1 className="title1">31/05/2023</h1>
                 <hr />
 
-                <p>
+                <p className="content-curso">
                     Ya se completo la descripcion de la estrucutra de directorios del sistema linux, se tiene un progreso 
                     del 30% para la aplicacion gifapp de la section have fun, faltan estilos para la aplicacion de gifapp 
                     pero se trabajara en eso una vez que se tenga terminada la funcionalidad de la pagina.
                 </p>
 
-                <h1>17/05/2023</h1>
+                <h1 className="title1">17/05/2023</h1>
                 <hr />
 
-                <p>
+                <p className="content-curso">
                     Ya se completaron algunos temas del curso de linux pero aun pueden estaer sujetos a cambios, hace falta 
                     invertir mas tiempo en el css pero se trabajara posteriormente, las paginas de have fun aun sigen en espero 
                     pero se pondra su implementacion los mas pronto posible. El build de produccion aun no se genera pero cuando 
                     se tenga terminado el curso de linux se generara el build de producccion.
                 </p>
 
-                <h1>08/05/2023</h1>
+                <h1 className="title1">08/05/2023</h1>
                 <hr />
 
-                <p>
+                <p className="content-curso">
                     Ya se definieron algunas nuevas aplicaciones para el apartado have fun, estara en desarrollo el modulo de 
                     gifapp como primer proyecto.
                 </p>
 
-                <h1>03/05/2023</h1>
+                <h1 className="title1">03/05/2023</h1>
                 <hr />
 
-                <p>
+                <p className="content-curso">
                     Un nuevo modulo de la pagina se implementara con el nombre de have fun que tiene como objetivo practicar el 
                     uso de las apis por parte del desarrollador y que dara un poco de entretenimiento para los usuarios.
                 </p>
 
-                <h1>17/04/2023</h1>
+                <h1 className="title1">17/04/2023</h1>
                 <hr />
 
-                <p>
+                <p className="content-curso">
                     Dumix ha llegado, se trata de una serie de paginas que compartiran con los usuarios conocimientos sobre 
                     comandos basicos y que puede ser de ayuda para usuarios inexperstos o que solo quieren corroborar el 
                     funcionamiento de algun comando y a pesar de que el nombre de la seccion puede resultar ofensivo la 
                     intension no es esa.
                 </p>
 
-                <h1>16/04/2023</h1>
+                <h1 className="title1">16/04/2023</h1>
                 <hr />
 
-                <p>
+                <p className="content-curso">
                     Se cierra la version dos de la pagina en la cual se cuenta con un nuevo diseno de la pagina home, steve, 
                     un nuevo apartado como los articulos de row and colums y se implementan reseñas de juegos que no requieren un 
                     analsis tan complejo, el seguimiento de este archivo no se habia tomado en cuenta pero se trata de llevar una 
                     mejor constancia.
                 </p>
 
-                <h1>4/03/2023</h1>
+                <h1 className="title1">4/03/2023</h1>
                 <hr />
 
-                <p>
+                <p className="content-curso">
                     Se implemente un nuevo diseno para el apartado de esteve, se tiene un portada y proximamente de pondra una malla 
                     para almacenar difentes elementos, aun no se decide los fondos ni los colores definitivos. La pagina home ya tiene 
                     un diseño definitivo y se estara trabajando en el css para que result mas llamtivo o mejor para el usuario.
                 </p>
 
-                <h1>1/03/2023</h1>
+                <h1 className="title1">1/03/2023</h1>
                 <hr />
 
-                <p>
+                <p className="content-curso">
                     Se cierra la version uno de la pagina y se deja la version en la rama v1 para poder desplegar esa version en
                     caso de tener problemas o si se requiere hacer una consulta, la verison uno es la que esta actualmente desplegada en 
                     el hosting, la version dos esta en desarrollo y sera desplegada tan pronto como se termine.
                 </p>
 
-                <h1>6/02/2023</h1>
+                <h1 className="title1">6/02/2023</h1>
                 <hr />
 
-                <p>Se implemento un carrusel para la informacion de steve, aun esta en desarrollo y falta definir 
+                <p className="content-curso">
+                    Se implemento un carrusel para la informacion de steve, aun esta en desarrollo y falta definir 
                     los estilos, la funcionalidad ya esta completa y se creo un hook para menejar el cambio de las 
                     tajetas y que al llegar al final de carrusel que se reinicie y si esta al principio y retrocede 
                     valla al final.
                 </p>
 
-                <h1>13/01/2023</h1>
+                <h1 className="title1">13/01/2023</h1>
                 <hr />
 
-                <p>
+                <p className="content-curso">
                     La pagina fue creada y desplegada en netlify y puede ser consultada, cuenta con las siguentes 
                     secciones:home, about, news, almanac, steve.
                 </p>
