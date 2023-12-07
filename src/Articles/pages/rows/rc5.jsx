c710402fa84c4b2af3f4bdb08a2deda60c07124b
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-07 13:18:40 -0600

    add more paragraphs for more readable

--- src/Articles/pages/rows/rc5.jsx
+++ src/Articles/pages/rows/rc5.jsx
@@ -10,7 +10,7 @@ function Rc5() {
                     <h1 className="main-title">Los 16 gigas de ram no son lo que eran</h1>
                 </div>
 
-                <hr className="line"/>
+                <hr className="line" />
 
                 <CreatorSing autor={"Steve"} date={"08/04/23"} img={"/Assets/steveA.png"} grade={"Free coder"} />
 
@@ -20,8 +20,8 @@ function Rc5() {
                         tenia que aumentar la ram de mi computadora para que fuera mas rápida, por mucho
                         tiempo no entendia la relación que tenia ese componente con la rapidez de una
                         computadora y actualmente se que hay otros factores que influyen como por ejemplo
-                        el tener un estado solido para que el sistema operativo carge mas rapido pero por 
-                        mucho tiempo tuve una computadora que tenia solo dos gigas de ram y un proncesador 
+                        el tener un estado solido para que el sistema operativo carge mas rapido pero por
+                        mucho tiempo tuve una computadora que tenia solo dos gigas de ram y un proncesador
                         celero o algun otro que no era muy potente, esto me permita jugar cosas simples
                         como plantas contra zombies o age of empires pero cuando pude armar una computadora
                         me decidi a instalar una memoria de ocho gigas lo cual era mucha memoria para mi
@@ -33,57 +33,64 @@ function Rc5() {
 
                 <h1 className="title1">Ya no tienes poder sobre tu memoria ram</h1>
 
-                <hr className="line"/>
+                <hr className="line" />
 
                 <div className="back-articles">
                     <p className="content-curso">
                         No hace mucho me puse a revisar los recursos de mi computadora
                         mientras jugaba y me dio un gran susto ver que el total de uso de ram era de 15
-                        gigas y apenas quedaba libre unos cuantos megas de memoria, bueno tampoco fue un susto 
-                        pero si me lleve una sorpresa que no fue porque el juego me fuera mal o porque mi 
-                        computadora se quedara congelada si no porque creía que no estaba usando toda la ram de 
-                        mi computadora y hasta sentía que no hubiera gastado dinero en mas memoria ram. 
-                        Esto me hizo darme cuenta que los gloriosos 16 gigas ya no son la que antes era si no 
-                        que son el nuevo requerimiento mínimo lo cual no suena mal hasta qyeu te pones a pensar 
-                        que realmente se minimo puede llegar a ser necesario solo para jugar, ya que si bien yo 
-                        estaba haciendo bastante uso de la computadora (tenia fornite abierto, escuchando música 
+                        gigas y apenas quedaba libre unos cuantos megas de memoria, bueno tampoco fue un susto
+                        pero si me lleve una sorpresa que no fue porque el juego me fuera mal o porque mi
+                        computadora se quedara congelada si no porque creía que no estaba usando toda la ram de
+                        mi computadora y hasta sentía que no hubiera gastado dinero en mas memoria ram.
+                    </p>
+
+                    <p className="content-curso">
+                        Esto me hizo darme cuenta que los gloriosos 16 gigas ya no son la que antes era si no
+                        que son el nuevo requerimiento mínimo lo cual no suena mal hasta qyeu te pones a pensar
+                        que realmente se minimo puede llegar a ser necesario solo para jugar, ya que si bien yo
+                        estaba haciendo bastante uso de la computadora (tenia fornite abierto, escuchando música
                         en youtube, discord y un alguna otra aplicación) no esperaba ver mi memoria casi al limite.
                     </p>
                 </div>
 
                 <h1 className="title1">Un mejor manejo de los recursos es necesario</h1>
 
-                <hr className="line"/>
+                <hr className="line" />
 
                 <div className="back-articles">
                     <p className="content-curso">
-                        Despues de ver que mi memoria estaba al maximo me puse revisar que era lo que mas consumia 
-                        y luego de abrir el administrador de tareas y observar el uso de la memoria me di cuenta 
-                        que la mayoría del tiempo el sistema usa cinco gigas sin tener abierto nada si no solamente 
-                        al iniciar el sistema, lo cual es un poco molesto ya que Windows debería poner mas atención 
+                        Despues de ver que mi memoria estaba al maximo me puse revisar que era lo que mas consumia
+                        y luego de abrir el administrador de tareas y observar el uso de la memoria me di cuenta
+                        que la mayoría del tiempo el sistema usa cinco gigas sin tener abierto nada si no solamente
+                        al iniciar el sistema, lo cual es un poco molesto ya que Windows debería poner mas atención
                         a esto y hacer que se puedan desactivar muchos procesos que consumen ram y otros recursos ya
                         que la mayoría de usuarios no utilizan todos los servicios dejando libre más recursos
-                        para juegos o alguna otra tarea de los usuarios. Luego de ver esto me hace pensar que realmente 
+                        para juegos o alguna otra tarea de los usuarios.
+                    </p>
+
+                    <p className="content-curso">
+                        Luego de ver esto me hace pensar que realmente
                         el mercado está lleno de computadoras que no funcionaran correctamente pues es común
                         encontrar computadoras que solo tienen 4 gigas y dentro de esto se encuentran muchas
                         laptops que la mayoría de usuarios inexpertos compraran y lamentablemente se toparan
                         con la realidad de que su computadora se vuelve lenta ya que Windows no tiene nada de
-                        optimización y deja libre apenas unos cuantos megas para el uso del usuario, puede que suene 
-                        como el típico linuxero molesto pero es impresionante tener un sistema operativo que consume 
-                        menos de un giga de memoria ram me dejo impresionado porque estaba acostumbrado a que el 
+                        optimización y deja libre apenas unos cuantos megas para el uso del usuario, puede que suene
+                        como el típico linuxero molesto pero es impresionante tener un sistema operativo que consume
+                        menos de un giga de memoria ram me dejo impresionado porque estaba acostumbrado a que el
                         sistema usara la mitad o mas de la mitad de la ram del equipo.
                     </p>
                 </div>
 
                 <h1 className="title1">De nuevo gnu salva el dia</h1>
 
-                <hr className="line"/>
+                <hr className="line" />
 
                 <div className="back-articles">
                     <p className="content-curso">
-                        Lo primero que me impresiono la primera ves que instale ubunti era ver que tenía a mi disposición 
-                        muchos mas recursos ya que esta ni siquiera es la version mas ligera y aun asi no utilizaba no la mitad 
-                        de la memoria total del equipo ademas de que tenia un mejor manejo de la carga ya que no llego colgarse a 
+                        Lo primero que me impresiono la primera ves que instale ubunti era ver que tenía a mi disposición
+                        muchos mas recursos ya que esta ni siquiera es la version mas ligera y aun asi no utilizaba no la mitad
+                        de la memoria total del equipo ademas de que tenia un mejor manejo de la carga ya que no llego colgarse a
                         pesar de que aumente inicie multiples procesos y el equipo solo tenia dos nucleos, todo esto me hizo
                         pensar que empresas como Microsoft deberían tomar en cuenta a sus usuarios y darles la
                         posibilidad de tener un Windows lite, o al menos optimizar mejor su sistema operativo
@@ -97,23 +104,27 @@ function Rc5() {
 
                 <h1 className="title1">Las desarrolladoras no ayudan</h1>
 
-                <hr className="line"/>
+                <hr className="line" />
 
                 <div className="back-articles">
                     <p className="content-curso">
-                        Si bien una gran parte de la culpa la tiene el sistema operativo ya que es el que mayor 
-                        ram puede utilizar las desarrolladoras de juegos tampoco ponen de su parte ya que muchos 
-                        juegos recientes o de la generación pasada piden como minimo ocho gigas de ram que si lo 
-                        sumamos a la cantidad de ram que usa Windows tenemos que el minimo de ram se ha convertido 
-                        en diez y seis gigas como nuevo minimo y quizá en un futuro 32 sea el nuevo minimo si es 
-                        que las empresas no hacen lo posible por optmizar sus productos, bueno esto puede sonar 
-                        dramático pero no es nada nuevo que los estándares de hardware están subiendo pero al mismo 
-                        tiempo este tipo de recursos se vuelven mas accesible para las personas pero no estaría 
-                        mal dar un poco de aire a los componentes con una buena optimizacion y no tener que actualizar 
-                        los componentes en por lo menos 5 años a no ser que pronto lleguemos a un nuevo salto de 
-                        generación como lo fue el paso de los equipos de kilobites de ram y procesadores de 32 bits 
-                        a la nueva generación con multiples hilos y nucleos pero esto no es fácil de predecir, por el 
-                        momento estamos limitados a aceptar el nuevo minimo de ram, bueno esto fue filas y columnas hasta 
+                        Si bien una gran parte de la culpa la tiene el sistema operativo ya que es el que mayor
+                        ram puede utilizar las desarrolladoras de juegos tampoco ponen de su parte ya que muchos
+                        juegos recientes o de la generación pasada piden como minimo ocho gigas de ram que si lo
+                        sumamos a la cantidad de ram que usa Windows tenemos que el minimo de ram se ha convertido
+                        en diez y seis gigas como nuevo minimo y quizá en un futuro 32 sea el nuevo minimo si es
+                        que las empresas no hacen lo posible por optmizar sus productos.
+                    </p>
+
+                    <p className="content-curso">
+                        Bueno esto puede sonar
+                        dramático pero no es nada nuevo que los estándares de hardware están subiendo pero al mismo
+                        tiempo este tipo de recursos se vuelven mas accesible para las personas pero no estaría
+                        mal dar un poco de aire a los componentes con una buena optimizacion y no tener que actualizar
+                        los componentes en por lo menos 5 años a no ser que pronto lleguemos a un nuevo salto de
+                        generación como lo fue el paso de los equipos de kilobites de ram y procesadores de 32 bits
+                        a la nueva generación con multiples hilos y nucleos pero esto no es fácil de predecir, por el
+                        momento estamos limitados a aceptar el nuevo minimo de ram, bueno esto fue filas y columnas hasta
                         la próxima.
                     </p>
                 </div>
