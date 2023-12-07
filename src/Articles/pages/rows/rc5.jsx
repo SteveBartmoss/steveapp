03b6c5348e8c500e5ef06e68f8d208c821e016a9
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-06 19:01:57 -0600

    add a structure for the page

--- src/Articles/pages/rows/rc5.jsx
+++ src/Articles/pages/rows/rc5.jsx
@@ -1,69 +1,116 @@
 import React from "react";
+import CreatorSing from "../../../component/sing";
 
-function Rc5(){
-    return(
+function Rc5() {
+    return (
         <>
-        <div>
-            <h1>Los 16 gigas de ram no son lo que eran</h1>
+            <div className="div-about">
+                <div>
+                    <h1>Los 16 gigas de ram no son lo que eran</h1>
+                </div>
 
-            <div>
-                <p>
-                    Cuando era mas joven y no sabia mucho sobre computadoras siempre me decian que 
-                    tenia que aumentar la ram de mi computadora para que fuera mas rápida, por mucho 
-                    tiempo no entendia la relación que tenia ese componente con la rapidez de una 
-                    computadora y actualmente se que hay otros factores que influyen como por ejemplo 
-                    el tener un estado solido para el sistema operativo pero por mucho tiempo tuve una 
-                    computadora que tenia solo dos gigas de ram, esto me permita jugar cosas simples 
-                    como plantas contra zombies o age of empires pero cuando pude armar un computadora 
-                    me decidi a instalar una memoria de ocho gigas lo cual era mucha memoria para mi 
-                    pero posteriormente logre comprar otro ocho gigas para por fin completar 16 gigas 
-                    que en teoría prometían ser la libertad de almacenamiento volátil pero actualmente 
-                    esto ya no es asi. No hace mucho me puse a revisar los recursos de mi computadora 
-                    mientras jugaba y me dio un gran susto ver que el total de uso de ram era de 15 
-                    gigas y apenas quedaba libre unos cuantos megas de memoria, mi sorpresa no fue porque 
-                    el juego me fuera mal o porque mi computadora se quedara congelada si no porque creía 
-                    que no estaba usando toda la ram de mi computadora y hasta sentía que no hubiera 
-                    gastado dinero en mas memoria ram pero esto me hizo darme cuenta que los gloriosos 16 
-                    gigas ya no son la que antes era si no que son el nuevo requerimiento mínimo. Esto me 
-                    deja un poco desconcertado ya que si bien yo estaba haciendo bastante uso de la 
-                    computadora (tenia fornite abierto, escuchando música en youtube, discord y un 
-                    alguna otra aplicación) no esperaba ver mi memoria casi al limite y luego de 
-                    observar el uso de la memoria con el administrador de tareas me di cuenta que la mayoría 
-                    del tiempo el sistema usa cinco gigas sin tener abierto si no solamente al iniciar 
-                    el sistema lo cual es un poco molesto ya que Windows debería poner mas atención a esto 
-                    y hacer que se puedan desactivar muchos procesos que consumen ram y otros recursos ya 
-                    que la mayoría de usuarios no utilizan todos los servicios dejando libre más recursos 
-                    para juegos o alguna otra tarea de los usuarios, esto me hace pensar que realmente el 
-                    mercado está lleno de computadoras que no funcionaran correctamente pues es común 
-                    encontrar computadoras que solo tienen 4 gigas y dentro de esto se encuentran muchas 
-                    laptops que la mayoría de usuarios inexpertos compraran y lamentablemente se toparan 
-                    con la realidad de que su computadora se vuelve lenta ya que Windows no tiene nada de 
-                    optimización y puede que suene como el típico linuxero molesto pero es impresionante 
-                    tener un sistema operativo que consume menos de un giga de memoria ram me dejo 
-                    impresionado porque estab acostumbrado a que el sistema usara la mitad o mas de la 
-                    mitad de la ram del equipo ver que tenía a mi disposición muchos mas recursos me hizo 
-                    pensar que empresas como Microsoft deberían tomar en cuenta a sus usuarios y darles la 
-                    posibilidad de tener un Windows lite, o al menos optimizar mejor su sistema operativo 
-                    para laptops que son mas difíciles de actualizar con mejores componentes, lo anterior no 
-                    solo ocurre con Linux si no que sistemas como mac os funcionan bien con sus recursos 
-                    aunque no sean precisamente los mejores lo cual demuestra que si la empresa que desarrolla 
-                    el software se lo propone puede llegar a dar un buen producto a sus usuarios pero en el 
-                    caso de Windows esto está lejos de ser verdad, por otro lado las desarrolladoras de juegos 
-                    tampoco ponen de su parte ya que muchos juegos recientes o de la generación pasada 
-                    piden como minimo ocho gigas de ram que si lo sumamos a la cantidad de ram que usa 
-                    Windows tenemos que el minimo de ram se ha convertido en diez y seis gigas como nuevo 
-                    minimo y quizá en un futuro 32 sea el nuevo minimo si es que las empresas no hacen lo 
-                    posible por optmizar sus productos, bueno esto puede sonar dramático pero no es nada 
-                    nuevo que los estándares de hardware están subiendo pero al mismo tiempo este se vuelve 
-                    mas accesible para las personas pero no estaría mal dar un poco de aire a los componentes 
-                    y no tener que actualizar los componentes en por lo menos 5 años a no ser que pronto 
-                    lleguemos a un nuevo salto de generación como lo fue el paso de los equipos de kilobites 
-                    de ram y procesadores de 32 bits a la nueva generación con multiples hilos y nucleos pero 
-                    esto no es fácil de predecir, por el momento estamos limitados a aceptar el nuevo minimo 
-                    de ram, bueno esto fue filas y columnas hasta la próxima
-                </p>
+                <hr />
+
+                <CreatorSing autor={"Steve"} date={"08/04/23"} img={"/Assets/steveA.png"} grade={"Free coder"} />
+
+                <div>
+                    <p>
+                        Cuando era mas joven y no sabia mucho sobre computadoras siempre me decian que
+                        tenia que aumentar la ram de mi computadora para que fuera mas rápida, por mucho
+                        tiempo no entendia la relación que tenia ese componente con la rapidez de una
+                        computadora y actualmente se que hay otros factores que influyen como por ejemplo
+                        el tener un estado solido para el sistema operativo pero por mucho tiempo tuve una
+                        computadora que tenia solo dos gigas de ram, esto me permita jugar cosas simples
+                        como plantas contra zombies o age of empires pero cuando pude armar un computadora
+                        me decidi a instalar una memoria de ocho gigas lo cual era mucha memoria para mi
+                        pero posteriormente logre comprar otro ocho gigas para por fin completar 16 gigas
+                        que en teoría prometían ser la libertad de almacenamiento volátil pero actualmente
+                        esto ya no es asi.
+                    </p>
+                </div>
+
+                <h1>Ya no tienes poder sobre tu memoria ram</h1>
+
+                <hr />
+
+                <div>
+                    <p>
+                        No hace mucho me puse a revisar los recursos de mi computadora
+                        mientras jugaba y me dio un gran susto ver que el total de uso de ram era de 15
+                        gigas y apenas quedaba libre unos cuantos megas de memoria, mi sorpresa no fue porque
+                        el juego me fuera mal o porque mi computadora se quedara congelada si no porque creía
+                        que no estaba usando toda la ram de mi computadora y hasta sentía que no hubiera
+                        gastado dinero en mas memoria ram pero esto me hizo darme cuenta que los gloriosos 16
+                        gigas ya no son la que antes era si no que son el nuevo requerimiento mínimo. Esto me
+                        deja un poco desconcertado ya que si bien yo estaba haciendo bastante uso de la
+                        computadora (tenia fornite abierto, escuchando música en youtube, discord y un
+                        alguna otra aplicación) no esperaba ver mi memoria casi al limite
+                    </p>
+                </div>
+
+                <h1>Un mejor manejo de los recursos es necasrio</h1>
+
+                <hr />
+
+                <div>
+                    <p>
+                        y luego de
+                        observar el uso de la memoria con el administrador de tareas me di cuenta que la mayoría
+                        del tiempo el sistema usa cinco gigas sin tener abierto si no solamente al iniciar
+                        el sistema lo cual es un poco molesto ya que Windows debería poner mas atención a esto
+                        y hacer que se puedan desactivar muchos procesos que consumen ram y otros recursos ya
+                        que la mayoría de usuarios no utilizan todos los servicios dejando libre más recursos
+                        para juegos o alguna otra tarea de los usuarios, esto me hace pensar que realmente el
+                        mercado está lleno de computadoras que no funcionaran correctamente pues es común
+                        encontrar computadoras que solo tienen 4 gigas y dentro de esto se encuentran muchas
+                        laptops que la mayoría de usuarios inexpertos compraran y lamentablemente se toparan
+                        con la realidad de que su computadora se vuelve lenta ya que Windows no tiene nada de
+                        optimización y puede que suene como el típico linuxero molesto pero es impresionante
+                        tener un sistema operativo que consume menos de un giga de memoria ram me dejo
+                        impresionado porque estab acostumbrado a que el sistema usara la mitad o mas de la
+                        mitad de la ram del equipo
+                    </p>
+                </div>
+
+                <h1>De nuevo gnu salva el dia</h1>
+
+                <hr />
+
+                <div>
+                    <p>
+                        ver que tenía a mi disposición muchos mas recursos me hizo
+                        pensar que empresas como Microsoft deberían tomar en cuenta a sus usuarios y darles la
+                        posibilidad de tener un Windows lite, o al menos optimizar mejor su sistema operativo
+                        para laptops que son mas difíciles de actualizar con mejores componentes, lo anterior no
+                        solo ocurre con Linux si no que sistemas como mac os funcionan bien con sus recursos
+                        aunque no sean precisamente los mejores lo cual demuestra que si la empresa que desarrolla
+                        el software se lo propone puede llegar a dar un buen producto a sus usuarios pero en el
+                        caso de Windows esto está lejos de ser verdad,
+                    </p>
+                </div>
+
+                <h1>Las desarrolladoras no ayudan</h1>
+
+                <hr />
+                
+                <div>
+                    <p>
+                        por otro lado las desarrolladoras de juegos
+                        tampoco ponen de su parte ya que muchos juegos recientes o de la generación pasada
+                        piden como minimo ocho gigas de ram que si lo sumamos a la cantidad de ram que usa
+                        Windows tenemos que el minimo de ram se ha convertido en diez y seis gigas como nuevo
+                        minimo y quizá en un futuro 32 sea el nuevo minimo si es que las empresas no hacen lo
+                        posible por optmizar sus productos, bueno esto puede sonar dramático pero no es nada
+                        nuevo que los estándares de hardware están subiendo pero al mismo tiempo este se vuelve
+                        mas accesible para las personas pero no estaría mal dar un poco de aire a los componentes
+                        y no tener que actualizar los componentes en por lo menos 5 años a no ser que pronto
+                        lleguemos a un nuevo salto de generación como lo fue el paso de los equipos de kilobites
+                        de ram y procesadores de 32 bits a la nueva generación con multiples hilos y nucleos pero
+                        esto no es fácil de predecir, por el momento estamos limitados a aceptar el nuevo minimo
+                        de ram, bueno esto fue filas y columnas hasta la próxima
+                    </p>
+                </div>
             </div>
-        </div>
         </>
     );
 }
