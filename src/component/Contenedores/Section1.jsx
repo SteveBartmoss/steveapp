c72f1e6692bbd992b78dd7a8746f647ccced0111
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-08 19:47:27 -0600

    add a new folder for containers, add a two containers for the main subjects

---
+++ src/component/Contenedores/Section1.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Section1({children}){
+    return(
+        <div className="section-1">
+            {children}
+        </div>
+    );
+}
---
+++ src/component/Contenedores/Section2.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Section2({children}){
+    return(
+        <div className="section-2">
+            {children}
+        </div>
+    )
+}
