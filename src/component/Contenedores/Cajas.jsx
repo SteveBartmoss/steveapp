241c1cf5216ec682f18bb27fe40ba3475041368b
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-09 16:19:49 -0600

    add a new file for the components of divs

---
+++ src/component/Contenedores/Cajas.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../App.css";
+
+export function Paper({children}){
+    return(
+        <div className="">
+            {children}
+        </div>
+    );
+}
