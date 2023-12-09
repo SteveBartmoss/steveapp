22fab9d753f5b4ca707f01e2a5aa488bbae10f1e
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-08 21:14:23 -0600

    add more sections for the page home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,6 +10,7 @@ import Section1 from "../component/Contenedores/Section1";
 import FlexDiv from "../component/Contenedores/FlexDiv";
 import SquareDiv from "../component/Contenedores/SquareDiv";
 import CardDiv from "../component/Contenedores/CardDiv";
+import Section2 from "../component/Contenedores/Section2";
 
 
 
@@ -18,24 +19,25 @@ function Home() {
     return (
         <>
             {document.title = "stevechambitas"}
-            <DivBaner name="Baner" />
-            <Section1>
-                <FlexDiv>
-                    <h1 className="title-1">Videos con mas visitas</h1>
-                    <p className="p-main">
-                        Algunos de los videos mas populares del canal,
-                        escoge uno y pasa un buen rato mientras obtienes
-                        algo de informacion.
-                    </p>
-                </FlexDiv>
+            <div className="paper">
+                <DivBaner name="Baner" />
+                <Section1>
+                    <FlexDiv>
+                        <h1 className="title-1">Videos con mas visitas</h1>
+                        <p className="p-main">
+                            Algunos de los videos mas populares del canal,
+                            escoge uno y pasa un buen rato mientras obtienes
+                            algo de informacion.
+                        </p>
+                    </FlexDiv>
 
-                <SquareDiv>
-                        
+                    <SquareDiv>
+
                         <CardDiv>
                             <h1>Borrar System32</h1>
                             <p>
-                                La popular broma de borrar System32 para que tu pc 
-                                sea mas rapida puede generar mas problemas de lo 
+                                La popular broma de borrar System32 para que tu pc
+                                sea mas rapida puede generar mas problemas de lo
                                 que crees
                             </p>
                             <a>Ir al video</a>
@@ -44,8 +46,8 @@ function Home() {
                         <CardDiv>
                             <h1>Borrar System32</h1>
                             <p>
-                                La popular broma de borrar System32 para que tu pc 
-                                sea mas rapida puede generar mas problemas de lo 
+                                La popular broma de borrar System32 para que tu pc
+                                sea mas rapida puede generar mas problemas de lo
                                 que crees
                             </p>
                             <a>Ir al video</a>
@@ -54,8 +56,8 @@ function Home() {
                         <CardDiv>
                             <h1>Borrar System32</h1>
                             <p>
-                                La popular broma de borrar System32 para que tu pc 
-                                sea mas rapida puede generar mas problemas de lo 
+                                La popular broma de borrar System32 para que tu pc
+                                sea mas rapida puede generar mas problemas de lo
                                 que crees
                             </p>
                             <a>Ir al video</a>
@@ -64,8 +66,8 @@ function Home() {
                         <CardDiv>
                             <h1>Borrar System32</h1>
                             <p>
-                                La popular broma de borrar System32 para que tu pc 
-                                sea mas rapida puede generar mas problemas de lo 
+                                La popular broma de borrar System32 para que tu pc
+                                sea mas rapida puede generar mas problemas de lo
                                 que crees
                             </p>
                             <a>Ir al video</a>
@@ -73,7 +75,176 @@ function Home() {
 
                     </SquareDiv>
 
-            </Section1>
+                </Section1>
+
+                <Section2>
+                    <FlexDiv>
+                        <h1 className="title-1">Rows And Colums</h1>
+                        <p className="p-main">
+                            Algunos de los videos mas populares del canal,
+                            escoge uno y pasa un buen rato mientras obtienes
+                            algo de informacion.
+                        </p>
+                    </FlexDiv>
+
+                    <SquareDiv>
+
+                        <CardDiv>
+                            <h1>Borrar System32</h1>
+                            <p>
+                                La popular broma de borrar System32 para que tu pc
+                                sea mas rapida puede generar mas problemas de lo
+                                que crees
+                            </p>
+                            <a>Ir al video</a>
+                        </CardDiv>
+
+                        <CardDiv>
+                            <h1>Borrar System32</h1>
+                            <p>
+                                La popular broma de borrar System32 para que tu pc
+                                sea mas rapida puede generar mas problemas de lo
+                                que crees
+                            </p>
+                            <a>Ir al video</a>
+                        </CardDiv>
+
+                        <CardDiv>
+                            <h1>Borrar System32</h1>
+                            <p>
+                                La popular broma de borrar System32 para que tu pc
+                                sea mas rapida puede generar mas problemas de lo
+                                que crees
+                            </p>
+                            <a>Ir al video</a>
+                        </CardDiv>
+
+                        <CardDiv>
+                            <h1>Borrar System32</h1>
+                            <p>
+                                La popular broma de borrar System32 para que tu pc
+                                sea mas rapida puede generar mas problemas de lo
+                                que crees
+                            </p>
+                            <a>Ir al video</a>
+                        </CardDiv>
+
+                    </SquareDiv>
+
+                </Section2>
+
+                <Section1>
+                    <FlexDiv>
+                        <h1 className="title-1">Videos con pocas reproducciones</h1>
+                        <p className="p-main">
+                            Algunos de los videos mas populares del canal,
+                            escoge uno y pasa un buen rato mientras obtienes
+                            algo de informacion.
+                        </p>
+                    </FlexDiv>
+
+                    <SquareDiv>
+
+                        <CardDiv>
+                            <h1>Borrar System32</h1>
+                            <p>
+                                La popular broma de borrar System32 para que tu pc
+                                sea mas rapida puede generar mas problemas de lo
+                                que crees
+                            </p>
+                            <a>Ir al video</a>
+                        </CardDiv>
+
+                        <CardDiv>
+                            <h1>Borrar System32</h1>
+                            <p>
+                                La popular broma de borrar System32 para que tu pc
+                                sea mas rapida puede generar mas problemas de lo
+                                que crees
+                            </p>
+                            <a>Ir al video</a>
+                        </CardDiv>
+
+                        <CardDiv>
+                            <h1>Borrar System32</h1>
+                            <p>
+                                La popular broma de borrar System32 para que tu pc
+                                sea mas rapida puede generar mas problemas de lo
+                                que crees
+                            </p>
+                            <a>Ir al video</a>
+                        </CardDiv>
+
+                        <CardDiv>
+                            <h1>Borrar System32</h1>
+                            <p>
+                                La popular broma de borrar System32 para que tu pc
+                                sea mas rapida puede generar mas problemas de lo
+                                que crees
+                            </p>
+                            <a>Ir al video</a>
+                        </CardDiv>
+
+                    </SquareDiv>
+
+                </Section1>
+
+                <Section2>
+                    <FlexDiv>
+                        <h1 className="title-1">Reviews mas Recientes</h1>
+                        <p className="p-main">
+                            Algunos de los videos mas populares del canal,
+                            escoge uno y pasa un buen rato mientras obtienes
+                            algo de informacion.
+                        </p>
+                    </FlexDiv>
+
+                    <SquareDiv>
+
+                        <CardDiv>
+                            <h1>Borrar System32</h1>
+                            <p>
+                                La popular broma de borrar System32 para que tu pc
+                                sea mas rapida puede generar mas problemas de lo
+                                que crees
+                            </p>
+                            <a>Ir al video</a>
+                        </CardDiv>
+
+                        <CardDiv>
+                            <h1>Borrar System32</h1>
+                            <p>
+                                La popular broma de borrar System32 para que tu pc
+                                sea mas rapida puede generar mas problemas de lo
+                                que crees
+                            </p>
+                            <a>Ir al video</a>
+                        </CardDiv>
+
+                        <CardDiv>
+                            <h1>Borrar System32</h1>
+                            <p>
+                                La popular broma de borrar System32 para que tu pc
+                                sea mas rapida puede generar mas problemas de lo
+                                que crees
+                            </p>
+                            <a>Ir al video</a>
+                        </CardDiv>
+
+                        <CardDiv>
+                            <h1>Borrar System32</h1>
+                            <p>
+                                La popular broma de borrar System32 para que tu pc
+                                sea mas rapida puede generar mas problemas de lo
+                                que crees
+                            </p>
+                            <a>Ir al video</a>
+                        </CardDiv>
+
+                    </SquareDiv>
+                </Section2>
+
+            </div>
             <div className="Contenedor">
                 <h3 className="title1">Temas populares del canal</h3>
                 <MenuItems />
