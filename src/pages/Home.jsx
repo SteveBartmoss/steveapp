9c24b273b5e444ed06c8a97e6d5b3a409d2835f1
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-08 20:53:33 -0600

    add new sections and new styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,32 +7,81 @@ import Branding from "../component/Branding";
 import DivBaner from "../component/DivBaner";
 import MenuItems from "../component/MenuItem";
 import Section1 from "../component/Contenedores/Section1";
+import FlexDiv from "../component/Contenedores/FlexDiv";
+import SquareDiv from "../component/Contenedores/SquareDiv";
+import CardDiv from "../component/Contenedores/CardDiv";
 
 
 
 
-function Home(){
-    return(
+function Home() {
+    return (
         <>
-        {document.title="stevechambitas"}
-        <DivBaner name="Baner" />
-        <Section1>
-            <h1 className="title-1">Videos con mas visitas</h1>
-            <p className="p-main">
-                Algunos de los videos mas populares del canal, 
-                escoge uno y pasa un buen rato mientras obtienes 
-                algo de informacion.
-            </p>
-            
-        </Section1>
-        <div className="Contenedor">
-            <h3 className="title1">Temas populares del canal</h3>
-            <MenuItems />   
-        </div>
-        <div className="Contenedor">
-            <h3 className="title1">Reviews mas recientes</h3>
-        </div>
-        <Pie/>
+            {document.title = "stevechambitas"}
+            <DivBaner name="Baner" />
+            <Section1>
+                <FlexDiv>
+                    <h1 className="title-1">Videos con mas visitas</h1>
+                    <p className="p-main">
+                        Algunos de los videos mas populares del canal,
+                        escoge uno y pasa un buen rato mientras obtienes
+                        algo de informacion.
+                    </p>
+                </FlexDiv>
+
+                <SquareDiv>
+                        
+                        <CardDiv>
+                            <h1>Borrar System32</h1>
+                            <p>
+                                La popular broma de borrar System32 para que tu pc 
+                                sea mas rapida puede generar mas problemas de lo 
+                                que crees
+                            </p>
+                            <a>Ir al video</a>
+                        </CardDiv>
+
+                        <CardDiv>
+                            <h1>Borrar System32</h1>
+                            <p>
+                                La popular broma de borrar System32 para que tu pc 
+                                sea mas rapida puede generar mas problemas de lo 
+                                que crees
+                            </p>
+                            <a>Ir al video</a>
+                        </CardDiv>
+
+                        <CardDiv>
+                            <h1>Borrar System32</h1>
+                            <p>
+                                La popular broma de borrar System32 para que tu pc 
+                                sea mas rapida puede generar mas problemas de lo 
+                                que crees
+                            </p>
+                            <a>Ir al video</a>
+                        </CardDiv>
+
+                        <CardDiv>
+                            <h1>Borrar System32</h1>
+                            <p>
+                                La popular broma de borrar System32 para que tu pc 
+                                sea mas rapida puede generar mas problemas de lo 
+                                que crees
+                            </p>
+                            <a>Ir al video</a>
+                        </CardDiv>
+
+                    </SquareDiv>
+
+            </Section1>
+            <div className="Contenedor">
+                <h3 className="title1">Temas populares del canal</h3>
+                <MenuItems />
+            </div>
+            <div className="Contenedor">
+                <h3 className="title1">Reviews mas recientes</h3>
+            </div>
+            <Pie />
         </>
     );
 }
