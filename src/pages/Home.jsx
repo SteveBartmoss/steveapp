48867cc3d233eb296b9133b1ca4f604026389ea1
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-08 19:46:49 -0600

    new desing in progress

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import Pie from "../component/Pie";
 import Branding from "../component/Branding";
 import DivBaner from "../component/DivBaner";
 import MenuItems from "../component/MenuItem";
+import Section1 from "../component/Contenedores/Section1";
 
 
 
@@ -15,6 +16,15 @@ function Home(){
         <>
         {document.title="stevechambitas"}
         <DivBaner name="Baner" />
+        <Section1>
+            <h1 className="title-1">Videos con mas visitas</h1>
+            <p className="p-main">
+                Algunos de los videos mas populares del canal, 
+                escoge uno y pasa un buen rato mientras obtienes 
+                algo de informacion.
+            </p>
+            
+        </Section1>
         <div className="Contenedor">
             <h3 className="title1">Temas populares del canal</h3>
             <MenuItems />   
