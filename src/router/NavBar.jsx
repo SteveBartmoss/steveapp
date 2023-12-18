2cabba4db87da74308f4aba7596351dc1b8bb716
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-17 20:31:35 -0600

    format the document

--- src/router/NavBar.jsx
+++ src/router/NavBar.jsx
@@ -1,46 +1,46 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-export const NavBar=()=>{
-    return(
-            <div className="menu">
+export const NavBar = () => {
+    return (
+        <div className="menu">
 
             <li>
-                <NavLink className={({isActive})=>`iten ${isActive ? 'active':''}`}to="home">Home</NavLink>
+                <NavLink className={({ isActive }) => `iten ${isActive ? 'active' : ''}`} to="home">Home</NavLink>
             </li>
 
             <li>
-                <NavLink className={({isActive})=>`iten ${isActive ? 'active':''}`}to="about">About</NavLink>
+                <NavLink className={({ isActive }) => `iten ${isActive ? 'active' : ''}`} to="about">About</NavLink>
             </li>
 
             <li>
-                <NavLink className={({isActive})=>`iten ${isActive ? 'active':''}`}to="news">News</NavLink>
+                <NavLink className={({ isActive }) => `iten ${isActive ? 'active' : ''}`} to="news">News</NavLink>
             </li>
 
             <li>
-                <NavLink className={({isActive})=>`iten ${isActive ? 'active':''}`}to="almanac">Almanac</NavLink>
+                <NavLink className={({ isActive }) => `iten ${isActive ? 'active' : ''}`} to="almanac">Almanac</NavLink>
             </li>
 
             <li>
-                <NavLink className={({isActive})=>`iten ${isActive ? 'active':''}`}to="articulos">Articulos</NavLink>
+                <NavLink className={({ isActive }) => `iten ${isActive ? 'active' : ''}`} to="articulos">Articulos</NavLink>
             </li>
 
             <li>
-                <NavLink className={({isActive})=>`iten ${isActive ? 'active':''}`}to="stevelabs">SteveLabs</NavLink>
+                <NavLink className={({ isActive }) => `iten ${isActive ? 'active' : ''}`} to="stevelabs">SteveLabs</NavLink>
             </li>
 
             <li>
-                <NavLink className={({isActive})=>`iten ${isActive ? 'active':''}`}to="dumix">Dumix</NavLink>
+                <NavLink className={({ isActive }) => `iten ${isActive ? 'active' : ''}`} to="dumix">Dumix</NavLink>
             </li>
 
             <li>
-                <NavLink className={({isActive})=>`iten ${isActive ? 'active':''}`}to="havefun">Have Fun</NavLink>
+                <NavLink className={({ isActive }) => `iten ${isActive ? 'active' : ''}`} to="havefun">Have Fun</NavLink>
             </li>
 
             <li>
-                <NavLink className={({isActive})=>`iten ${isActive ? 'active':''}`}to="steve">Steve</NavLink>
+                <NavLink className={({ isActive }) => `iten ${isActive ? 'active' : ''}`} to="steve">Steve</NavLink>
             </li>
 
-            </div> 
+        </div>
     );
 }
