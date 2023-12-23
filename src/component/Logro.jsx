8cd9efadbe90c789743546c27c55f4bda74d2df9
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-23 16:32:52 -0600

    implementes the new component

--- src/component/Logro.jsx
+++ src/component/Logro.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import "../App.css";
+import { DivLogro } from "./Contenedores/Cajas";
 
 function Logro({imge, title, paragrap}){
     return(
-        <div className="grid-logro">
+        <DivLogro>
             <img className="imgLogro" src={imge} />
             <div className="div-text">
                 <h1 className="title1 Content">{title}</h1>
                 <p className="Content">{paragrap}</p>
             </div>
-        </div>
+        </DivLogro>
     );
 }
 
