54c7ec4f77406d18f70a9ec7fcb22f73b0bef960
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-24 01:01:53 -0600

    add the prop alt

--- src/component/Logro.jsx
+++ src/component/Logro.jsx
@@ -5,7 +5,7 @@ import { DivLogro } from "./Contenedores/Cajas";
 function Logro({imge, title, paragrap}){
     return(
         <DivLogro>
-            <img className="imgLogro" src={imge} />
+            <img alt="logroimg" className="imgLogro" src={imge} />
             <div className="div-text">
                 <h1 className="title1 Content">{title}</h1>
                 <p className="Content">{paragrap}</p>
