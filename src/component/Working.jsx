283873954f21ec5822b3e7e9b037f31a8a939e93
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-26 20:50:10 -0600

    add the img for the page in progress

--- src/component/Working.jsx
+++ src/component/Working.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { Paper } from "./Contenedores/Cajas";
+import { DivWorkImg, Paper } from "./Contenedores/Cajas";
 
 export function Working() {
 
     return (
         <Paper>
-            <img src="https://media1.giphy.com/media/bGgsc5mWoryfgKBx1u/giphy.gif?cid=53b38410lvif8bs1blkhfr3rjy8w8e4c8pw1yys31s0koyfv&ep=v1_gifs_search&rid=giphy.gif&ct=g" />
+            <h1>Estamos trabajando para mejorar la pagina</h1>
+            <DivWorkImg>
+                <img src="https://media1.giphy.com/media/bGgsc5mWoryfgKBx1u/giphy.gif?cid=53b38410lvif8bs1blkhfr3rjy8w8e4c8pw1yys31s0koyfv&ep=v1_gifs_search&rid=giphy.gif&ct=g" />
+            </DivWorkImg>
+            <h1>404</h1>
+            <h1>Proximamente habra una pagina aqui (espero)</h1>
         </Paper>
     );
 }
