26f6216fb7d23e52273ddbd8c11b8c654f3eada2
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-26 21:34:03 -0600

    add the component for the working section

--- src/dumix/Linux1/pages/Permisos.jsx
+++ src/dumix/Linux1/pages/Permisos.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Working } from "../../../component/Working";
 
 function Rwx(){
     return(
         <div className="div-curso">
-            <h1 className="title1">Permisos</h1>
+            <Working />
         </div>
     );
 }
