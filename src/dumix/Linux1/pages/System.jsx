5a3b27841cd20f8177ea67d6f602668c0671c400
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-26 21:34:55 -0600

    add the component for the working section

--- src/dumix/Linux1/pages/System.jsx
+++ src/dumix/Linux1/pages/System.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Working } from "../../../component/Working";
 
 function System(){
     return(
         <div className="div-curso">
-            <h1 className="title1">System</h1>
+            <Working />
         </div>
     );
 }
