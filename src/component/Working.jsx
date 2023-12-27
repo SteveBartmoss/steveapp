4251917b1802b46c793f13927cadc89406048a3f
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-26 21:10:04 -0600

    add styles for the component

--- src/component/Working.jsx
+++ src/component/Working.jsx
@@ -1,16 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { DivWorkImg, Paper } from "./Contenedores/Cajas";
+import React from "react";
+import { DivCenter, DivWorkImg, Paper } from "./Contenedores/Cajas";
 
 export function Working() {
 
     return (
         <Paper>
-            <h1>Estamos trabajando para mejorar la pagina</h1>
+            <DivCenter>
+                <h1 className="title-working">Estamos trabajando para</h1>
+                <h1 className="title-working">mejorar la pagina</h1>
+            </DivCenter>
             <DivWorkImg>
                 <img src="https://media1.giphy.com/media/bGgsc5mWoryfgKBx1u/giphy.gif?cid=53b38410lvif8bs1blkhfr3rjy8w8e4c8pw1yys31s0koyfv&ep=v1_gifs_search&rid=giphy.gif&ct=g" />
             </DivWorkImg>
-            <h1>404</h1>
-            <h1>Proximamente habra una pagina aqui (espero)</h1>
+            <DivCenter>
+                <h1 className="title-working">404</h1>
+                <h1 className="title-working">Proximamente habra una pagina aqui (espero)</h1>
+            </DivCenter>
         </Paper>
     );
 }
