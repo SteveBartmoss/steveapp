76b93bdfa299f1d5f00d7377056896da050f942e
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-26 21:34:36 -0600

    add the component for the working section

--- src/dumix/Linux1/pages/Sudo.jsx
+++ src/dumix/Linux1/pages/Sudo.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Working } from "../../../component/Working";
 
 function Sudo(){
     return(
         <div className="div-curso">
-            <h1 className="title1">Sudo</h1>
+            <Working />
         </div>
     );
 }
