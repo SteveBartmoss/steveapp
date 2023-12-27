103bbe2ee4705bc376f92c1a443797987aa3ffd5
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-26 20:24:46 -0600

    add new component for the page in progres

---
+++ src/component/Working.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import { Paper } from "./Contenedores/Cajas";
+
+export function Working() {
+
+    return (
+        <Paper>
+            <img src="https://media1.giphy.com/media/bGgsc5mWoryfgKBx1u/giphy.gif?cid=53b38410lvif8bs1blkhfr3rjy8w8e4c8pw1yys31s0koyfv&ep=v1_gifs_search&rid=giphy.gif&ct=g" />
+        </Paper>
+    );
+}
