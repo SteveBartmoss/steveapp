df85981831aae51271521ab0fee719cff6a80d4f
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-26 21:32:49 -0600

    add the component for the working section

--- src/dumix/Linux1/pages/Redireccion.jsx
+++ src/dumix/Linux1/pages/Redireccion.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Working } from "../../../component/Working";
 
 function Redireccion(){
     return(
         <div className="div-curso">
-            <h1 className="title1">Redireccion</h1>
+            <Working />
         </div>
     );
 }
