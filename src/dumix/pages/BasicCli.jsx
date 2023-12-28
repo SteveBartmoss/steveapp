40a9ac435e498f5786b11c532d1d57fd44aeb868
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-27 20:50:07 -0600

    add new page for new article

---
+++ src/dumix/pages/BasicCli.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Paper } from "../../component/Contenedores/Cajas";
+
+export function BasicCli(){
+    return(
+        <>
+            <Paper>
+                <h1>Comandos basicos de linux</h1>
+            </Paper>
+        </>
+    );
+}
