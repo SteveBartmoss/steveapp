f53affe0529d1d0f624414629a2d55a5449efed6
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-27 19:29:36 -0600

    add the new dising for the home page

--- src/dumix/dumixHome.jsx
+++ src/dumix/dumixHome.jsx
@@ -1,76 +1,117 @@
 import React, { useEffect } from "react";
 import DumixAbout from "./pages/dumixAbout";
 import DivBaner from "../component/DivBaner";
+import { DivCenter, DivFlex, DivSection1, DivSection2 } from "../component/Contenedores/Cajas";
+import SquareDiv from "../component/Contenedores/SquareDiv";
+import CardDiv from "../component/Contenedores/CardDiv";
 
 function DumixHome() {
 
-    useEffect(()=>{
-        document.title="Dumix"
-    },[]);
-    
+    useEffect(() => {
+        document.title = "Dumix"
+    }, []);
+
     return (
         <>
             <DivBaner name="back-dumix" />
 
-            <div className="center">
-                <h1 className="title1">Contenido principal</h1>
-            </div>
+            <DivSection1>
+                <DivFlex>
+                    <h1 className="title-1">¿Que es dumix?</h1>
+                    <p className="p-main">
+                        Ya que mis habilidades para apoyar al desarrollo del codigo abierto
+                        no son las mejores y mi tiempo es limitado, cree este apartado en
+                        mi pagina para poder compartir tutoriales para que la gente pueda
+                        introducirse a los comandos de linux de manera facil y perder
+                        el miedo de usar la terminal.
+                    </p>
+                </DivFlex>
+            </DivSection1>
 
-            <div className="display-topics">
-
-                <div className="div-topic">
-
-                    <h1 className="title1">Primeros pasos</h1>
-
-                    <p className="Content">En esta seccion se abordaran temas para personas
+            <DivSection2>
+                <DivFlex>
+                    <h1 className="title-1">Primeros pasos</h1>
+                    <p className="p-main">
+                        En esta seccion se abordaran temas para personas
                         que no han interactuado con linux, se describira lo que es una terminal
                         de comandos, cuales son las partes que componen la terminal, comandos basicos
                         para crear un archovo, como ver los permisos, como listar los elementos de un
                         directorio, como navegae entre direcctorios. Esta parte busca dar una introduccion
-                        a los usuarios que nunca han utilizado comandos</p>
-
-                    <div className="center">
+                        a los usuarios que nunca han utilizado comandos
+                    </p>
+                    <DivCenter>
                         <a className="dumix-btn" href="dumix/linux1">Ir al curso</a>
-                    </div>
+                    </DivCenter>
+                </DivFlex>
+            </DivSection2>
 
-                </div>
-
-                <div className="div-topic">
-                    <h1 className="title1">Pimp my terminal</h1>
-                    <p className="Content">Un poco de estilo a nadie le queda mal y si vas pasar gran
+            <DivSection1>
+                <DivFlex>
+                    <h1 className="title-1">Pimp my terminal</h1>
+                    <p className="p-main">
+                        Un poco de estilo a nadie le queda mal y si vas pasar gran
                         parte de tu tiempo interactuando con la terminal de comando es buena idea que puedas
                         personalizarla y asi por lo menos tener un area de trabajo mas agradable, este post
                         te dara algunsejos para tener un cli mas a la moda y digna de un hackerma asi podras
-                        impresionar a las nenas cuando les muestras todo el tiempo que ahorras por usar vim</p>
-                    <div className="center">
+                        impresionar a las nenas cuando les muestras todo el tiempo que ahorras por usar vim
+                    </p>
+                    <DivCenter>
                         <a className="dumix-btn" href="dumix/pimp">Ir al post</a>
-                    </div>
+                    </DivCenter>
+                </DivFlex>
+            </DivSection1>
 
-                </div>
-
-                <div className="div-topic">
-                    <h1 className="title1">Mejores aplicaciones para terminal</h1>
-                    <p className="Content">Una recopilacion de las mejores aplicaciones que se pueden usar
+            <DivSection2>
+                <DivFlex>
+                    <h1 className="title-1">Mejores aplicaciones para terminal</h1>
+                    <p className="p-main">
+                        Una recopilacion de las mejores aplicaciones que se pueden usar
                         en la terminal y que resultan bastante utiles cuando se esta interactuando con la
-                        consola de linux</p>
-                    <div className="center">
+                        consola de linux
+                    </p>
+                    <DivCenter>
                         <a className="dumix-btn" href="dumix/terminalapps">Ir al post</a>
-                    </div>
-                </div>
+                    </DivCenter>
+                </DivFlex>
+            </DivSection2>
 
-                <div className="div-topic">
-                    <h1 className="title1">Vimgg una guia para mortales</h1>
-                    <p className="Content">Empezar a utilizar vim no es facil pero en esta seccion se encuentran
+            <DivSection1>
+                <DivFlex>
+                    <h1 className="title-1">Vimgg una guia para mortales</h1>
+                    <p className="p-main">Empezar a utilizar vim no es facil pero en esta seccion se encuentran
                         tutoriales o consejos que pueden hacer mas facil el cambio a este tipo de aplicaciones
                         y puede que al final te convence y se convierte en tu editor predilecto</p>
                     <div className="center">
                         <a className="dumix-btn" href="dumix/vimgg">Ir al post</a>
                     </div>
+                </DivFlex>
+            </DivSection1>
 
-                </div>
+            <DivSection2>
+                <DivFlex>
+                    <h1 className="title-1">¿Porque deberia aprender linux?</h1>
 
-            </div>
-            <DumixAbout />
+                    <p className="p-main">Si no eres un usuario que esta enfocado a la informartica, las ingenierias de
+                        computacion o ciencias puede que no sea una gran herramienta, no lo mal interpretes no es malo que aprendas
+                        linux pero siendo un usario comun de computadora no obtendras una herramienta tan util pero es bueno conocer
+                        una alternativa al tipico sistema operativo windows
+                    </p>
+                </DivFlex>
+            </DivSection2>
+
+            <DivSection1>
+                <DivFlex>
+                    <h1 className="title-1">Lista de comando basicos</h1>
+                    <p className="p-main">
+                        Una recopilacion de los comandos basicos que te ayudaran 
+                        manejar linux desde la terminal como todo un sysadmin, o 
+                        por lo menos te dejaran usar tu distribucion de linux
+                    </p>
+                    <DivCenter>
+                        <a className="dumix-btn" href="dumix/clibasic">Ir al post</a>
+                    </DivCenter>
+                </DivFlex>
+            </DivSection1>
         </>
     );
 }
