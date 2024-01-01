ce480cd6b0099a1b4b05bec32931124465cb05d6
Marco <za19012397@zapopan.tecmm.edu.mx>
2023-12-31 19:09:32 -0600

    add more content

--- src/stevelabs/funprog/pages/Compile.jsx
+++ src/stevelabs/funprog/pages/Compile.jsx
@@ -19,17 +19,24 @@ function Prog1Compile() {
                         letras para escribir, así como reglas semánticas pero las computadoras solo saben leer
                         unos y ceros, esto es un problema ya que escribir en unos y ceros puede ser muy complejo
                         para los humanos ya que se tendría que analizar grandes bloques de unos y ceros para
-                        poder entender una palabra. Por esta razón las personas pensaron una mejor manera de dar
-                        instrucciones a una computadora y está es mediante un lenguaje de programación que sea
-                        más fácil de leer para las personas y con esto surgieron los lenguajes de programación,
-                        aunque suene ilógico los primeros programas que se crearon estaban escritos por unos y
-                        ceros así que los programadores de esa época debian leer muy bien sus códigos para no
-                        cometer errores. De manera simple se puede pensar que el proceso de compilar un
-                        programa es transformar cada una de nuestras líneas de código a unos y ceros, el procesó
-                        verdadero es mucho más complejo que lo que se menciona pero para este curso basta con
-                        saber un ejemplo simple ya que la tarea de crear un compilador esta bastante arriba en
-                        la escala de aprendizaje ya que esto suele ser un proyecto de una clase de universidad
-                        de los últimos grados.
+                        poder entender una palabra.
+                    </p>
+
+                    <p className="content-curso">
+                        Por esta razón las personas pensaron una mejor manera de dar instrucciones a una computadora
+                        y está es mediante un lenguaje de programación que sea más fácil de leer para las personas
+                        y con esto surgieron los lenguajes de programación, aunque suene ilógico los primeros
+                        programas que se crearon estaban escritos por unos y ceros así que los programadores de esa
+                        época debian leer muy bien sus códigos para no cometer errores.
+
+                    </p>
+
+                    <p className="content-curso">
+                        De manera simple se puede pensar que el proceso de compilar un programa es transformar
+                        cada una de nuestras líneas de código a unos y ceros, el procesó verdadero es mucho más
+                        complejo que lo que se menciona pero para este curso basta con saber un ejemplo simple ya
+                        que la tarea de crear un compilador esta bastante arriba en la escala de aprendizaje ya que
+                        esto suele ser un proyecto de una clase de universidad de los últimos grados.
                     </p>
 
                     <div className="center">
@@ -41,13 +48,16 @@ function Prog1Compile() {
                         pudo traducir nuestros código a su equivalente en unos y ceros, aunque esto es un
                         error se debe hacer una distinción ya que si un programa no compila es porque su
                         código está más escrito y efectivamente es un error pero suele ser un error en la
-                        escritura del mismo que también le llaman error lexico. Cuando nuestro programa se
-                        termina de compilar y al ejecutarlo no hace lo que esperamos que haga como poner
-                        ejemplo no imprime el mensaje correcto o el resultado de una operación es incorrecto
-                        entonces estamos frente a un error lógico o un error ala hora de decirle a la computadora
-                        lo que tiene que hacer, por esta razón es importante distinguir los dos tipos de errores
-                        ya que los dos provienen de diferente parte y de igual manera la forma de resolver cada
-                        tipo de error es diferente la una de la otra.
+                        escritura del mismo que también le llaman error lexico.
+                    </p>
+
+                    <p className="content-curso">
+                        Cuando nuestro programa se termina de compilar y al ejecutarlo no hace lo que esperamos
+                        que haga como poner ejemplo no imprime el mensaje correcto o el resultado de una operación
+                        es incorrecto entonces estamos frente a un error lógico o un error ala hora de decirle a
+                        la computadora lo que tiene que hacer, por esta razón es importante distinguir los dos
+                        tipos de errores ya que los dos provienen de diferente parte y de igual manera la forma
+                        de resolver cada tipo de error es diferente la una de la otra.
                     </p>
 
                     <div className="center">
@@ -61,6 +71,10 @@ function Prog1Compile() {
                         casos el complidor nos indicara que hay un error en el código y por lo general te menciona la
                         línea en que se encontró el error. Además también te dará una idea de que lo que está mal ya que
                         suele mostrar mensajes como se esperaba ) y se recibio , o se esperaba ; y se recibio : .
+
+                    </p>
+
+                    <p className="content-curso">
                         Mientras mejor hecho este el compilador más información y más errores será capaz de reconocer
                         ya que también puede indicar errores de tipos de datos, que una variable no exista entre muchos
                         otros errores de sintaxis, otra de las características de un buen compilador es que pueda tolerar
@@ -68,7 +82,6 @@ function Prog1Compile() {
                         detecte continúe su proceso y busque la mayor cantidad de errores que pueda, en este sentido el
                         compilador es un buen amigo del programador ya que ayuda en la tarea de resolver errores pero no
                         es perfecto y algunos tipos de errores no será capaz de detectar.
-
                     </p>
 
                     <div className="center">
@@ -80,15 +93,19 @@ function Prog1Compile() {
                         programación este símbolo es necesario para que el programa funcione bien, la razón es simple
                         un punto y cómo indica al compilador el final de una línea de código y así sabe que debe leer
                         la línea que sigue. Así como las personas sabemos que el renglón termino y debemos bajar a leer
-                        el siguiente el programa sabe que al ver un punto y cómo debe leer la siguiente línea de nuestro
+                        el siguiente el programa sabe que al ver un punto y coma debe leer la siguiente línea de nuestro
                         código, puede que te estés preguntando porque el compilador no puede usar el salto de línea para
-                        saber cuándo un renglón termina y esto es porque los humanos lo usamos al escribir y esto limitaría
-                        la forma en que se puede escribir el código pues tendríamos que usar el salto de línea solamente
-                        cuando queremos finalizar una línea de código y no cuando ya se abarcó el espacio legible en la pantalla.
-                        Por lo anterior se decidío usar el punto y cómo carácter final de una línea de código y así permitir
-                        a las personas dar saltos de línea siempre que quieran, logrando que el código quede de una forma más
-                        legible y que no se tenga que desplazar la barra horizontal todo el tiempo para poder leer una línea
-                        bastante larga.
+                        saber cuándo un renglón termina.
+                    </p>
+
+                    <p className="content-curso">
+                        Despues de todo usar un salto de linea es lo mas comun pero los humanos lo usamos al escribir y esto 
+                        limitaría la forma en que se puede escribir el código pues tendríamos que usar el salto de 
+                        línea solamente cuando queremos finalizar una línea de código y no cuando ya se abarcó el espacio 
+                        legible en la pantalla. Por lo anterior se decidío usar el punto y cómo carácter final de una línea 
+                        de código y así permitir a las personas dar saltos de línea siempre que quieran, logrando que el 
+                        código quede de una forma más legible y que no se tenga que desplazar la barra horizontal todo el 
+                        tiempo para poder leer una línea bastante larga.
                     </p>
                 </DivRead>
 
