0fef0f03de969dbd5585f1b33fff5368575eb864
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-05 19:42:22 -0600

    add styles for the divs for more desing

--- src/stevelabs/components/labsFooter.jsx
+++ src/stevelabs/components/labsFooter.jsx
@@ -1,41 +1,43 @@
 import React from "react";
 import { DivFooter, Paper } from "../../component/Contenedores/Cajas";
 
-function LabsFooter(){
-    return(
+function LabsFooter() {
+    return (
         <Paper>
-            <div>
-                <ul className="lista">
+            <div className="div-back-footer">
+                <div className="div-nav-footer">
+                    <ul className="lista">
 
-                    <li>
-                        <a className="archor">Steve labs</a>
-                    </li>
+                        <li>
+                            <a className="archor">SteveLabs</a>
+                        </li>
 
-                    <li>
-                        <a className="archor">Dumix</a>
-                    </li>
+                        <li>
+                            <a className="archor">Dumix</a>
+                        </li>
 
-                    <li>
-                        <a className="archor">Articulos</a>
-                    </li>
+                        <li>
+                            <a className="archor">Articulos</a>
+                        </li>
 
-                </ul>
+                    </ul>
+                </div>
             </div>
             <DivFooter>
                 <div className="footer-item">
-                <h1 className="title1">Importante</h1>
-                <p className="Content">Esta seccion es creada con el fin de compartir informacion 
-                con otros usuarios o personas que quieren empezar en el mundo de linux pero 
-                que pueden verse abrumados por la cantidad de comandos o diferencias que se 
-                tienen con los sistemas de linux</p>
-            </div>
-            <div className="item-grid">
-                <h1 className="title1">Contacto</h1>
-                <p className="contacto">Enviame un <a className="contacto" href="mailto:stevechanvitas@gmail.com">Correo</a></p>
-                <a className="contacto" href="https://github.com/MarcoAntonioGuadalupeRivasGuzman">Githud</a>
-                <a className="contacto" href="https://www.youtube.com/@stevechambitas2782">Youtube</a>
-                <h1 className="title1">Power by <a className="contacto" href="https://giphy.com/">Giphy</a></h1>
-            </div>
+                    <h1 className="title1">Importante</h1>
+                    <p className="Content">Esta seccion es creada con el fin de compartir informacion
+                        con otros usuarios o personas que quieren empezar en el mundo de linux pero
+                        que pueden verse abrumados por la cantidad de comandos o diferencias que se
+                        tienen con los sistemas de linux</p>
+                </div>
+                <div className="item-grid">
+                    <h1 className="title1">Contacto</h1>
+                    <p className="contacto">Enviame un <a className="contacto" href="mailto:stevechanvitas@gmail.com">Correo</a></p>
+                    <a className="contacto" href="https://github.com/MarcoAntonioGuadalupeRivasGuzman">Githud</a>
+                    <a className="contacto" href="https://www.youtube.com/@stevechambitas2782">Youtube</a>
+                    <h1 className="title1">Power by <a className="contacto" href="https://giphy.com/">Giphy</a></h1>
+                </div>
             </DivFooter>
         </Paper>
     );
