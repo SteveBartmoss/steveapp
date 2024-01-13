df0c5b062180dfa2f52b632b6e9faffa7dcc5e70
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-12 20:24:48 -0600

    add more style for the footer

--- src/component/Pie.jsx
+++ src/component/Pie.jsx
@@ -25,33 +25,35 @@ function Pie() {
                             <p className="Content">Esta pagina fue creada como primer
                                 portafolio pero no por eso es menos importante ya que
                                 recibira actualizaciones para hacerla mas interesante.
-                                La version actual seria 2.8.0 y se planea que siga 
+                                La version actual seria 2.8.0 y se planea que siga
                                 evolucionando hasta volverse una plataforma mas completa.
-                                Tambien puedes pasar a mi canal de Youtube quiza encuentras 
+                                Tambien puedes pasar a mi canal de Youtube quiza encuentras
                                 contenido que te agrade y me agregas a tus canales
                                 preferidos sigue aprendiendo.
                             </p>
                         </div>
-                        <DivRow>
-                            <DivColum>
-                                <p className="Content">Cursos</p>
-                                <a className="link-tag">About</a>
-                            </DivColum>
-                            <DivColum>
-                                <p className="Content">Developer</p>
-                                <a className="link-tag">How to</a>
-                            </DivColum>
-                            <DivColum>
-                                <p className="Content">Dumix</p>
-                                <a className="link-tag">Linux basico</a>
-                                <a className="link-tag">Comandos</a>
-                                <a className="link-tag">Vingg</a>
-                            </DivColum>
-                            <DivColum>
-                                <p className="Content">Articulos</p>
-                                <a className="link-tag">Web Masters</a>
-                            </DivColum>
-                        </DivRow>
+                        <div className="div-iconos">
+                            <DivRow>
+                                <DivColum>
+                                    <p className="Content">Cursos</p>
+                                    <a className="link-tag">About</a>
+                                </DivColum>
+                                <DivColum>
+                                    <p className="Content">Developer</p>
+                                    <a className="link-tag">How to</a>
+                                </DivColum>
+                                <DivColum>
+                                    <p className="Content">Dumix</p>
+                                    <a className="link-tag">Linux basico</a>
+                                    <a className="link-tag">Comandos</a>
+                                    <a className="link-tag">Vingg</a>
+                                </DivColum>
+                                <DivColum>
+                                    <p className="Content">Articulos</p>
+                                    <a className="link-tag">Web Masters</a>
+                                </DivColum>
+                            </DivRow>
+                        </div>
                     </DivRow>
                     <hr />
                 </Div70>
@@ -60,7 +62,7 @@ function Pie() {
                         <div className="div-footer-logo">
                             <img className="img-footer-logo" src="/Assets/logoCanal.png" />
                         </div>
-                        <div>
+                        <div className="div-iconos">
                             <DivRow>
                                 <a href="https://www.youtube.com/@stevechambitas2782"><img alt="youtube" className="img-icon" src="/Assets/yticon.png" /></a>
                                 <a href="https://discord.gg/QTdVBq5Q9r"><img alt="discord" className="img-icon" src="/Assets/discord.png" /></a>
