5afb91b5427a62f56785d0eac473152d85481470
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-13 21:50:40 -0600

    add new content for the footer

--- src/HaveFun/component/FooterHave.jsx
+++ src/HaveFun/component/FooterHave.jsx
@@ -1,32 +1,98 @@
 import React from "react";
 import "../../App.css";
+import { Div60, Div70, DivAuto, DivColum, DivFooter, DivRow } from "../../component/Contenedores/Cajas";
 
-function FooterHave({power, linkPower}) {
+function FooterHave({ power, linkPower }) {
 
     return (
-        <div className="gif-footer">
-            <div className="footer-item">
-                <h1 className="title1">Importante</h1>
-                <p className="Content">Esta seccion fue creada gracias al curso de
-                    react de Fernando Herrera, si quieres apoyarlo puedes comprar su
-                    curso o recomendarlo.
-                </p>
+        <>
+            <DivFooter>
+                <Div70>
+                    <ul className="lista">
+                        <li>
+                            <a className="archor">SteveLabs</a>
+                        </li>
+                        <li>
+                            <a className="archor">Dumix</a>
+                        </li>
+                        <li>
+                            <a className="archor">Articulos</a>
+                        </li>
+                    </ul>
+                </Div70>
+                <Div70>
+                    <DivRow>
+                        <Div60>
+                            <h1 className="title1">Importante</h1>
+                            <p className="Content">Esta seccion es creada con el fin de compartir informacion
+                                con otros usuarios o personas que quieren empezar en el mundo de linux pero
+                                que pueden verse abrumados por la cantidad de comandos o diferencias que se
+                                tienen con los sistemas de linux</p>
+                        </Div60>
+                        <DivRow>
+                            <DivColum>
+                                <p className="Content">Cursos</p>
+                                <a className="link-tag">About</a>
+                            </DivColum>
+                            <DivColum>
+                                <p className="Content">Developer</p>
+                                <a className="link-tag">How to</a>
+                            </DivColum>
+                            <DivColum>
+                                <p className="Content">Dumix</p>
+                                <a className="link-tag">Linux basico</a>
+                                <a className="link-tag">Comandos</a>
+                                <a className="link-tag">Vingg</a>
+                            </DivColum>
+                            <DivColum>
+                                <p className="Content">Articulos</p>
+                                <a className="link-tag">Web Masters</a>
+                            </DivColum>
+                        </DivRow>
+                    </DivRow>
+                    <hr />
+                </Div70>
+                <Div70>
+                    <DivRow>
+                        <div className="div-footer-logo">
+                            <img className="img-footer-logo" alt="logosteve" src="/Assets/logoCanal.png" />
+                        </div>
+                        <DivAuto>
+                            <DivRow>
+                                <a href="https://www.youtube.com/@stevechambitas2782"><img alt="youtube" className="img-icon" src="/Assets/yticon.png" /></a>
+                                <a href="https://discord.gg/QTdVBq5Q9r"><img alt="discord" className="img-icon" src="/Assets/discord.png" /></a>
+                                <a href="https://github.com/MarcoAntonioGuadalupeRivasGuzman"><img alt="github" className="img-icon" src="/Assets/github.png" /></a>
+                                <a href="mailto:stevechanvitas@gmail.com"><img className="img-icon" alt="email" src="/Assets/email.png" /></a>
+                            </DivRow>
+                        </DivAuto>
+                    </DivRow>
+                    <h1 className="title1">Power by <a className="contacto" href="https://react.dev/">React</a></h1>
+                </Div70>
+            </DivFooter>
+            <div className="gif-footer">
+                <div className="footer-item">
+                    <h1 className="title1">Importante</h1>
+                    <p className="Content">Esta seccion fue creada gracias al curso de
+                        react de Fernando Herrera, si quieres apoyarlo puedes comprar su
+                        curso o recomendarlo.
+                    </p>
 
-                <p className="Content">Esta pagina forma parte de mi portafolio, si
-                    te interesa puedes contactarme en algunas de las siguientes redes.</p>
+                    <p className="Content">Esta pagina forma parte de mi portafolio, si
+                        te interesa puedes contactarme en algunas de las siguientes redes.</p>
 
-                <p className="Content">Tambien puedes pasar a mi canal de Youtube
-                    quiza encuentras contenido que te agrade y me agregas a tus canala
-                    preferidos</p>
+                    <p className="Content">Tambien puedes pasar a mi canal de Youtube
+                        quiza encuentras contenido que te agrade y me agregas a tus canala
+                        preferidos</p>
+                </div>
+                <div className="item-grid">
+                    <h1 className="title1">Contacto</h1>
+                    <p className="contacto">Enviame un <a className="contacto" href="mailto:stevechanvitas@gmail.com">Correo</a></p>
+                    <a className="contacto" href="https://github.com/MarcoAntonioGuadalupeRivasGuzman">Githud</a>
+                    <a className="contacto" href="https://www.youtube.com/@stevechambitas2782">Youtube</a>
+                    <h1 className="title1">Power by <a className="contacto" href={linkPower} target="blanck">{power}</a></h1>
+                </div>
             </div>
-            <div className="item-grid">
-                <h1 className="title1">Contacto</h1>
-                <p className="contacto">Enviame un <a className="contacto" href="mailto:stevechanvitas@gmail.com">Correo</a></p>
-                <a className="contacto" href="https://github.com/MarcoAntonioGuadalupeRivasGuzman">Githud</a>
-                <a className="contacto" href="https://www.youtube.com/@stevechambitas2782">Youtube</a>
-                <h1 className="title1">Power by <a className="contacto" href={linkPower} target="blanck">{power}</a></h1>
-            </div>
-        </div>
+        </>
     );
 }
 
