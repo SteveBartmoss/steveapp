e9837d3f48a13a5f5e44c58c8f890ebfe2ed4065
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-13 22:09:12 -0600

    remove content duplicated

--- src/HaveFun/component/FooterHave.jsx
+++ src/HaveFun/component/FooterHave.jsx
@@ -24,10 +24,13 @@ function FooterHave({ power, linkPower }) {
                     <DivRow>
                         <Div60>
                             <h1 className="title1">Importante</h1>
-                            <p className="Content">Esta seccion es creada con el fin de compartir informacion
-                                con otros usuarios o personas que quieren empezar en el mundo de linux pero
-                                que pueden verse abrumados por la cantidad de comandos o diferencias que se
-                                tienen con los sistemas de linux</p>
+                            <p className="Content">Esta seccion fue creada gracias al curso de
+                                react de Fernando Herrera, si quieres apoyarlo puedes comprar su
+                                curso o recomendarlo. Esta pagina forma parte de mi portafolio, si
+                                te interesa puedes contactarme en algunas de las siguientes redes.
+                                Tambien puedes pasar a mi canal de Youtube quiza encuentras contenido 
+                                que te agrade y me agregas a tus canales preferidos.
+                            </p>
                         </Div60>
                         <DivRow>
                             <DivColum>
@@ -69,29 +72,6 @@ function FooterHave({ power, linkPower }) {
                     <h1 className="title1">Power by <a className="contacto" href="https://react.dev/">React</a></h1>
                 </Div70>
             </DivFooter>
-            <div className="gif-footer">
-                <div className="footer-item">
-                    <h1 className="title1">Importante</h1>
-                    <p className="Content">Esta seccion fue creada gracias al curso de
-                        react de Fernando Herrera, si quieres apoyarlo puedes comprar su
-                        curso o recomendarlo.
-                    </p>
-
-                    <p className="Content">Esta pagina forma parte de mi portafolio, si
-                        te interesa puedes contactarme en algunas de las siguientes redes.</p>
-
-                    <p className="Content">Tambien puedes pasar a mi canal de Youtube
-                        quiza encuentras contenido que te agrade y me agregas a tus canala
-                        preferidos</p>
-                </div>
-                <div className="item-grid">
-                    <h1 className="title1">Contacto</h1>
-                    <p className="contacto">Enviame un <a className="contacto" href="mailto:stevechanvitas@gmail.com">Correo</a></p>
-                    <a className="contacto" href="https://github.com/MarcoAntonioGuadalupeRivasGuzman">Githud</a>
-                    <a className="contacto" href="https://www.youtube.com/@stevechambitas2782">Youtube</a>
-                    <h1 className="title1">Power by <a className="contacto" href={linkPower} target="blanck">{power}</a></h1>
-                </div>
-            </div>
         </>
     );
 }
