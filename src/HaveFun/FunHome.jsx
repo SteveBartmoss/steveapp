e0a92ea7981e925a18a57d043952b0125e4eb631
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-13 22:08:52 -0600

    remove content duplicated

--- src/HaveFun/FunHome.jsx
+++ src/HaveFun/FunHome.jsx
@@ -65,31 +65,6 @@ function FunHome() {
             </div>
 
             <FooterHave />
-
-            <div className="gif-footer">
-                <div className="footer-item">
-                    <h1 className="title1">Importante</h1>
-                    <p className="Content">Estas seccion fueron creadas gracias al curso de
-                        react de Fernando Herrera, si quieres apoyarlo puedes comprar su
-                        curso o recomendarlo.
-                    </p>
-
-                    <p className="Content">Esta pagina forma parte de mi portafolio, si
-                        te interesa puedes contactarme en algunas de las siguientes redes.</p>
-
-                    <p className="Content">Tambien puedes pasar a mi canal de Youtube
-                        quiza encuentras contenido que te agrade y me agregas a tus canala
-                        preferidos</p>
-                </div>
-
-                <div className="item-grid">
-                    <h1 className="title1">Contacto</h1>
-                    <p className="contacto">Enviame un <a className="contacto" href="mailto:stevechanvitas@gmail.com">Correo</a></p>
-                    <a className="contacto" href="https://github.com/MarcoAntonioGuadalupeRivasGuzman">Githud</a>
-                    <a className="contacto" href="https://www.youtube.com/@stevechambitas2782">Youtube</a>
-                </div>
-            </div>
-
         </>
     );
 }
