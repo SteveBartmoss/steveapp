a8db3d5947f291f83444cbfb406ddae471c3108d
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-22 21:43:07 -0600

    center the button to video of the article

--- src/component/TagVideo.jsx
+++ src/component/TagVideo.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { DivCenter } from "./Contenedores/Cajas";
 
-function TagVideo({url}){
-    return(
+function TagVideo({ url }) {
+    return (
         <div className="back-articles">
             <p className="content-curso">Si lo prefieres pueder ver el contenido en youtube aqui:</p>
-            <a className="dumix-btn" href={url}>Ver video</a>
+            <DivCenter>
+                <a className="dumix-btn" href={url}>Ver video</a>
+            </DivCenter>
         </div>
     );
 }
