8258a6e4e90ca494f02886727a7debd85f0e49f8
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-24 17:17:07 -0600

    add new component for the card of the characters in the app ricoso

---
+++ src/HaveFun/component/CardCharacter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CardCharacter({character}){
+    return(
+        <>
+            <div>
+                <img src={character.image} />
+                <h1>{character.name}</h1>
+            </div>
+        </>
+    );
+}
