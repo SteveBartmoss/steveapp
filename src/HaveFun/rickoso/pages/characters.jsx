c3246bb0c53a327762e7388e39b56eadeb36a5eb
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-24 17:10:12 -0600

    add the satate and map for show the characters in ricoso app

--- src/HaveFun/rickoso/pages/characters.jsx
+++ src/HaveFun/rickoso/pages/characters.jsx
@@ -1,24 +1,34 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-function Characters(){
+function Characters() {
+
+    const [personajes, setPersonajes] = useState([]);
 
     const listCharacters = async () => {
-        try{
+        try {
             const response = await axios.get('https://rickandmortyapi.com/api/character');
             console.log(response.data.results);
+            setPersonajes(response.data.results);
         }
-        catch (error){
+        catch (error) {
             console.error(error);
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         listCharacters();
-    },[]);
+    }, []);
 
-    return(
-        <h1 className="title1">Characters Page</h1>
+    return (
+        <>
+            <h1 className="title1">Characters Page</h1>
+            {
+                personajes.map(perso=>
+                        <h1>{perso.name}</h1>
+                    )
+            }
+        </>
     );
 }
 
