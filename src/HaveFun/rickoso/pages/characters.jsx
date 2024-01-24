dc885ee5300724aead98e63ce9f1e9247ba9563d
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-23 21:12:44 -0600

    test the first implementation for the app ricoso

--- src/HaveFun/rickoso/pages/characters.jsx
+++ src/HaveFun/rickoso/pages/characters.jsx
@@ -1,6 +1,22 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 function Characters(){
+
+    const listCharacters = async () => {
+        try{
+            const response = await axios.get('https://rickandmortyapi.com/api/character');
+            console.log(response.data.results);
+        }
+        catch (error){
+            console.error(error);
+        }
+    }
+
+    useEffect(()=>{
+        listCharacters();
+    },[]);
+
     return(
         <h1 className="title1">Characters Page</h1>
     );
