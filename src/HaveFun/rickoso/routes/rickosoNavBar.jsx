f2adfea1ce467856206e0faec3c624aee8134748
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-23 20:36:38 -0600

    add the components navlink for the nav bar in the ricoso app

--- src/HaveFun/rickoso/routes/rickosoNavBar.jsx
+++ src/HaveFun/rickoso/routes/rickosoNavBar.jsx
@@ -1,19 +1,23 @@
 import React from "react";
-import { NavLink, Outlet  } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
-function RickosoNavBar(){
-    return(
+function RickosoNavBar() {
+    return (
         <>
-        <div>
             <div>
-                <div className="rick-menu">
-                    <li>Personajes</li>
-                    <li>Locaciones</li>
-                    <li>Episodios</li>
+                <div className="div-menu-ricoso">
+                    <li>
+                        <NavLink className={({ isActive }) => `iten ${isActive ? 'active' : ''}`}>Personajes</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={({ isActive }) => `iten ${isActive ? 'active' : ''}`}>Locaciones</NavLink>
+                    </li>
+                    <li>
+                        <NavLink className={({ isActive }) => `iten ${isActive ? 'active' : ''}`}>Episodios</NavLink>
+                    </li>
                 </div>
                 <Outlet />
             </div>
-        </div>
         </>
     );
 }
