287327617e0c622d5adcf7c6ca94346c75608f79
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-24 18:48:10 -0600

    add classname for the styles

--- src/HaveFun/component/CardCharacter.jsx
+++ src/HaveFun/component/CardCharacter.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function CardCharacter({character}){
     return(
         <>
-            <div>
-                <img src={character.image} />
-                <h1>{character.name}</h1>
-                <h1>{character.status}</h1>
-                <h1>{character.species}</h1>
+            <div className="div-character-card">
+                <img className="img-character" src={character.image} />
+                <h1 className="content-card">{character.name}</h1>
+                <h1 className="content-card">{character.status}</h1>
+                <h1 className="content-card">{character.species}</h1>
             </div>
         </>
     );
