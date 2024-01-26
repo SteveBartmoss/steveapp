230c71cc1cea18682856b0508fdf13a6dff1e43a
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-25 19:48:10 -0600

    add the call of api for the page locations

--- src/HaveFun/rickoso/pages/locations.jsx
+++ src/HaveFun/rickoso/pages/locations.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-function Locations(){
-    return(
-        <h1 className="title1">Locations Page</h1>
-    )
+function Locations() {
+
+    const [locaciones, setLocaciones] = useState([]);
+
+    const listLocaciones = async () => {
+        try {
+            const response = await axios.get('https://rickandmortyapi.com/api/location');
+            setLocaciones(response.data.results);
+            console.log(response.data.results);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        listLocaciones();
+    }, []);
+
+    return (
+        <>
+            <h1 className="title1">Locations Page</h1>
+            {
+                locaciones.map(locacion=>
+                    <h1>{locacion.name}</h1>
+                )
+            }
+        </>
+    );
 }
 
 export default Locations;
