74a0b77ab3e00d523b8cbc3a238b754ffa1c8aeb
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-25 20:11:00 -0600

    add the call of api for the episodes in ricoso app

--- src/HaveFun/rickoso/pages/episodes.jsx
+++ src/HaveFun/rickoso/pages/episodes.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-function Episodes(){
-    return(
-        <h1 className="title1">Episodes page</h1>
+function Episodes() {
+
+    const [episodios, setEpisodios] = useState([]);
+
+    const listEpisodios = async () => {
+        const response = await axios.get("https://rickandmortyapi.com/api/episode");
+        setEpisodios(response.data.results);
+        console.log(response.data.results);
+    }
+
+    useEffect(() => {
+        listEpisodios();
+    }, []);
+
+    return (
+        <>
+            <h1 className="title1">Episodes page</h1>
+            {
+                episodios.map(episodio=>
+                        <h1>{episodio.name}</h1>
+                    )
+            }
+        </>
     );
 }
 
