bbd8309df884b2bee73257c5e2ce69ba3d5b6b33
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-27 14:20:14 -0600

    add more content

--- src/HaveFun/rickoso/pages/rickosoapp.jsx
+++ src/HaveFun/rickoso/pages/rickosoapp.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
-function RickosoApp(){
-    return(
+function RickosoApp() {
+    return (
         <>
-            <h1>Rickoso App</h1>
+            <div className="div-ricoso-page">
+                <div>
+                    <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ204dm12MmJzZXJsYmI3ZThzZzhieWNtdXZ0aDA0cnA1N2Jlb3d0eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l41lI4bYmcsPJX9Go/giphy.gif" />
+                </div>
+                <h1>Rickoso App</h1>
+                <p>
+                    Ponte ricoso con esta aplicacion en la que
+                    encontrartas informacion de rick and morty
+                </p>
+            </div>
         </>
     );
 }
