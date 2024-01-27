d5b05d7523b09168c471db98ce512ea3a391d02b
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-26 19:57:47 -0600

    add the fragment for future implements

--- src/HaveFun/component/CardLocation.jsx
+++ src/HaveFun/component/CardLocation.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-function CardLocation({location}){
-    return(
-        <div className="div-character-card">
-            <h1 className="content-card">{location.name}</h1>
-            <h1 className="content-card">{location.dimension}</h1>
-            <h1 className="content-card">{location.type}</h1>
-        </div>
+function CardLocation({ location }) {
+    return (
+        <>
+            <div className="div-character-card">
+                <h1 className="content-card">{location.name}</h1>
+                <h1 className="content-card">{location.dimension}</h1>
+                <h1 className="content-card">{location.type}</h1>
+            </div>
+        </>
     );
 }
 
