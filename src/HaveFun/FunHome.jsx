547b936e470e33cb8eee33422273216956930dec
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-27 19:20:01 -0600

    implement the component divarticle

--- src/HaveFun/FunHome.jsx
+++ src/HaveFun/FunHome.jsx
@@ -1,32 +1,41 @@
 import React, { useEffect } from "react";
 import DivBaner from "../component/DivBaner";
 import FooterHave from "./component/FooterHave";
+import { DivArticle, DivCenter } from "../component/Contenedores/Cajas";
 
 function FunHome() {
 
-    useEffect(()=>{
-        document.title="Havefun"
-    },[])
+    useEffect(() => {
+        document.title = "Havefun"
+    }, [])
     return (
         <>
             <DivBaner name="baner-have-fun" />
 
-            <div className="div-havefun">
-
-                <p className="p-havefun">Un apartado donde puedes divertirte un poco con las aplicaciones
+            <DivArticle>
+                <p className="p-havefun">
+                    Un apartado donde puedes divertirte un poco con las aplicaciones
                     que ofrecen diferentes interacciones e informacion de diferentes personajes
-                    que te puede agradar</p>
+                    que te puede agradar
+                </p>
 
                 <div className="card-havefun">
+                    <img src="https://media2.giphy.com/media/xT5LMHxhOfscxPfIfm/giphy.gif?cid=53b38410k5i0xybbeivy1ma041z940wlsg0da8flau90pw09&ep=v1_gifs_search&rid=giphy.gif&ct=g" />
                     <h1 className="h1-havefun">gifApp</h1>
-                    <p className="p-card">Una aplicacion para buscar gifs que te gusten
-                        solo ingresa las palabras busca tus gifs favoritos</p>
+                    <p className="p-card">
+                        Una aplicacion para buscar gifs que te gusten
+                        solo ingresa las palabras busca tus gifs favoritos
+                    </p>
                     <div className="button-havefun">
                         <a className="a-card" href="havefun/gifapp">visitar</a>
                     </div>
                 </div>
 
                 <div className="card-havefun">
+                    <DivCenter>
+                        <img src="https://media3.giphy.com/media/l0ExsgrTuACbtPaqQ/giphy.gif?cid=53b38410ndcui17do9mm7yvqvihom3di66snfry5342g9gbq&ep=v1_gifs_search&rid=giphy.gif&ct=g" />
+                    </DivCenter>
+
                     <h1 className="h1-havefun">OraculoApp</h1>
                     <p className="p-card">Una aplicacion a la cual puedes hacerle preguntas
                         para sabar mas sobre tu destino</p>
@@ -36,6 +45,7 @@ function FunHome() {
                 </div>
 
                 <div className="card-havefun">
+                    <img src="https://media0.giphy.com/media/jsYfLXpibzkdr5sjZ8/giphy.gif?cid=ecf05e47es8xl4cwolqrqacyj3u1cev1hz6tsox2w6ux4j9x&ep=v1_gifs_related&rid=giphy.gif&ct=g" />
                     <h1 className="h1-havefun">rickosoApp</h1>
                     <p className="p-card">Relajate un rato y ponte rickoso mientras buscas informacion
                         sobre tus personajes favoritos de rick y morty</p>
@@ -61,6 +71,10 @@ function FunHome() {
                         <a className="a-card" href="havefun/pokeapp">visitar</a>
                     </div>
                 </div>
+
+            </DivArticle>
+
+            <div className="div-havefun">
 
             </div>
 
