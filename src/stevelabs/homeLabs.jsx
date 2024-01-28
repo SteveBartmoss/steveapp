f95e180dcfc546112d984ee0b57729cb153dfd9f
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-01-27 20:17:53 -0600

    implement the component divarticle and divmosaico

--- src/stevelabs/homeLabs.jsx
+++ src/stevelabs/homeLabs.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
 import LabsAbout from "./components/labsAbout";
+import { DivArticle, DivMosaico } from "../component/Contenedores/Cajas";
 
 function HomeLabs() {
 
-    useEffect(()=>{
-        document.title="Stevelabs"
-    },[]);
-    
+    useEffect(() => {
+        document.title = "Stevelabs"
+    }, []);
+
     return (
         <>
             <div className="center">
@@ -17,60 +18,65 @@ function HomeLabs() {
                 <h1 className="title1">Comienza a aprender</h1>
             </div>
 
+            <DivArticle>
+                <DivMosaico>
+
+                    <div className="div-min-article">
+                        <div className="center">
+                            <h1 className="title1">Primeros pasos en programacion</h1>
+                        </div>
+                        <p className="Content">En este curso se abordan temas basicos de programacion
+                            ya que esta orientado a personas que comienzan a programar por lo cual no
+                            se esperan muchos conocimientos</p>
+                        <div className="center">
+                            <a className="dumix-btn" href="stevelabs/program1">Ir al curso</a>
+                        </div>
+
+                    </div>
+
+                    <div className="div-min-article">
+                        <div className="center">
+                            <h1 className="title1">Primeros pasos en Base de datos</h1>
+                        </div>
+                        <p className="Content">En este curso se abordan temas basicos sobre bases de
+                            datos, esta orientado a personas que empiezan por lo
+                            cual no se esperan muchos conocimientos
+                        </p>
+                        <div className="center">
+                            <a className="dumix-btn" href="stevelabs/program1">Ir al curso</a>
+                        </div>
+
+                    </div>
+
+                    <div className="div-min-article">
+                        <div className="center">
+                            <h1 className="title1">Web developer</h1>
+                        </div>
+                        <p className="Content">En este apartado puede consultar los articulos que fueron
+                            creados por un colaborador, los temas tratados seran sobre desarrollo de paginas
+                            web y que permitiran tener informacion sobre este tipo de temas
+                        </p>
+                        <div className="center">
+                            <a className="dumix-btn" href="webdeveloper">Ver articulos</a>
+                        </div>
+                    </div>
+
+                    <div className="div-min-article">
+                        <div className="center">
+                            <h1 className="title1">Coder Life</h1>
+                        </div>
+                        <p className="Content">Aqui podras leer articulos sobre temas de la vida
+                            de desarolladores, juegos o simplemente reflexiones sobre temas de informatica
+                        </p>
+                        <div className="center">
+                            <a className="dumix-btn" href="#">Ver articulos</a>
+                        </div>
+                    </div>
+
+                </DivMosaico>
+            </DivArticle>
+
             <div className="div-flex-articles">
-
-                <div className="div-min-article">
-                    <div className="center">
-                        <h1 className="title1">Primeros pasos en programacion</h1>
-                    </div>
-                    <p className="Content">En este curso se abordan temas basicos de programacion
-                        ya que esta orientado a personas que comienzan a programar por lo cual no
-                        se esperan muchos conocimientos</p>
-                    <div className="center">
-                        <a className="dumix-btn" href="stevelabs/program1">Ir al curso</a>
-                    </div>
-
-                </div>
-
-                <div className="div-min-article">
-                    <div className="center">
-                        <h1 className="title1">Primeros pasos en Base de datos</h1>
-                    </div>
-                    <p className="Content">En este curso se abordan temas basicos sobre bases de
-                        datos, esta orientado a personas que empiezan por lo 
-                        cual no se esperan muchos conocimientos
-                    </p>
-                    <div className="center">
-                        <a className="dumix-btn" href="stevelabs/program1">Ir al curso</a>
-                    </div>
-
-                </div>
-
-                <div className="div-min-article">
-                    <div className="center">
-                        <h1 className="title1">Web developer</h1>
-                    </div>
-                    <p className="Content">En este apartado puede consultar los articulos que fueron
-                        creados por un colaborador, los temas tratados seran sobre desarrollo de paginas
-                        web y que permitiran tener informacion sobre este tipo de temas
-                    </p>
-                    <div className="center">
-                        <a className="dumix-btn" href="webdeveloper">Ver articulos</a>
-                    </div>
-                </div>
-
-                <div className="div-min-article">
-                    <div className="center">
-                        <h1 className="title1">Coder Life</h1>
-                    </div>
-                    <p className="Content">Aqui podras leer articulos sobre temas de la vida 
-                    de desarolladores, juegos o simplemente reflexiones sobre temas de informatica
-                    </p>
-                    <div className="center">
-                        <a className="dumix-btn" href="#">Ver articulos</a>
-                    </div>
-                </div>
-
 
             </div>
 
