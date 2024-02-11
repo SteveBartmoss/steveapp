fcfadc3dcd26244ea213df401eeba5ef6d0bb6db
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-02-11 16:59:04 -0600

    add new route for the new article

--- src/stevelabs/WebArticles/routes/WebRoutes.jsx
+++ src/stevelabs/WebArticles/routes/WebRoutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import Opinion1 from '../Articles/Opinion1';
+import LetVsVar from '../Articles/LetVsVar';
 
 
 
@@ -9,6 +10,8 @@ const WebRoutes = () => {
     <div>
          <Routes>
                 <Route path="opinion1" element={<Opinion1/>} />
+
+                <Route path='letvsvar' element={<LetVsVar />} />
          </Routes>
       
     </div>
