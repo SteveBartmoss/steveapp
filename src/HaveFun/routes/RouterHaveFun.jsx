878564b1ee095fdd5ec1621eb0cce712ad6bcb0f
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-02-12 21:20:14 -0600

    add new routes for the poke app

--- src/HaveFun/routes/RouterHaveFun.jsx
+++ src/HaveFun/routes/RouterHaveFun.jsx
@@ -9,30 +9,32 @@ import { PokeMain } from "../pokeapp/pokeMain";
 import Locations from "../rickoso/pages/locations";
 import Episodes from "../rickoso/pages/episodes";
 import RickosoApp from "../rickoso/pages/rickosoapp";
+import PokeAppNavBar from "../pokeapp/routes/pokeappNavBar";
+import Pokemons from "../pokeapp/pages/pokemons";
+import PokeApp from "../pokeapp/pages/pokeapp";
 
-function HaveFunRouter(){
-    return(
+function HaveFunRouter() {
+    return (
         <>
             <Routes>
 
-                <Route path="rickosoapp" element={<RickosoNavbar/>}>
+                <Route path="rickosoapp" element={<RickosoNavbar />}>
                     <Route index element={<RickosoApp />} />
 
                     <Route path="home" element={<RickosoApp />} />
-                    
+
                     <Route path="personajes" element={<Characters />} />
 
                     <Route path="locaciones" element={<Locations />} />
 
                     <Route path="episodios" element={<Episodes />} />
-                    
+
                 </Route>
-                
-                
+
             </Routes>
 
             <Routes>
-                <Route path="gifapp" element={<GifApp />}/>
+                <Route path="gifapp" element={<GifApp />} />
             </Routes>
 
             <Routes>
@@ -40,11 +42,17 @@ function HaveFunRouter(){
             </Routes>
 
             <Routes>
-                <Route path="marvelapp" element={<MarvelApp />}/>
+                <Route path="marvelapp" element={<MarvelApp />} />
             </Routes>
 
             <Routes>
-                <Route path="pokeapp" element={<PokeMain />} />
+                <Route path="pokeapp" element={<PokeAppNavBar />}>
+                    <Route index element={<PokeApp />} />
+
+                    <Route path="home" element={<PokeApp />} />
+
+                    <Route path="pokemones" element={<Pokemons />} />
+                </Route>
             </Routes>
         </>
     );
