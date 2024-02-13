0038c01866b774f03d717dcccf3d51b21e62a055
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-02-12 21:22:06 -0600

    add new page for the pokemons page

---
+++ src/HaveFun/pokeapp/pages/pokemons.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { DivArticle } from "../../../component/Contenedores/Cajas";
+
+function Pokemons() {
+    return (
+        <>
+            <h1 className="tittle-app">Pokemones</h1>
+            <DivArticle>
+                
+            </DivArticle>
+        </>
+    );
+}
+
+export default Pokemons;
