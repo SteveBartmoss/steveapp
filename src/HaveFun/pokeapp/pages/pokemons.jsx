72129f406c3d73dd86e7488e7a5434d9d0fee3c0
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-02-13 19:53:59 -0600

    add the hook use state for call the function and set the pokemons

--- src/HaveFun/pokeapp/pages/pokemons.jsx
+++ src/HaveFun/pokeapp/pages/pokemons.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { DivArticle } from "../../../component/Contenedores/Cajas";
 import axios from "axios";
 
@@ -6,16 +6,20 @@ function Pokemons() {
 
     const [pokemons, setPokemons] = useState([]);
 
-    const lisPokemons = async () => {
+    const listPokemons = async () => {
         try{
-            const repsonse = await axios.get('https://pokeapi.co/api/v2/pokemon');
+            const response = await axios.get('https://pokeapi.co/api/v2/pokemon');
             setPokemons(response.data.results);
         }
         catch (error){
             console.error(error);
         }
     }
-    
+
+    useEffect(()=>{
+        listPokemons();
+    },[]);
+
     return (
         <>
             <h1 className="tittle-app">Pokemones</h1>
