45019f90c55b69556b81f36cd4cbe28fbbed09ea
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-02-13 19:45:28 -0600

    add the state of the pokemons and a function for the call of the api pokemon

--- src/HaveFun/pokeapp/pages/pokemons.jsx
+++ src/HaveFun/pokeapp/pages/pokemons.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { DivArticle } from "../../../component/Contenedores/Cajas";
+import axios from "axios";
 
 function Pokemons() {
+
+    const [pokemons, setPokemons] = useState([]);
+
+    const lisPokemons = async () => {
+        try{
+            const repsonse = await axios.get('https://pokeapi.co/api/v2/pokemon');
+            setPokemons(response.data.results);
+        }
+        catch (error){
+            console.error(error);
+        }
+    }
+    
     return (
         <>
             <h1 className="tittle-app">Pokemones</h1>
             <DivArticle>
-                
+
             </DivArticle>
         </>
     );
