53597dcded192f0cc74373d345406a7ec36416ef
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-02-19 21:17:56 -0600

    add a class name for the elements in the ul list of the road map page

--- src/pages/roadMap.jsx
+++ src/pages/roadMap.jsx
@@ -19,23 +19,23 @@ function RoadMap() {
                     <h1 className="title1">Mejoras que llegaran a la pagina</h1>
 
                     <ul>
-                        <li>
+                        <li className="list-item">
                             <p>Curso de porgamacion completo</p>
                         </li>
 
-                        <li>
+                        <li className="list-item">
                             <p>Dos secciones nuevas en have fun &#40; ricoso app y poke app &#41;</p>
                         </li>
 
-                        <li>
+                        <li className="list-item">
                             <p>Mas contenido para dumix</p>
                         </li>
 
-                        <li>
+                        <li className="list-item">
                             <p>Nuevos articulos de rows and colums</p>
                         </li>
 
-                        <li>
+                        <li className="list-item">
                             <p>Nuevos trucos para linux</p>
                         </li>
                     </ul>
@@ -45,23 +45,23 @@ function RoadMap() {
                     <h1 className="title1">Cursos nuevos que tendra la pagina</h1>
 
                     <ul>
-                        <li>
+                        <li className="list-item">
                             <p>Curso basico de base de datos</p>
                         </li>
 
-                        <li>
+                        <li className="list-item">
                             <p>Curso de Programacion Orientad a objetos</p>
                         </li>
 
-                        <li>
+                        <li className="list-item">
                             <p>Implementaciones y ejemplos de algoritmos</p>
                         </li>
 
-                        <li>
+                        <li className="list-item">
                             <p>Curso basico de git</p>
                         </li>
 
-                        <li>
+                        <li className="list-item">
                             <p>Conceptos de creacion de creacion de UI en java</p>
                         </li>
                     </ul>
