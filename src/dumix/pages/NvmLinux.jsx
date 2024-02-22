5d4160f070618cc88301c9adcf41614a77eec98b
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-02-21 21:40:18 -0600

    add more content for the new article

--- src/dumix/pages/NvmLinux.jsx
+++ src/dumix/pages/NvmLinux.jsx
@@ -4,6 +4,11 @@ function NvmLinux(){
     return(
         <>
         <h1>Node Version Manager</h1>
+
+        <p>
+            Una gran herramienta a la hora de crear paginas web o 
+            aplicaciones web es node js ya que nos permite ejecutar
+        </p>
         </>
     );
 }
