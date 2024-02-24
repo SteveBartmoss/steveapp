3504d575a8651c10c67d47ab6d4dc38a9dc30dc2
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-02-23 21:06:04 -0600

    add more content for the post of nvm in linux

--- src/dumix/pages/NvmLinux.jsx
+++ src/dumix/pages/NvmLinux.jsx
@@ -7,7 +7,11 @@ function NvmLinux(){
 
         <p>
             Una gran herramienta a la hora de crear paginas web o 
-            aplicaciones web es node js ya que nos permite ejecutar
+            aplicaciones web es node js ya que nos permite ejecutar 
+            el entorno de programacion para una pagina web, pero si 
+            llevas un tiempo usando sistemas linux entonces sabras que 
+            es un poco elaborado instalar programas que no estan en los 
+            repositorios oficiales
         </p>
         </>
     );
