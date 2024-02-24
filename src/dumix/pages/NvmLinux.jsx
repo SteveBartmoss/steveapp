fb6834c6cb385a79863712de8f329f9311acca70
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-02-24 16:08:13 -0600

    add more content for the article

--- src/dumix/pages/NvmLinux.jsx
+++ src/dumix/pages/NvmLinux.jsx
@@ -1,24 +1,32 @@
 import React from "react";
-import { DivCenter, Paper } from "../../component/Contenedores/Cajas";
+import { Div70, DivCenter, DivRead, Paper } from "../../component/Contenedores/Cajas";
 
-function NvmLinux(){
-    return(
+function NvmLinux() {
+    return (
         <>
-        <Paper>
-            <DivCenter>
-                <h1 className="title-1">Node Version Manager</h1>
-            </DivCenter>
-        </Paper>
-        
+            <Paper>
 
-        <p>
-            Una gran herramienta a la hora de crear paginas web o 
-            aplicaciones web es node js ya que nos permite ejecutar 
-            el entorno de programacion para una pagina web, pero si 
-            llevas un tiempo usando sistemas linux entonces sabras que 
-            es un poco elaborado instalar programas que no estan en los 
-            repositorios oficiales
-        </p>
+                <DivCenter>
+                    <h1 className="title-1">Node Version Manager</h1>
+                </DivCenter>
+
+                <Div70>
+
+                    <DivRead>
+                        <p>
+                            Una gran herramienta a la hora de crear paginas web o
+                            aplicaciones web es node js ya que nos permite ejecutar
+                            el entorno de programacion para una pagina web, pero si
+                            llevas un tiempo usando sistemas linux entonces sabras que
+                            es un poco elaborado instalar programas que no estan en los
+                            repositorios oficiales puese ser algo tardado o para algunos 
+                            puede ser un dificil ya que aunque me guste usar linux 
+                            debo admitir que en ocaciones puede ser algo frustante/
+                        </p>
+                    </DivRead>
+                </Div70>
+            </Paper>
+
         </>
     );
 }
