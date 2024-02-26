7929105d460f07cdcc7e4f0b58ca465f1e1b4add
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-02-25 19:46:45 -0600

    add more content for the article

--- src/dumix/pages/NvmLinux.jsx
+++ src/dumix/pages/NvmLinux.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Div70, DivCenter, DivRead, Paper } from "../../component/Contenedores/Cajas";
+import { Div70, DivCenter, DivCode, DivRead, Paper } from "../../component/Contenedores/Cajas";
 
 function NvmLinux() {
     return (
@@ -23,22 +23,40 @@ function NvmLinux() {
                         </p>
 
                         <p className="content-curso">
-                            Esto es porque si el programa no esta en los repositorios 
-                            oficiales es algo tardado o para algunos puede ser dificil 
-                            ya que aunque me guste usar linux debo admitir que en ocaciones 
-                            puede ser algo frustante tener que seguir mas pasos para poder 
+                            Esto es porque si el programa no esta en los repositorios
+                            oficiales es algo tardado o para algunos puede ser dificil
+                            ya que aunque me guste usar linux debo admitir que en ocaciones
+                            puede ser algo frustante tener que seguir mas pasos para poder
                             instalar un programa.
                         </p>
 
                         <p className="content-curso">
-                            Fue gracias a esto que descubri una herramienta para poder instalar 
-                            node js en una distribucion de linux de una forma rapida y simple, 
-                            nvm quiere decir node version manager y como su nombre los indica es 
-                            mas que un simple instalador de node si no que tambien es un manejador 
+                            Fue gracias a esto que descubri una herramienta para poder instalar
+                            node js en una distribucion de linux de una forma rapida y simple,
+                            nvm quiere decir node version manager y como su nombre los indica es
+                            mas que un simple instalador de node si no que tambien es un manejador
                             de versiones que nos permite mantener diferentes versiones.
                         </p>
 
+                        <p>
+                            Para instalar esta aplicacion debemos tener curl que es un comando
+                            que nos permite traer datos desde un servidor mediante una url, este
+                            comando puede ser util pero tambien debe ser usado con cuidado pues
+                            software malicioso podria llegar usando este comando, en este caso el
+                            url es el oficial del programa pero siempre se debe verificar que es lo
+                            que estamos descargando
+                        </p>
+
+                        <DivCenter>
+                            <DivCode>
+                                <p className="code-simple">curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash</p>
+                            </DivCode>
+                        </DivCenter>
+
+                        
+
                         <h1 className="title1">Mas que un simple instalador</h1>
+
 
                     </DivRead>
                 </Div70>
