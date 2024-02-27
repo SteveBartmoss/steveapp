3210cb1036eddd36ca2a4c7a9a7ec0a2478855ba
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-02-27 09:06:19 -0600

    add new file for the data of quotes

---
+++ src/component/Data/Quotes.js
@@ -0,0 +1,34 @@
+export const Quotes=[
+    {
+        quote: 'Hablar es barato, muestrame el codigo',
+        autor: 'Linus Torvalds'
+    },
+    {
+        quote: 'Algunas personas me han dicho que no creen que un pingüino gordo encarne el espíritu de Linux, lo que me dice que nunca han visto a un pingüino cabreado corriendo hacia ellos a más de 100 millas por hora',
+        autor: 'Linus Torvalds'
+    },
+    {
+        quote: 'El manejo de memoria de powerpc puede ser usado para asustar a ninos pequenos',
+        autor: 'Linus Torvalds'
+    },
+    {
+        quote: 'Si compila esta bien, si arranca es perfecto',
+        autor: 'Linus Torvalds'
+    },
+    {
+        quote: 'Si debuggear es el proceso de remover errores de codigo, entonces programar debe ser el proceso de ponerlos',
+        autor: 'Edsger Dijkstra'
+    },
+    {
+        quote: 'Reglas de optimizacion Regla 1: No lo hagas Regla2 (solo para expertos): No lo hagas todavia',
+        autor: 'Michael A Jackson'
+    },
+    {
+        quote: 'Cualquier codigo escrito que no hayas mirado en los ultimos seis meses bien podria haber sido escrito por otra persona',
+        autor: 'Eaglesons law'
+    },
+    {
+        quote: 'Nunca confies en un ordenador que no puedas lanzar por una ventana',
+        autor: 'Steve Wozniak'
+    }
+]
