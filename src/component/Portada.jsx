851ee3096de691baf1b2b655f06862d92ef6337d
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-03-10 00:40:07 -0600

    change the elements in the page steve

--- src/component/Portada.jsx
+++ src/component/Portada.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { DivColum, DivRow } from "./Contenedores/Cajas";
 
 function Portada({image}){
     return(
         <section className="portada">
-            <div className="frame-img">
-                <img className="img-portada"  src={image}/>
-                <h1 className="title1">Steve</h1>
-                <p className="text-portada">Estudiante, programador, amante de los videjuegos</p>
-            </div>
+            <DivRow>
+                <div className="frame-img">
+                    <img className="img-portada"  src={image}/>            
+                </div>
+                <DivColum>
+                    <h1 className="title1">Steve</h1>
+                    <p className="text-portada">Estudiante, programador, amante de los videjuegos</p>
+                </DivColum>
+            </DivRow>
         </section>
     );
 }
