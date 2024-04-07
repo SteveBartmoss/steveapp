2bf080fae08984cf73c643d0a2d39a853ac4ca65
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-04-07 12:20:15 -0600

    se cambio el componente, ahora retorno diferentes elementos de la cards

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,15 +1,43 @@
 import React from "react";
 import '../App.css';
 
-function Card({imgUrl,title,paragraph,urlVideo}){
+export function Card({children,imgUrl,title,paragraph,urlVideo}){
     return(
-        <div className="div-card">
-            <img className="imgCard" src={imgUrl} alt="imagenVideo"/>
-            <h3 className="titleCard">{title}</h3>
-            <p>{paragraph}</p>
-            <a className="linkVideo" href={urlVideo} target="_blank">Ir al video</a>
+        <div className="card">
+            {children}
         </div>
     );
 }
 
-export default Card;
+
+export function CardTitle({children}){
+    return(
+        <div className="card-title">
+            {children}
+        </div>
+    );
+}
+
+export function CardText({children}){
+    return(
+        <div className="card-text">
+            {children}
+        </div>
+    );   
+}
+
+export function CardHeader({children}){
+    return(
+        <div className="card-header">
+            {children}
+        </div>
+    );
+}
+
+export function CardFooter({children}){
+    return(
+        <div className="card-footer">
+            {children}
+        </div>
+    );
+}
