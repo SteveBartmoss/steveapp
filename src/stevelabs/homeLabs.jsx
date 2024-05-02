49c2b27af97a89356208ec4c3ebd356dc6fd3ff8
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-05-01 20:34:37 -0600

    se incion con el cambio de diseno

--- src/stevelabs/homeLabs.jsx
+++ src/stevelabs/homeLabs.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import LabsAbout from "./components/labsAbout";
-import { DivArticle, DivMosaico } from "../component/Contenedores/Cajas";
+import { Div70, DivArticle, DivColum, DivMosaico, DivRow, Panel } from "../component/Contenedores/Cajas";
 import LabsFooter from "./components/labsFooter";
+import { Card, CardFooter, CardHeader, CardText, CardTitle } from "../component/Card";
 
 function HomeLabs() {
 
@@ -19,63 +20,105 @@ function HomeLabs() {
                 <h1 className="title1">Comienza a aprender</h1>
             </div>
 
-            <DivArticle>
-                <DivMosaico>
+            <Panel>
+                <Div70>
+                    <DivRow>
+                        <DivColum>
+                            <Card>
+                                <DivRow>
+                                    <DivColum>
+                                        <CardHeader>
+                                            <p>steve</p>
+                                            <p>18/01/21</p>
+                                        </CardHeader>
+                                        <CardTitle>Primeros pasos en programacion</CardTitle>
+                                        <CardText>
+                                            En este curso se abordan temas basicos de programacion
+                                            ya que esta orientado a personas que comienzan a programar por lo cual no
+                                            se esperan muchos conocimientos
+                                        </CardText>
+                                        <CardFooter>
+                                            <a className="dumix-btn" href="stevelabs/program1">Ir al curso</a>
+                                        </CardFooter>
+                                    </DivColum>
+                                    <DivColum>
+                                        <img className="card-image" src="/Assets/music.png" alt="discimg" />
+                                    </DivColum>
+                                </DivRow>
+                            </Card>
 
-                    <div className="div-min-article">
-                        <div className="center">
-                            <h1 className="title1">Primeros pasos en programacion</h1>
-                        </div>
-                        <p className="Content">En este curso se abordan temas basicos de programacion
-                            ya que esta orientado a personas que comienzan a programar por lo cual no
-                            se esperan muchos conocimientos</p>
-                        <div className="center">
-                            <a className="dumix-btn" href="stevelabs/program1">Ir al curso</a>
-                        </div>
+                            <Card>
+                                <DivRow>
+                                    <DivColum>
+                                        <CardHeader>
+                                            <p>steve</p>
+                                            <p>18/01/21</p>
+                                        </CardHeader>
+                                        <CardTitle>Primeros pasos en Base de datos</CardTitle>
+                                        <CardText>
+                                            En este curso se abordan temas basicos sobre bases de
+                                            datos, esta orientado a personas que empiezan por lo
+                                            cual no se esperan muchos conocimientos
+                                        </CardText>
+                                        <CardFooter>
+                                            <a className="dumix-btn" href="stevelabs/program1">Ir al curso</a>
+                                        </CardFooter>
+                                    </DivColum>
+                                    <DivColum>
+                                        <img className="card-image" src="/Assets/music.png" alt="discimg" />
+                                    </DivColum>
+                                </DivRow>
+                            </Card>
+                        </DivColum>
+                        <DivColum>
+                            <Card>
+                                <DivRow>
+                                    <DivColum>
+                                        <CardHeader>
+                                            <p>steve</p>
+                                            <p>18/01/21</p>
+                                        </CardHeader>
+                                        <CardTitle>Web developer</CardTitle>
+                                        <CardText>
+                                            En este apartado puede consultar los articulos que fueron
+                                            creados por un colaborador, los temas tratados seran sobre desarrollo de paginas
+                                            web y que permitiran tener informacion sobre este tipo de temas
+                                        </CardText>
+                                        <CardFooter>
+                                            <a className="dumix-btn" href="webdeveloper">Ver articulos</a>
+                                        </CardFooter>
+                                    </DivColum>
+                                    <DivColum>
+                                        <img className="card-image" src="/Assets/music.png" alt="discimg" />
+                                    </DivColum>
+                                </DivRow>
+                            </Card>
 
-                    </div>
-
-                    <div className="div-min-article">
-                        <div className="center">
-                            <h1 className="title1">Primeros pasos en Base de datos</h1>
-                        </div>
-                        <p className="Content">En este curso se abordan temas basicos sobre bases de
-                            datos, esta orientado a personas que empiezan por lo
-                            cual no se esperan muchos conocimientos
-                        </p>
-                        <div className="center">
-                            <a className="dumix-btn" href="stevelabs/program1">Ir al curso</a>
-                        </div>
-
-                    </div>
-
-                    <div className="div-min-article">
-                        <div className="center">
-                            <h1 className="title1">Web developer</h1>
-                        </div>
-                        <p className="Content">En este apartado puede consultar los articulos que fueron
-                            creados por un colaborador, los temas tratados seran sobre desarrollo de paginas
-                            web y que permitiran tener informacion sobre este tipo de temas
-                        </p>
-                        <div className="center">
-                            <a className="dumix-btn" href="webdeveloper">Ver articulos</a>
-                        </div>
-                    </div>
-
-                    <div className="div-min-article">
-                        <div className="center">
-                            <h1 className="title1">Coder Life</h1>
-                        </div>
-                        <p className="Content">Aqui podras leer articulos sobre temas de la vida
-                            de desarolladores, juegos o simplemente reflexiones sobre temas de informatica
-                        </p>
-                        <div className="center">
-                            <a className="dumix-btn" href="stevelabs/coder">Ver articulos</a>
-                        </div>
-                    </div>
-
-                </DivMosaico>
-            </DivArticle>
+                            <Card>
+                                <DivRow>
+                                    <DivColum>
+                                        <CardHeader>
+                                            <p>steve</p>
+                                            <p>18/01/21</p>
+                                        </CardHeader>
+                                        <CardTitle>Coder Life</CardTitle>
+                                        <CardText>
+                                            Aqui podras leer articulos sobre temas de la vida
+                                            de desarolladores, juegos o simplemente reflexiones sobre temas de informatica
+                                        </CardText>
+                                        <CardFooter>
+                                            <a className="dumix-btn" href="stevelabs/coder">Ver articulos</a>
+                                        </CardFooter>
+                                    </DivColum>
+                                    <DivColum>
+                                        <img className="card-image" src="/Assets/music.png" alt="discimg" />
+                                    </DivColum>
+                                </DivRow>
+                            </Card>
+                        </DivColum>
+                    </DivRow>
+                </Div70>
+            </Panel>
 
             <LabsAbout />
 
