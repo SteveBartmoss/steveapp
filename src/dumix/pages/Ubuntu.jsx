3e7020f9b6cde5c2f40bea18cd67121d65a4f4ce
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-05-28 21:35:28 -0600

    se agrego mas contenido a la pagina de ubuntu

--- src/dumix/pages/Ubuntu.jsx
+++ src/dumix/pages/Ubuntu.jsx
@@ -2,46 +2,49 @@
 
 export function Ubuntu() {
     return (
-        <p>
-            una de las distribuciones mas comunes a la hora de recomendar linux
-            para personas que no son expertas en el tema es ubuntu ya que suele ser
-            un sistema operativo muy amigable para los usuarios lo cual no esta equivocado
-            ya que luego de probarlo por un ano puedo decir que es una de las aplicaciones
-            mas amigables que existen.
+        <div>
+            <h1>Ubuntu la vieja confiable</h1>
+            <p>
+                una de las distribuciones mas comunes a la hora de recomendar linux
+                para personas que no son expertas en el tema es ubuntu ya que suele ser
+                un sistema operativo muy amigable para los usuarios lo cual no esta equivocado
+                ya que luego de probarlo por un ano puedo decir que es una de las aplicaciones
+                mas amigables que existen.
 
-            Ventajas
+                Ventajas
 
-            instalacion simple
-            Una de las principales ventajas de ubuntu es que tiene un sistema para instalar
-            todo por interfaz grafica, lo cual puede parecer raro de mencionar pero hay que tener en
-            cuenta que no todas las distribuciones vienen con un instalador grafico desde el principio
-            siendo arch linux un buen ejemplo de esto, si bien ubunutu tiene un instalador grafico no
-            representa una gran ventaja frente a todas las distribuciones pues una cantidad considerable
-            ya incorporan esto pero como esto se trata de distribuciones enfocadas a usuarios inexpertos
-            se puede decir que tiene una instalacion simple ya que basta con llenar los campos que piden y dar
-            siguiente.
+                instalacion simple
+                Una de las principales ventajas de ubuntu es que tiene un sistema para instalar
+                todo por interfaz grafica, lo cual puede parecer raro de mencionar pero hay que tener en
+                cuenta que no todas las distribuciones vienen con un instalador grafico desde el principio
+                siendo arch linux un buen ejemplo de esto, si bien ubunutu tiene un instalador grafico no
+                representa una gran ventaja frente a todas las distribuciones pues una cantidad considerable
+                ya incorporan esto pero como esto se trata de distribuciones enfocadas a usuarios inexpertos
+                se puede decir que tiene una instalacion simple ya que basta con llenar los campos que piden y dar
+                siguiente.
 
-            tienda de aplicaciones
-            Otra de las ventajas que tiene es que cuenta con una tienda de aplicaciones lo cual no viene
-            por defecto con todas las distribuciones linux, y en comparacion con algunas otras distribuciones
-            que tienen tiendas creo que la de ubuntu es la que mas aplicaciones populares tiene, esto hace
-            que instalar editores como vscode o programas como gimp sea tna facil como buscarlo en la tienda y hacer
-            click en descargar.
+                tienda de aplicaciones
+                Otra de las ventajas que tiene es que cuenta con una tienda de aplicaciones lo cual no viene
+                por defecto con todas las distribuciones linux, y en comparacion con algunas otras distribuciones
+                que tienen tiendas creo que la de ubuntu es la que mas aplicaciones populares tiene, esto hace
+                que instalar editores como vscode o programas como gimp sea tna facil como buscarlo en la tienda y hacer
+                click en descargar.
 
-            listo para usar desde el principio
-            creo que la principal ventaja de esta distribucion es que practicamente viene lista desde la instalacion
-            para usar, ya que seleccionando la instalacion de drivers de terceros se obtiene un sistema operativo completo
-            como si de  microsoft se tratara, lo cual lo hace excelente para usuarios que no tienen mucha idea de como
-            preparar un sistema operativo desde cero lo cual lo hace una buena opcion para iniciar
+                listo para usar desde el principio
+                creo que la principal ventaja de esta distribucion es que practicamente viene lista desde la instalacion
+                para usar, ya que seleccionando la instalacion de drivers de terceros se obtiene un sistema operativo completo
+                como si de  microsoft se tratara, lo cual lo hace excelente para usuarios que no tienen mucha idea de como
+                preparar un sistema operativo desde cero lo cual lo hace una buena opcion para iniciar
 
-            gran cantidad de tutoriales y comunidad
-            siendo una de las dsitribuciones mas populares por mucho tiempo cuenta con una gran cantidad de foros,
-            tutoriales e informacion para resolver problemas, instalar aplicaciones fuera de la tienda y tambien para
-            personalizacion asi que es probable que un problema que tengas se pueda arreglar de una forma simple o sin
-            tener que buscar en foros rusos
+                gran cantidad de tutoriales y comunidad
+                siendo una de las dsitribuciones mas populares por mucho tiempo cuenta con una gran cantidad de foros,
+                tutoriales e informacion para resolver problemas, instalar aplicaciones fuera de la tienda y tambien para
+                personalizacion asi que es probable que un problema que tengas se pueda arreglar de una forma simple o sin
+                tener que buscar en foros rusos
 
-            gran compatibilidad de drivers y programas
-            teniendo como base la distribucion debian
-        </p>
+                gran compatibilidad de drivers y programas
+                teniendo como base la distribucion debian
+            </p>
+        </div>
     )
 }
