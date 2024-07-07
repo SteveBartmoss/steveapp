fbd860ac474422b4713d1ae13162c029b07d5719
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-07 13:40:25 -0600

    se agrego mas contenido al articulo

--- src/stevelabs/WebArticles/Articles/LetVsVar.jsx
+++ src/stevelabs/WebArticles/Articles/LetVsVar.jsx
@@ -19,32 +19,33 @@ function LetVsVar() {
 
                 <CreatorSing autor={"Steve"} date={"08/04/23"} img={"/Assets/steveA.png"} grade={"Free coder"} />
 
-                <div className="back-articles">
-                    <p className="content-curso">
-                        El uso de var en JavaScript ha sido tradicionalmente la forma de declarar variables, pero con la introducción de let y const en ECMAScript 6 (ES6), hay varias razones por las cuales var ya no es aconsejable para la mayoría de los casos. Aquí te explico algunas de estas razones:
-                        Problemas con var
+                <p className="content-curso">
+                    El uso de var en JavaScript ha sido tradicionalmente la forma de declarar variables, pero con la introducción de let y const en ECMAScript 6 (ES6), hay varias razones por las cuales var ya no es aconsejable para la mayoría de los casos. Aquí te explico algunas de estas razones:
+                    Problemas con var
 
-                        Ámbito de Función (Function Scope) en lugar de Ámbito de Bloque (Block Scope):
-                        Las variables declaradas con var tienen ámbito de función, lo que significa que están disponibles en toda la función en la que se declaran, incluso si se declaran dentro de un bloque (por ejemplo, dentro de un if o un for).
-                        Esto puede llevar a comportamientos inesperados y errores difíciles de depurar.
-                    </p>
-                </div>
+                    Ámbito de Función (Function Scope) en lugar de Ámbito de Bloque (Block Scope):
+                    Las variables declaradas con var tienen ámbito de función, lo que significa que están disponibles en toda la función en la que se declaran, incluso si se declaran dentro de un bloque (por ejemplo, dentro de un if o un for).
+                    Esto puede llevar a comportamientos inesperados y errores difíciles de depurar.
+                </p>
+
+                <h1 className="title1">Problemas con el Hoisting:</h1>
+                <hr className="line" />
+
+                <p className="content-curso">
+                    Las variables declaradas con var son "hoisted" (elevadas) al comienzo de su contexto de ejecución (contexto de función o contexto global), pero no se inicializan. Esto significa que puedes usar la variable antes de su declaración sin obtener un error.
+                </p>
+
+                <p className="content-curso">
+                    javascript
+
+                    console.log(x); // undefined
+                    var x = 5;
+                </p>
+
+
             </div>
 
 
-
-            <h1>Problemas con el Hoisting:</h1>
-
-            <p>
-                Las variables declaradas con var son "hoisted" (elevadas) al comienzo de su contexto de ejecución (contexto de función o contexto global), pero no se inicializan. Esto significa que puedes usar la variable antes de su declaración sin obtener un error.
-            </p>
-
-            <p>
-                javascript
-
-                console.log(x); // undefined
-                var x = 5;
-            </p>
 
             <p>
                 Aunque esto puede parecer conveniente, a menudo puede llevar a errores difíciles de entender y depurar.
