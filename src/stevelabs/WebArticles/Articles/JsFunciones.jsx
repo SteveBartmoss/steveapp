952395026346c229ace1c6dc366a465b36d4974e
Steve <za19012397@zapopan.tecmm.edu.mx>
2024-07-07 13:18:13 -0600

    se agrego otra pagina para un articulo sobre java script

---
+++ src/stevelabs/WebArticles/Articles/JsFunciones.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export function JsFunciones(){
+    return(
+        <>
+            <div>
+                <h1>Funciones utiles de javaScript</h1>
+                <p>
+                    Si eres programador web entonces seguro has trabajado 
+                    con javaScript ya que este es uno de los lenguajes mas 
+                    usado para darle logica a componentes de la web, por esta 
+                    razon es buena idea conocer esta funciones que te pueden 
+                    ayudar a no crear tanto codigo y aprovechar el tiempo para 
+                    ver memes
+                </p>
+
+                <h1>map</h1>
+
+                <p>
+                    
+                </p>
+            </div>
+        </>
+    )
+}
